85cd7320492e79e299cf29c3e04252c83a4b0945
larry <26318510+larry0x@users.noreply.github.com>
2023-12-25 17:32:13 +0000

    add comments

--- guests/greeter/src/lib.rs
+++ guests/greeter/src/lib.rs
@@ -5,19 +5,24 @@ use sdk::Region;
 /// E.g. if input is `"Larry"`, the output should be `"Hello Larry!"`.
 ///
 /// The input and output of this function are strings, which are not primitive
-/// types. Therefore we have to dynamically allocate them onto the Wasm module's
+/// types. Therefore we have to dynamically allocate them into the Wasm module's
 /// linear memory, and pass pointers over the FFI boundary.
 #[no_mangle]
 pub extern "C" fn hello(region_addr: usize) -> usize {
+    // use Region::consume to read a byte slice that has been loaded into the
+    // Wasm memory by the host
     let name_bytes = unsafe { Region::consume(region_addr as *mut Region) };
+
+    // attempt to parse the input data into a String
     let name = String::from_utf8(name_bytes).unwrap_or_else(|err| {
         panic!("Failed to parse name from utf8: {err}");
     });
 
+    // say hello
     let greeting = format!("Hello {name}!");
-    let greeting_bytes = greeting.into_bytes();
 
-    Region::release_buffer(greeting_bytes) as usize
+    // use Region::release_buffer to return the response to the host
+    Region::release_buffer(greeting.into_bytes()) as usize
 }
 
 // ----------------------------------- tests -----------------------------------
@@ -29,10 +34,18 @@ mod tests {
     #[test]
     fn greeting() {
         let name = "Larry".as_bytes().to_vec();
+
+        // host uses Region::release_buffer to load the input data into Wasm
+        // memory
         let ptr = Region::release_buffer(name);
 
+        // host calls guest function, gets a memory address back
         let addr = hello(ptr as usize);
+
+        // host uses Region::consume to read the byte slice returned by guest
         let bytes = unsafe { Region::consume(addr as *mut Region) };
+
+        // attempt to parse the response into a String
         let greeting = String::from_utf8(bytes).unwrap();
 
         assert_eq!(greeting, "Hello Larry!");
