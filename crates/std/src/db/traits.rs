12b9cc2d3ba6600b03b4c25d74bc7d157dde3916
larry <26318510+larry0x@users.noreply.github.com>
2024-01-02 14:04:32 +0000

    add a comment

--- crates/std/src/db/traits.rs
+++ crates/std/src/db/traits.rs
@@ -4,18 +4,23 @@ use crate::{Batch, Op, Order, Record};
 pub trait Storage {
     fn read(&self, key: &[u8]) -> anyhow::Result<Option<Vec<u8>>>;
 
+    fn write(&mut self, key: &[u8], value: &[u8]) -> anyhow::Result<()>;
+
+    fn remove(&mut self, key: &[u8]) -> anyhow::Result<()>;
+
     // minimum bound is always inclusive, maximum bound is always exclusive.
     // if min > max, an empty iterator is to be returned.
+    //
+    // calling next() on this iterator returns Option<Result<Record>>.
+    // if it's None, it means the record doesn't exist (iteration reached end);
+    // if it's Some(Err), if means record exists but failed to read it;
+    // if it's Some(OK(value)), the record exists and successfully read.
     fn scan<'a>(
         &'a self,
         min:   Option<&[u8]>,
         max:   Option<&[u8]>,
         order: Order,
     ) -> Box<dyn Iterator<Item = anyhow::Result<Record>> + 'a>;
-
-    fn write(&mut self, key: &[u8], value: &[u8]) -> anyhow::Result<()>;
-
-    fn remove(&mut self, key: &[u8]) -> anyhow::Result<()>;
 
     /// Apply a batch of inserts or deletes all together.
     fn apply(&mut self, batch: Batch) -> anyhow::Result<()> {
