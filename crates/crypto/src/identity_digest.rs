d16d092693f1723b03b428da4b2e54432ce1815a
larry <26318510+larry0x@users.noreply.github.com>
2024-01-07 11:24:59 +0000

    add cw-crypto crate

---
+++ crates/crypto/src/identity_digest.rs
@@ -0,0 +1,49 @@
+use digest::{
+    consts::U32, generic_array::GenericArray, FixedOutput, HashMarker, OutputSizeUser, Update,
+};
+
+/// Hash the given message using BLAKE3 hash function, return the result hash as
+/// an Identity256.
+pub(crate) fn hash(data: &[u8]) -> Identity256 {
+    let mut digest = Identity256::default();
+    digest.update(blake3::hash(data).as_bytes());
+    digest
+}
+
+/// To utilize the `signature::DigestVerifier::verify_digest` method, the digest
+/// must implement the `digest::Digest` trait, which in turn requires the
+/// following traits:
+///
+/// - Default
+/// - OutputSizeUser
+/// - Update
+/// - FixedOutput
+/// - HashMarker
+///
+/// Here we define a container struct that implements the required traits.
+///
+/// Adapted from:
+/// https://github.com/CosmWasm/cosmwasm/blob/main/packages/crypto/src/identity_digest.rs
+#[derive(Default, Clone)]
+pub struct Identity256 {
+    bytes: GenericArray<u8, U32>,
+}
+
+impl OutputSizeUser for Identity256 {
+    type OutputSize = U32;
+}
+
+impl Update for Identity256 {
+    fn update(&mut self, data: &[u8]) {
+        assert_eq!(data.len(), 32);
+        self.bytes = *GenericArray::from_slice(data);
+    }
+}
+
+impl FixedOutput for Identity256 {
+    fn finalize_into(self, out: &mut digest::Output<Self>) {
+        *out = self.bytes
+    }
+}
+
+impl HashMarker for Identity256 {}
---
+++ crates/crypto/src/secp256r1.rs
@@ -0,0 +1,58 @@
+use {
+    crate::identity_digest::hash,
+    p256::ecdsa::{signature::DigestVerifier, Signature, VerifyingKey},
+};
+
+pub fn secp256r1_verify(prehash_msg: &[u8], sig: &[u8], pk: &[u8]) -> anyhow::Result<()> {
+    let hash = hash(prehash_msg);
+    let sig = Signature::from_bytes(sig.into())?;
+    let vk = VerifyingKey::from_sec1_bytes(pk)?;
+    vk.verify_digest(hash, &sig).map_err(Into::into)
+}
+
+// ----------------------------------- tests -----------------------------------
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        p256::ecdsa::{signature::DigestSigner, Signature, SigningKey},
+        rand::rngs::OsRng,
+    };
+
+    #[test]
+    fn verifying_secp256k1() {
+        // generate a valid signature
+        let sk = SigningKey::random(&mut OsRng);
+        let vk = VerifyingKey::from(&sk);
+        let prehash_msg = b"Jake";
+        let msg = hash(prehash_msg);
+        let sig: Signature = sk.sign_digest(msg.clone());
+
+        // valid signature
+        {
+            assert!(vk.verify_digest(msg.clone(), &sig).is_ok());
+        }
+
+        // incorrect private key
+        {
+            let false_sk = SigningKey::random(&mut OsRng);
+            let false_sig: Signature = false_sk.sign_digest(msg.clone());
+            assert!(vk.verify_digest(msg.clone(), &false_sig).is_err());
+        }
+
+        // incorrect public key
+        {
+            let false_sk = SigningKey::random(&mut OsRng);
+            let false_vk = VerifyingKey::from(&false_sk);
+            assert!(false_vk.verify_digest(msg, &sig).is_err());
+        }
+
+        // incorrect message
+        {
+            let false_prehash_msg = b"Larry";
+            let false_msg = hash(false_prehash_msg);
+            assert!(vk.verify_digest(false_msg, &sig).is_err());
+        }
+    }
+}
