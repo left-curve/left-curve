47feaaae551b63cc2c54c8ca0723bb2a749bd792
larry <26318510+larry0x@users.noreply.github.com>
2024-01-26 14:53:50 +0000

    remove unused stuff

--- crates/crypto/src/lib.rs
+++ crates/crypto/src/lib.rs
@@ -1,13 +1,11 @@
 mod error;
 mod identity_digest;
-mod key;
 mod secp256k1;
 mod secp256r1;
 
 pub use crate::{
     error::{CryptoError, CryptoResult},
     identity_digest::Identity256,
-    key::{Curve, SigningKey},
     secp256k1::secp256k1_verify,
     secp256r1::secp256r1_verify,
 };
