a66d4915f1c14a3ccf9ce372977f60ec2963655d
larry <26318510+larry0x@users.noreply.github.com>
2024-01-27 12:02:05 +0000

    create utils crate

---
+++ crates/utils/src/lib.rs
@@ -0,0 +1,115 @@
+use {
+    anyhow::{bail, ensure},
+    cw_std::{hash, to_json, Addr, Binary, Coins, Config, GenesisState, Hash, Message},
+    serde::ser::Serialize,
+    std::{fs::File, io::Read, path::Path},
+};
+
+pub enum AdminOption {
+    SetToAddr(Addr),
+    SetToSelf,
+    SetToNone,
+}
+
+/// Helper for building genesis state. See the examples folder of this repository
+/// for an example.
+pub struct GenesisBuilder {
+    cfg:        Option<Config>,
+    code_msgs:  Vec<Message>,
+    other_msgs: Vec<Message>,
+}
+
+impl GenesisBuilder {
+    pub fn new() -> Self {
+        Self {
+            cfg:        None,
+            code_msgs:  vec![],
+            other_msgs: vec![],
+        }
+    }
+
+    pub fn store_code(&mut self, path: impl AsRef<Path>) -> anyhow::Result<Hash> {
+        // read Wasm byte code from file
+        let mut file = File::open(path)?;
+        let mut wasm_byte_code = vec![];
+        file.read_to_end(&mut wasm_byte_code)?;
+
+        // compute hash
+        let code_hash = hash(&wasm_byte_code);
+
+        // push the message into queue
+        self.code_msgs.push(Message::StoreCode {
+            wasm_byte_code: wasm_byte_code.into(),
+        });
+
+        Ok(code_hash)
+    }
+
+    pub fn instantiate<M: Serialize>(
+        &mut self,
+        code_hash: Hash,
+        msg:       M,
+        salt:      Binary,
+        funds:     Coins,
+        admin:     AdminOption,
+    ) -> anyhow::Result<Addr> {
+        // derive the contract address
+        // note that for now we use an all-zero address as the message sender
+        // during genesis. this design may change.
+        let contract = Addr::compute(&Addr::mock(0), &code_hash, &salt);
+
+        // decide contract admin
+        let admin = match admin {
+            AdminOption::SetToAddr(addr) => Some(addr),
+            AdminOption::SetToSelf => Some(contract.clone()),
+            AdminOption::SetToNone => None,
+        };
+
+        // push the message into queue
+        self.other_msgs.push(Message::Instantiate {
+            code_hash,
+            msg: to_json(&msg)?,
+            salt,
+            funds,
+            admin,
+        });
+
+        Ok(contract)
+    }
+
+    pub fn execute<M: Serialize>(
+        &mut self,
+        contract: Addr,
+        msg:      M,
+        funds:    Coins,
+    ) -> anyhow::Result<()> {
+        self.other_msgs.push(Message::Execute {
+            contract,
+            msg: to_json(&msg)?,
+            funds,
+        });
+
+        Ok(())
+    }
+
+    pub fn set_config(&mut self, cfg: Config) -> anyhow::Result<()> {
+        ensure!(self.cfg.is_none(), "Config is set twice. Something is probably wrong in your workflow");
+
+        self.cfg = Some(cfg);
+
+        Ok(())
+    }
+
+    pub fn finalize(mut self) -> anyhow::Result<GenesisState> {
+        // a config must have been provided
+        let Some(config) = self.cfg.take() else {
+            bail!("Config is not set");
+        };
+
+        // ensure that store code messages are in front of all other msgs
+        let mut msgs = self.code_msgs;
+        msgs.extend(self.other_msgs);
+
+        Ok(GenesisState { config, msgs })
+    }
+}
