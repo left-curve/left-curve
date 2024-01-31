cb30cccb54ca1804cf856c29b6620d03fd4824dc
larry <26318510+larry0x@users.noreply.github.com>
2024-01-31 04:42:35 +0000

    add transaction and cw-account type definitions

--- sdk/ts/src/types.ts
+++ sdk/ts/src/types.ts
@@ -105,6 +105,65 @@ export type WasmSmartResponse = {
   data: string;
 };
 
+// ------------------------------------ tx -------------------------------------
+
+export type Tx = {
+  sender: string;
+  msgs: Message[];
+  credential: string;
+};
+
+export type Message = {
+  updateConfig?: MsgUpdateConfig;
+  transfer?: MsgTransfer;
+  storeCode?: MsgStoreCode;
+  instantiate?: MsgInstantiate;
+  execute?: MsgExecute;
+  migrate?: MsgMigrate;
+};
+
+export type MsgUpdateConfig = {
+  newCfg: Config;
+};
+
+export type MsgTransfer = {
+  to: string;
+  coins: Coin[];
+}
+
+export type MsgStoreCode = {
+  wasmByteCode: string;
+};
+
+export type MsgInstantiate = {
+  codeHash: string;
+  msg: string;
+  salt: string;
+  funds: Coin[];
+  admin?: string;
+};
+
+export type MsgExecute = {
+  contract: string;
+  msg: string;
+  funds: Coin[];
+};
+
+export type MsgMigrate = {
+  contract: string;
+  newCodeHash: string;
+  msg: string;
+};
+
+// -------------------------------- cw-account ---------------------------------
+
+export type PubKey = { secp256k1: string } | { secp256r1: string };
+
+export type AccountStateResponse = {
+  pubkey: PubKey;
+  sequence: number;
+};
+
 // -------------------------------- other types --------------------------------
 
 export type Config = {
