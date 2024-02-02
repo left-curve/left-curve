22b12d604e675ae0e6c6aebcb919e8df38d4499c
larry <26318510+larry0x@users.noreply.github.com>
2024-02-02 21:25:01 +0000

    add comment and test for Empty

--- crates/std/src/types/empty.rs
+++ crates/std/src/types/empty.rs
@@ -1,4 +1,20 @@
 use serde::{Deserialize, Serialize};
 
+/// When serializing to JSON, gives an pair of brackets: `{}`. Useful for use in
+/// contract messages when there isn't any intended inputs.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct Empty {}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::{from_json, to_json},
+    };
+
+    #[test]
+    fn serializing_empty() {
+        assert_eq!(to_json(&Empty {}).unwrap(), b"{}".to_vec().into());
+        assert_eq!(from_json::<Empty>(b"{}").unwrap(), Empty {});
+    }
+}
