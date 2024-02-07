a2ee496c5378caf46a49e02104bfc5c40e277a40
larry <26318510+larry0x@users.noreply.github.com>
2024-02-07 16:05:01 +0000

    fix bugs and add tests to bitarray

--- crates/jellyfish-merkle/src/bitarray.rs
+++ crates/jellyfish-merkle/src/bitarray.rs
@@ -1,23 +1,24 @@
 use cw_std::{Hash, StdError, StdResult};
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BitArray {
-    /// For our use case, the maximum size of the bitarray is 256 bits, so the
-    /// length can be represented by a u16 (2 bytes).
     pub(crate) num_bits: usize,
     /// We opt for the stack-allocated `[u8; N]` over heap-allocated `Vec<u8>`.
     /// In practice, the vast majority of node keys are not the full 256 bits,
     /// so this is a waste of memory space. Essentially, we trade memory usage
     /// for speed.
     /// For blockchain nodes in general, memory is cheap, while time is expensive.
-    pub(crate) bytes: [u8; Hash::LENGTH],
+    pub(crate) bytes: [u8; Self::MAX_BYTE_LENGTH],
 }
 
 impl BitArray {
+    pub const MAX_BIT_LENGTH:  usize = Self::MAX_BYTE_LENGTH * 8; // 256
+    pub const MAX_BYTE_LENGTH: usize = Hash::LENGTH;              // 32
+
     pub fn empty() -> Self {
         Self {
             num_bits: 0,
-            bytes: [0; Hash::LENGTH],
+            bytes: [0; Self::MAX_BYTE_LENGTH],
         }
     }
 
@@ -34,8 +35,8 @@ impl BitArray {
     }
 
     pub fn push(&mut self, bit: u8) {
+        debug_assert!(self.num_bits <= Self::MAX_BIT_LENGTH, "bitarray getting too long");
         debug_assert!(bit == 0 || bit == 1, "bit can only be 0 or 1, got {bit}");
-        self.num_bits += 1;
         let (quotient, remainder) = (self.num_bits / 8, self.num_bits % 8);
         let byte = &mut self.bytes[quotient];
         if bit == 1 {
@@ -47,42 +48,98 @@ impl BitArray {
             // for comparison, in C, `~` is bitwise NOT and `!` is logical NOT.
             *byte &= !(0b1 << (7 - remainder));
         }
+        self.num_bits += 1;
     }
 
     pub fn serialize(&self) -> Vec<u8> {
         let num_bytes = self.num_bits.div_ceil(8);
-        let mut bytes = Vec::with_capacity(2 + num_bytes);
-        bytes.extend(self.num_bits.to_be_bytes());
+        let mut bytes = Vec::with_capacity(1 + num_bytes);
+        // num_bits can be of value 256 at most, so (num_bits - 1) fits in a u8
+        bytes.extend(((self.num_bits - 1) as u8).to_be_bytes());
         bytes.extend(&self.bytes[..num_bytes]);
         bytes
     }
 
     pub fn deserialize(slice: &[u8]) -> StdResult<Self> {
         // the length of the bytes should be between 2 and 2+N (inclusive)
-        let range = 2..=(2 + Hash::LENGTH);
+        let range = 1..=(1 + Self::MAX_BYTE_LENGTH);
         if !range.contains(&slice.len()) {
             return Err(StdError::deserialize::<Self>(
                 format!("slice length must be in the range {range:?}, found {}", slice.len())
             ));
         }
 
-        let num_bits = u16::from_be_bytes(slice[..2].try_into()?) as usize;
+        // we subtracted 1 when serializng, so adding 1 back here
+        let num_bits = u8::from_be_bytes(slice[..1].try_into()?) as usize + 1;
 
-        let num_bytes = slice.len() - 2;
-        let mut bytes = [0; Hash::LENGTH];
-        bytes[..num_bytes].copy_from_slice(&slice[..num_bytes]);
+        // copy the bytes over
+        let num_bytes = slice.len() - 1;
+        let mut bytes = [0; Self::MAX_BYTE_LENGTH];
+        bytes[..num_bytes].copy_from_slice(&slice[1..=num_bytes]);
 
         Ok(BitArray { num_bits, bytes })
     }
 }
 
 impl<T: AsRef<[u8]>> From<T> for BitArray {
-    fn from(bytes: T) -> Self {
-        let bytes = bytes.as_ref();
-        assert!(bytes.len() < Hash::LENGTH);
+    fn from(slice: T) -> Self {
+        // the slice must be no longer than 32 bytes, otherwise panic
+        let slice = slice.as_ref();
+        let slice_len = slice.len();
+        assert!(slice_len <= Self::MAX_BYTE_LENGTH, "slice too long: {slice_len} > {}", Self::MAX_BYTE_LENGTH);
+
+        // copy the bytes over
+        let mut bytes = [0; Self::MAX_BYTE_LENGTH];
+        (&mut bytes[..slice_len]).copy_from_slice(slice);
+
         Self {
-            num_bits: bytes.len() * 8,
-            bytes: bytes.try_into().unwrap(),
+            num_bits: slice.len() * 8,
+            bytes,
         }
+    }
+}
+
+// ----------------------------------- tests -----------------------------------
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        rand::{thread_rng, Rng},
+    };
+
+    // TODO: convert to fuzz/proptest
+    #[test]
+    fn pushing_and_getting() {
+        let mut rng = thread_rng();
+        let mut bits = [0u8; BitArray::MAX_BIT_LENGTH];
+        let mut bitarray = BitArray::empty();
+
+        // put 256 bits into the array
+        for i in 0..BitArray::MAX_BIT_LENGTH {
+            let random_bit = if rng.gen_bool(0.5) { 1 } else { 0 };
+            bits[i] = random_bit;
+            bitarray.push(random_bit);
+        }
+
+        // now try to retrieve each bit
+        for i in 0..BitArray::MAX_BIT_LENGTH {
+            assert_eq!(bits[i], bitarray.bit_at_index(i));
+        }
+    }
+
+    // TODO: convert to fuzz/proptest
+    #[test]
+    fn serializing_and_deserializing() {
+        // generate random bytes of random length
+        let mut rng = thread_rng();
+        let mut bytes = [0u8; BitArray::MAX_BYTE_LENGTH];
+        let num_bytes = rng.gen_range(1..=BitArray::MAX_BYTE_LENGTH);
+        rng.fill(&mut bytes[..num_bytes]);
+
+        // serialize then deserialize; the original and the recovered must match
+        let bitarray = BitArray::from(&bytes[..num_bytes]);
+        let recovered = BitArray::deserialize(&bitarray.serialize()).unwrap();
+        assert_eq!(bitarray, recovered);
     }
 }
