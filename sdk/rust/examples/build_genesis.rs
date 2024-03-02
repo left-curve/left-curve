3a33ec17c1b73468682c1e68947eff31dd6f53ff
larry <26318510+larry0x@users.noreply.github.com>
2024-03-02 22:21:30 +0000

    update genesis builder example

--- sdk/rust/examples/build_genesis.rs
+++ sdk/rust/examples/build_genesis.rs
@@ -20,9 +20,10 @@ lazy_static! {
 const KEYSTORE_PASSWORD: &str = "123";
 
 fn main() -> anyhow::Result<()> {
-    // load two pubkeys from the keystore. we will register an account for each of them
+    // load a few pubkeys from the keystore. we will register an account for each of them
     let test1 = SigningKey::from_file(&KEYSTORE_DIR.join("test1.json"), KEYSTORE_PASSWORD)?;
     let test2 = SigningKey::from_file(&KEYSTORE_DIR.join("test2.json"), KEYSTORE_PASSWORD)?;
+    let test3 = SigningKey::from_file(&KEYSTORE_DIR.join("test3.json"), KEYSTORE_PASSWORD)?;
 
     // create the genesis builder
     let mut builder = GenesisBuilder::new();
@@ -46,28 +47,61 @@ fn main() -> anyhow::Result<()> {
     )?;
     let account2 = builder.register_account(
         account_factory.clone(),
-        account_code_hash,
+        account_code_hash.clone(),
         PublicKey::Secp256k1(test2.public_key().to_vec().into()),
+    )?;
+    let account3 = builder.register_account(
+        account_factory.clone(),
+        account_code_hash,
+        PublicKey::Secp256k1(test3.public_key().to_vec().into()),
     )?;
 
     // store and instantiate the bank contract
-    // give account1 some initial balances
+    // give each account some initial balances
     let bank = builder.store_code_and_instantiate(
         ARTIFACT_DIR.join("cw_bank-aarch64.wasm"),
         cw_bank::InstantiateMsg {
-            initial_balances: vec![Balance {
-                address: account1.clone(),
-                coins: Coins::from_vec_unchecked(vec![
-                    Coin {
-                        denom: "uatom".into(),
-                        amount: Uint128::new(12345),
-                    },
-                    Coin {
-                        denom: "uosmo".into(),
-                        amount: Uint128::new(23456),
-                    },
-                ]),
-            }],
+            initial_balances: vec![
+                Balance {
+                    address: account1.clone(),
+                    coins: Coins::from_vec_unchecked(vec![
+                        Coin {
+                            denom: "uatom".into(),
+                            amount: Uint128::new(20000),
+                        },
+                        Coin {
+                            denom: "uosmo".into(),
+                            amount: Uint128::new(20000),
+                        },
+                    ]),
+                },
+                Balance {
+                    address: account2.clone(),
+                    coins: Coins::from_vec_unchecked(vec![
+                        Coin {
+                            denom: "uatom".into(),
+                            amount: Uint128::new(20000),
+                        },
+                        Coin {
+                            denom: "uosmo".into(),
+                            amount: Uint128::new(20000),
+                        },
+                    ]),
+                },
+                Balance {
+                    address: account3.clone(),
+                    coins: Coins::from_vec_unchecked(vec![
+                        Coin {
+                            denom: "uatom".into(),
+                            amount: Uint128::new(20000),
+                        },
+                        Coin {
+                            denom: "uosmo".into(),
+                            amount: Uint128::new(20000),
+                        },
+                    ]),
+                },
+            ],
         },
         b"bank".to_vec().into(),
         AdminOption::SetToNone,
@@ -87,8 +121,8 @@ fn main() -> anyhow::Result<()> {
         bank:                   bank.clone(),
         begin_blockers:         vec![cron.clone()],
         end_blockers:           vec![cron.clone()],
-        store_code_permission:  Permission::Somebodies([account1.clone()].into()),
-        instantiate_permission: Permission::Somebodies([account1.clone(), account_factory.clone()].into()),
+        store_code_permission:  Permission::Somebodies([account1.clone(), account2.clone(), account3.clone()].into()),
+        instantiate_permission: Permission::Everybody,
     })?;
 
     // build the final genesis state and write to file
@@ -98,6 +132,7 @@ fn main() -> anyhow::Result<()> {
     println!("account-factory : {account_factory}");
     println!("account1        : {account1}");
     println!("account2        : {account2}");
+    println!("account3        : {account3}");
     println!("bank            : {bank}");
     println!("cron            : {cron}");
 
