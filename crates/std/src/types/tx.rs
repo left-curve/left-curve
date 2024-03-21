d080f53c5e04f7d120291945f9d49f8720778fa7
larry <26318510+larry0x@users.noreply.github.com>
2024-03-21 11:33:18 +0000

    update some comments

--- crates/std/src/types/tx.rs
+++ crates/std/src/types/tx.rs
@@ -15,16 +15,14 @@ pub struct Tx {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum Message {
-    /// Update the chain-level configurations. Only the `owner` can do this.
-    /// If `owner` is set to None, no one can update the config.
+    /// Update the chain-level configurations.
+    ///
+    /// Only the `owner` is authorized to do this. If the owner is set to `None`,
+    /// no one can update the config.
     SetConfig {
         new_cfg: Config,
     },
     /// Send coins to the given recipient address.
-    ///
-    /// Note that we don't assert the recipient is an account that exists, only
-    /// that it's a valid 32-byte hex string. The sender is reponsible to make
-    /// sure to put the correct address.
     Transfer {
         to: Addr,
         coins: Coins,
@@ -41,15 +39,16 @@ pub enum Message {
         funds: Coins,
         admin: Option<Addr>,
     },
-    /// Execute the contract.
+    /// Execute a contract.
     Execute {
         contract: Addr,
         msg: Json,
         funds: Coins,
     },
     /// Update the `code_hash` associated with a contract.
+    ///
     /// Only the contract's `admin` is authorized to do this. If the admin is
-    /// set to None, no one can update the code hash.
+    /// set to `None`, no one can update the code hash.
     Migrate {
         contract: Addr,
         new_code_hash: Hash,
