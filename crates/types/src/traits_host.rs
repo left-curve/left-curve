7c504188405ae89ccc5c36435cbfd1cf46fce47d
larry <26318510+larry0x@users.noreply.github.com>
2024-04-26 14:18:27 +0100

    add VM trait

--- crates/types/src/traits_host.rs
+++ crates/types/src/traits_host.rs
@@ -1,11 +1,16 @@
 use {
-    crate::{Batch, Hash, Storage},
+    crate::{
+        from_json_slice, to_json_vec, BankQueryMsg, BankQueryResponse, Batch, Context,
+        GenericResult, Hash, IbcClientUpdateMsg, IbcClientVerifyMsg, Json, Response, StdError,
+        Storage, SubMsgResult, TransferMsg, Tx,
+    },
+    borsh::{BorshDeserialize, BorshSerialize},
     serde::{de::DeserializeOwned, ser::Serialize},
 };
 
 // ------------------------------------ db -------------------------------------
 
-/// The API that a backing database must implement.
+/// Represents a database that our blockchain operates over.
 ///
 /// The database should follow [ADR-065](https://github.com/cosmos/cosmos-sdk/blob/main/docs/architecture/adr-065-store-v2.md).
 /// That is, it should contain two components:
@@ -84,6 +89,182 @@ pub trait Db {
 
 // ------------------------------------ vm -------------------------------------
 
-pub trait Vm {
-    type Error: ToString;
+/// Represents a virtual machine that can execute programs.
+pub trait Vm: Sized {
+    type Error: From<StdError> + ToString;
+
+    type Storage;
+    type Querier;
+
+    /// The type of programs intended to be run in this VM.
+    ///
+    /// It must be hashable and serializable with Borsh, so that it can be
+    /// stored in such a mapping: hash(program) => program.
+    type Program: std::hash::Hash + BorshSerialize + BorshDeserialize;
+
+    /// Create an instance of the VM given a storage, a querier, and a guest
+    /// program.
+    fn build_instance(
+        storage: Self::Storage,
+        querier: Self::Querier,
+        program: Self::Program,
+    ) -> Result<Self, Self::Error>;
+
+    /// Call a function that takes exactly 0 input parameter (other than the
+    /// context) and returns exactly 1 output.
+    fn call_in_0_out_1(&mut self, name: &str, ctx: &Context) -> Result<Vec<u8>, Self::Error>;
+
+    /// Call a function that takes exactly 1 input parameter (other than the
+    /// context) and returns exactly 1 output.
+    fn call_in_1_out_1(
+        &mut self,
+        name: &str,
+        ctx: &Context,
+        param1: impl AsRef<[u8]>,
+    ) -> Result<Vec<u8>, Self::Error>;
+
+    /// Call a function that takes exactly 2 input parameters (other than the
+    /// context) and returns exactly 1 output.
+    fn call_in_2_out_1(
+        &mut self,
+        name: &str,
+        ctx: &Context,
+        param1: impl AsRef<[u8]>,
+        param2: impl AsRef<[u8]>,
+    ) -> Result<Vec<u8>, Self::Error>;
+
+    fn call_instantiate(
+        &mut self,
+        ctx: &Context,
+        msg: &Json,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("instantiate", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_execute(
+        &mut self,
+        ctx: &Context,
+        msg: &Json,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("execute", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_query(
+        &mut self,
+        ctx: &Context,
+        msg: &Json,
+    ) -> Result<GenericResult<Json>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("query", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_migrate(
+        &mut self,
+        ctx: &Context,
+        msg: &Json,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("migrate", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_reply(
+        &mut self,
+        ctx: &Context,
+        msg: &Json,
+        events: &SubMsgResult,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_2_out_1(
+            "reply",
+            ctx,
+            to_json_vec(msg)?,
+            to_json_vec(events)?,
+        )?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_receive(&mut self, ctx: &Context) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_0_out_1("receive", ctx)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_before_block(&mut self, ctx: &Context) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_0_out_1("before_block", ctx)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_after_block(&mut self, ctx: &Context) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_0_out_1("after_block", ctx)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_before_tx(
+        &mut self,
+        ctx: &Context,
+        tx: &Tx,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("before_tx", ctx, to_json_vec(tx)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_after_tx(
+        &mut self,
+        ctx: &Context,
+        tx: &Tx,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("after_tx", ctx, to_json_vec(tx)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_bank_transfer(
+        &mut self,
+        ctx: &Context,
+        msg: &TransferMsg,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("bank_transfer", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_bank_query(
+        &mut self,
+        ctx: &Context,
+        msg: &BankQueryMsg,
+    ) -> Result<GenericResult<BankQueryResponse>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("bank_query", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_ibc_client_create(
+        &mut self,
+        ctx: &Context,
+        client_state: &Json,
+        consensus_state: &Json,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_2_out_1(
+            "ibc_client_create",
+            ctx,
+            to_json_vec(client_state)?,
+            to_json_vec(consensus_state)?,
+        )?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_ibc_client_update(
+        &mut self,
+        ctx: &Context,
+        msg: &IbcClientUpdateMsg,
+    ) -> Result<GenericResult<Response>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("ibc_client_update", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
+
+    fn call_ibc_client_verify(
+        &mut self,
+        ctx: &Context,
+        msg: &IbcClientVerifyMsg,
+    ) -> Result<GenericResult<()>, Self::Error> {
+        let res_bytes = self.call_in_1_out_1("ibc_client_verify", ctx, to_json_vec(msg)?)?;
+        Ok(from_json_slice(res_bytes)?)
+    }
 }
