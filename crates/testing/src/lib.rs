1f1dbd9ea7ab1b2143182555e7e1f5a3a978ef7f
larry <26318510+larry0x@users.noreply.github.com>
2024-05-11 01:58:53 +0100

    MVP of testing framework

---
+++ crates/testing/src/lib.rs
@@ -0,0 +1,125 @@
+use {
+    cw_app::App,
+    cw_db_memory::MemDb,
+    cw_types::{BlockInfo, GenesisState, Hash, QueryRequest, QueryResponse, Timestamp, Uint64},
+    cw_vm_rust::RustVm,
+    std::time::{SystemTime, UNIX_EPOCH},
+};
+
+fn current_time() -> Timestamp {
+    let nanos = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("failed to get current system time")
+        .as_nanos()
+        .try_into()
+        .expect("current system time overflows u64");
+    Timestamp::from_nanos(nanos)
+}
+
+pub struct MockApp {
+    inner: App<MemDb, RustVm>,
+}
+
+impl MockApp {
+    pub fn new() -> Self {
+        Self {
+            inner: App::new(MemDb::new()),
+        }
+    }
+
+    pub fn init_chain(&mut self, chain_id: impl ToString, genesis_state: GenesisState) {
+        let block = BlockInfo {
+            height: Uint64::new(0), // genesis height is always zero
+            timestamp: current_time(),
+            hash: Hash::ZERO,
+        };
+        self.inner.do_init_chain(chain_id.to_string(), block, genesis_state).unwrap();
+    }
+
+    pub fn query(&self, req: QueryRequest) -> QueryResponse {
+        self.inner.do_query_app(req, 0, false).unwrap()
+    }
+}
+
+// ----------------------------------- tests -----------------------------------
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        cw_types::{
+            hash, to_borsh_vec, to_json_value, Addr, Coins, Config, Empty, Message, Permission,
+            Permissions, Response, StdResult,
+        },
+        cw_vm_rust::{ContractWrapper, ExecuteFn, MigrateFn, QueryFn, ReceiveFn, ReplyFn},
+        cw_wasm::MutableCtx,
+        std::collections::BTreeSet,
+    };
+
+    fn bank_instantiate(_ctx: MutableCtx, _msg: Empty) -> StdResult<Response> {
+        Ok(Response::new().add_attribute("action", "bank_instantiate"))
+    }
+
+    #[test]
+    fn init_chain_works() {
+        let mut app = MockApp::new();
+        let bank_contract = ContractWrapper::new(
+            Box::new(bank_instantiate),
+            None::<ExecuteFn>,
+            None::<MigrateFn>,
+            None::<ReceiveFn>,
+            None::<ReplyFn>,
+            None::<QueryFn>,
+        );
+        let bank_code = to_borsh_vec(&bank_contract).unwrap();
+        let bank_code_hash = hash(&bank_code);
+        let genesis_state = GenesisState {
+            config: Config {
+                owner: None,
+                bank: Addr::mock(1),
+                begin_blockers: vec![],
+                end_blockers: vec![],
+                permissions: Permissions {
+                    upload: Permission::Everybody,
+                    instantiate: Permission::Everybody,
+                    create_client: Permission::Everybody,
+                    create_connection: Permission::Everybody,
+                    create_channel: Permission::Everybody,
+                },
+                allowed_clients: BTreeSet::new(),
+            },
+            msgs: vec![
+                Message::Upload {
+                    code: bank_code.into(),
+                },
+                Message::Instantiate {
+                    code_hash: bank_code_hash,
+                    msg: to_json_value(&Empty {}).unwrap(),
+                    salt: b"bank".to_vec().into(),
+                    funds: Coins::new_empty(),
+                    admin: None,
+                },
+            ],
+        };
+        app.init_chain("dev-1", genesis_state);
+
+        let info = app.query(QueryRequest::Info {}).as_info();
+        dbg!(&info);
+
+        let code_hashes = app
+            .query(QueryRequest::Codes {
+                start_after: None,
+                limit: None,
+            })
+            .as_codes();
+        dbg!(&code_hashes);
+
+        let accounts = app
+            .query(QueryRequest::Accounts {
+                start_after: None,
+                limit: None,
+            })
+            .as_accounts();
+        dbg!(&accounts);
+    }
+}
