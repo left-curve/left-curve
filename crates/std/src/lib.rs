5fe71bb8e6ef89284fff8f1e307edcfd1a3de822
larry <26318510+larry0x@users.noreply.github.com>
2024-06-01 15:06:32 +0100

    improve some comments

--- crates/std/src/lib.rs
+++ crates/std/src/lib.rs
@@ -1,9 +1,11 @@
-// Grug is a "meta crate", meaning it doesn't contain any content itself, but
+// This is a "meta crate", meaning it doesn't contain any content itself, but
 // rather just re-export contents from other crates. The objective is that
-// contract developers only needs to add one dependency that has everything they
-// need.
+// contract developers only needs to add one single dependency that has
+// everything they need.
 pub use {grug_macros::*, grug_storage::*, grug_types::*, grug_wasm::*};
 
+// The testing crate must be excluded if the target is Wasm, because it contains
+// Wasm-incompatible operators, e.g. in `MockApi` which uses RNGs.
 #[cfg(not(target_arch = "wasm32"))]
 pub use grug_testing::*;
 
