5b53577a278976c6e29d835006de73539771597b
larry <26318510+larry0x@users.noreply.github.com>
2024-06-15 21:32:31 +0100

    delete truncated blake2b_512

--- crates/crypto/src/hashers.rs
+++ crates/crypto/src/hashers.rs
@@ -42,10 +42,6 @@ pub fn blake2b_512(message: &[u8]) -> [u8; 64] {
     Blake2b512::digest(message).into()
 }
 
-pub fn blake2b_512_truncated(message: &[u8]) -> [u8; 32] {
-    truncate(&Blake2b512::digest(message)).unwrap()
-}
-
 pub fn blake3(message: &[u8]) -> [u8; 32] {
     blake3::hash(message).into()
 }
@@ -64,15 +60,14 @@ mod tests {
         };
     }
 
-    hash_test!(sha2_256,              "food" => "c1f026582fe6e8cb620d0c85a72fe421ddded756662a8ec00ed4c297ad10676b");
-    hash_test!(sha2_512,              "food" => "c235548cfe84fc87678ff04c9134e060cdcd7512d09ed726192151a995541ed8db9fda5204e72e7ac268214c322c17787c70530513c59faede52b7dd9ce64331");
-    hash_test!(sha2_512_truncated,    "food" => "c235548cfe84fc87678ff04c9134e060cdcd7512d09ed726192151a995541ed8");
-    hash_test!(keccak_256,            "food" => "a471c7c90860799b1facb54795f0a93d821fb727241025770865602471b765a8");
-    hash_test!(sha3_256,              "food" => "c6ab604b6867239fce3071722ab1ca096063d0164e730dfc557d1f3f0fcead6b");
-    hash_test!(sha3_512,              "food" => "8cd7cab3e1e542c16c56d91e105f48145313557c34dab00014b5ed56151eb78f96d58948646579904192e9c88d6577d74a69702de7d52f519e31dad1cef3115d");
-    hash_test!(sha3_512_truncated,    "food" => "8cd7cab3e1e542c16c56d91e105f48145313557c34dab00014b5ed56151eb78f");
-    hash_test!(blake2s_256,           "food" => "5a1ec796f11f3dfc7e8ca5de13828edf2e910eb7dd41caaac356a4acbefb1758");
-    hash_test!(blake2b_512,           "food" => "b1f115361afc179415d93d4f58dc2fc7d8fa434192d7cb9b65fca592f6aa904103d1f12b28655c2355478e10908ab002c418dc52a4367d8e645309cd25e3a504");
-    hash_test!(blake2b_512_truncated, "food" => "b1f115361afc179415d93d4f58dc2fc7d8fa434192d7cb9b65fca592f6aa9041");
-    hash_test!(blake3,                "food" => "f775a8ccf8cb78cd1c63ade4e9802de4ead836b36cea35242accf31d2c6a3697");
+    hash_test!(sha2_256,           "food" => "c1f026582fe6e8cb620d0c85a72fe421ddded756662a8ec00ed4c297ad10676b");
+    hash_test!(sha2_512,           "food" => "c235548cfe84fc87678ff04c9134e060cdcd7512d09ed726192151a995541ed8db9fda5204e72e7ac268214c322c17787c70530513c59faede52b7dd9ce64331");
+    hash_test!(sha2_512_truncated, "food" => "c235548cfe84fc87678ff04c9134e060cdcd7512d09ed726192151a995541ed8");
+    hash_test!(keccak_256,         "food" => "a471c7c90860799b1facb54795f0a93d821fb727241025770865602471b765a8");
+    hash_test!(sha3_256,           "food" => "c6ab604b6867239fce3071722ab1ca096063d0164e730dfc557d1f3f0fcead6b");
+    hash_test!(sha3_512,           "food" => "8cd7cab3e1e542c16c56d91e105f48145313557c34dab00014b5ed56151eb78f96d58948646579904192e9c88d6577d74a69702de7d52f519e31dad1cef3115d");
+    hash_test!(sha3_512_truncated, "food" => "8cd7cab3e1e542c16c56d91e105f48145313557c34dab00014b5ed56151eb78f");
+    hash_test!(blake2s_256,        "food" => "5a1ec796f11f3dfc7e8ca5de13828edf2e910eb7dd41caaac356a4acbefb1758");
+    hash_test!(blake2b_512,        "food" => "b1f115361afc179415d93d4f58dc2fc7d8fa434192d7cb9b65fca592f6aa904103d1f12b28655c2355478e10908ab002c418dc52a4367d8e645309cd25e3a504");
+    hash_test!(blake3,             "food" => "f775a8ccf8cb78cd1c63ade4e9802de4ead836b36cea35242accf31d2c6a3697");
 }
