232aea63d7fa51d472c988fb7f5d849b554d6878
larry <26318510+larry0x@users.noreply.github.com>
2024-06-26 16:59:47 +0100

    updates

--- crates/vm/wasm/tests/vm.rs
+++ crates/vm/wasm/tests/vm.rs
@@ -10,24 +10,29 @@ use {
         QueryResponse, Tx, Uint64, GENESIS_SENDER,
     },
     grug_vm_wasm::WasmVm,
-    std::{collections::BTreeSet, vec},
-    utils::{init_balances, read_wasm_file},
+    std::{
+        collections::{BTreeMap, BTreeSet},
+        fs, io, vec,
+    },
 };
 
-mod utils;
+const CHAIN_ID: &str = "grug-1";
+const ARTIFACTS_DIR: &str = "../../../artifacts";
 
-const CHAIN_ID: &str = "grug";
-
-struct AppVM {
-    inner: App<MemDb, WasmVm>,
-    block_info: BlockInfo,
+fn read_wasm_file(filename: &str) -> io::Result<Vec<u8>> {
+    fs::read(&format!("{ARTIFACTS_DIR}/{filename}"))
 }
 
-impl AppVM {
+struct TestSuite {
+    app: App<MemDb, WasmVm>,
+    last_finalized_block: BlockInfo,
+}
+
+impl TestSuite {
     pub fn new() -> Self {
         Self {
-            inner: App::new(MemDb::new()),
-            block_info: BlockInfo {
+            app: App::new(MemDb::new()),
+            last_finalized_block: BlockInfo {
                 height: Uint64::default(),
                 timestamp: current_time(),
                 hash: Hash::ZERO,
@@ -35,16 +40,17 @@ impl AppVM {
         }
     }
 
-    pub fn init_chain(&mut self, genesis_state: GenesisState) {
-        self.inner
-            .do_init_chain(CHAIN_ID.to_string(), self.block_info.clone(), genesis_state)
-            .unwrap();
+    pub fn init_chain(&mut self, genesis_state: GenesisState) -> AppResult<Hash> {
+        self.app.do_init_chain(
+            CHAIN_ID.to_string(),
+            self.last_finalized_block.clone(),
+            genesis_state,
+        )
     }
 
-    pub fn query(&self, req: QueryRequest) -> QueryResponse {
-        self.inner
-            .do_query_app(req, (self.block_info.height).into(), false)
-            .unwrap()
+    pub fn query(&self, req: QueryRequest) -> AppResult<QueryResponse> {
+        self.app
+            .do_query_app(req, self.last_finalized_block.height.into(), false)
     }
 
     #[allow(clippy::type_complexity)]
@@ -56,34 +62,35 @@ impl AppVM {
         let tx = Tx {
             sender: sender.clone(),
             msgs,
-            credential: Binary::default(),
+            credential: Binary::empty(),
         };
 
-        self.block_info.height += Uint64::ONE;
+        self.last_finalized_block.height += Uint64::ONE;
 
-        self.inner
-            .do_finalize_block(self.block_info.clone(), vec![(Hash::ZERO, tx)])
+        self.app
+            .do_finalize_block(self.last_finalized_block.clone(), vec![(Hash::ZERO, tx)])
     }
 }
 
 #[test]
-fn test() {
-    let mut app = AppVM::new();
+fn test() -> anyhow::Result<()> {
+    let mut suite = TestSuite::new();
 
     let sender = Addr::mock(2);
 
-    let initial_balances =
-        init_balances(vec![(&sender, vec![Coin::new("ugrug", 100_u128)])]).unwrap();
+    let initial_balances = BTreeMap::from([(
+        sender.clone(),
+        Coins::try_from(vec![Coin::new("ugrug", 100_u128)]).unwrap(),
+    )]);
 
-    let bank_code = to_borsh_vec(&read_wasm_file("grug_bank-aarch64").unwrap()).unwrap();
+    let bank_code = to_borsh_vec(&read_wasm_file("grug_bank-aarch64.wasm").unwrap()).unwrap();
     let bank_code_hash = Hash::from_slice(sha2_256(&bank_code));
-
-    let predicted_bank = Addr::compute(&GENESIS_SENDER, &bank_code_hash, &b"bank".to_vec().into());
+    let bank = Addr::compute(&GENESIS_SENDER, &bank_code_hash, &b"bank".to_vec().into());
 
     let genesis_state = GenesisState {
         config: Config {
             owner: None,
-            bank: predicted_bank,
+            bank,
             begin_blockers: vec![],
             end_blockers: vec![],
             permissions: Permissions {
@@ -109,12 +116,12 @@ fn test() {
         ],
     };
 
-    app.init_chain(genesis_state);
+    suite.init_chain(genesis_state)?;
 
-    let res = app.query(QueryRequest::Balance {
+    let res = suite.query(QueryRequest::Balance {
         address: sender.clone(),
         denom: "ugrug".to_string(),
-    });
+    })?;
 
     // THIS IS NOT PASSING
     assert_eq!(res, QueryResponse::Balance(Coin::new("ugrug", 100_u128)));
@@ -122,16 +129,18 @@ fn test() {
     // Send to mock_addr 3
     // let receiver = Addr::mock(3);
 
-    // app.execute(&sender, vec![Message::Transfer {
+    // suite.execute(&sender, vec![Message::Transfer {
     //     to: receiver.clone(),
     //     coins: vec![Coin::new("ugrug", 50_u128)].try_into().unwrap(),
     // }])
     // .unwrap();
 
-    // let res = app.query(QueryRequest::Balance {
+    // let res = suite.query(QueryRequest::Balance {
     //     address: receiver.clone(),
     //     denom: "ugurg".to_string(),
     // });
 
     // assert_eq!(res, QueryResponse::Balance(Coin::new("ugrug", 50_u128)));
+
+    Ok(())
 }
