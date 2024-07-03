3558cd3cb41aed78b7a4f74f7f6cd7f6ca693bb9
larry <26318510+larry0x@users.noreply.github.com>
2024-07-03 22:48:39 +0100

    simplify the tester contract

--- contracts/testers/immutable-state/src/lib.rs
+++ contracts/testers/immutable-state/src/lib.rs
@@ -1,89 +1,36 @@
-use {
-    grug::{
-        from_borsh_slice, from_json_slice, grug_derive, grug_export, to_json_vec,
-        unwrap_into_generic_result, Addr, Coins, Context, ExternalApi, ExternalQuerier,
-        ExternalStorage, GenericResult, Json, MutableCtx, QuerierWrapper, Region, Response,
-        StdResult,
-    },
-    serde::de::DeserializeOwned,
-};
-
-#[grug_derive(serde)]
-pub struct InstantiateMsg;
-
-#[grug_derive(serde)]
-pub struct ExecuteMsg;
-
-#[grug_derive(serde)]
-pub struct QueryMsg;
+use grug::{grug_export, Empty, MutableCtx, Region, Response, StdResult};
 
 #[grug_export]
-pub fn instantiate(_ctx: MutableCtx, _msg: InstantiateMsg) -> StdResult<Response> {
-    Ok(Response::default())
+pub fn instantiate(_ctx: MutableCtx, _msg: Empty) -> StdResult<Response> {
+    Ok(Response::new())
 }
 
 #[grug_export]
-pub fn execute(ctx: MutableCtx, _msg: ExecuteMsg) -> StdResult<Response> {
-    ctx.querier.query_wasm_smart(ctx.contract, &QueryMsg)?;
-    Ok(Response::default())
+pub fn execute(ctx: MutableCtx, _msg: Empty) -> StdResult<Response> {
+    // Call the contract's own `query` function.
+    ctx.querier.query_wasm_smart(ctx.contract, &Empty {})?;
+
+    Ok(Response::new())
 }
 
-#[cfg(target_arch = "wasm32")]
-mod __wasm_export_query {
-    #[no_mangle]
-    extern "C" fn query(ptr0: usize, ptr1: usize) -> usize {
-        {
-            super::_do_query(&super::query, ptr0, ptr1)
-        }
+extern "C" {
+    fn db_write(key_ptr: usize, value_ptr: usize);
+}
+
+#[no_mangle]
+extern "C" fn query(_ctx_ptr: usize, _msg_ptr: usize) -> usize {
+    let key = b"larry";
+    let key_region = Region::build(key);
+    let key_ptr = &*key_region as *const Region;
+
+    let value = b"engineer";
+    let value_region = Region::build(value);
+    let value_ptr = &*value_region as *const Region;
+
+    // This should fail!
+    unsafe {
+        db_write(key_ptr as usize, value_ptr as usize);
     }
-}
-pub fn query(ctx: MutableCtx, _msg: QueryMsg) -> StdResult<Json> {
-    ctx.storage.write(b"key", b"value");
-    Ok(Json::default())
-}
 
-fn _do_query<M, E>(
-    query_fn: &dyn Fn(MutableCtx, M) -> Result<Json, E>,
-    ctx_ptr: usize,
-    msg_ptr: usize,
-) -> usize
-where
-    M: DeserializeOwned,
-    E: ToString,
-{
-    let ctx_bytes = unsafe { Region::consume(ctx_ptr as *mut Region) };
-    let msg_bytes = unsafe { Region::consume(msg_ptr as *mut Region) };
-
-    let res = __do_query(query_fn, &ctx_bytes, &msg_bytes);
-    let res_bytes = to_json_vec(&res).unwrap();
-
-    Region::release_buffer(res_bytes) as usize
-}
-
-fn __do_query<M, E>(
-    query_fn: &dyn Fn(MutableCtx, M) -> Result<Json, E>,
-    ctx_bytes: &[u8],
-    msg_bytes: &[u8],
-) -> GenericResult<Json>
-where
-    M: DeserializeOwned,
-    E: ToString,
-{
-    let ctx: Context = unwrap_into_generic_result!(from_borsh_slice(ctx_bytes));
-
-    // Fake mutable context
-    let mutalbe_ctx = MutableCtx {
-        storage: &mut ExternalStorage,
-        api: &ExternalApi,
-        querier: QuerierWrapper::new(&ExternalQuerier),
-        chain_id: ctx.chain_id,
-        block: ctx.block,
-        contract: ctx.contract,
-        sender: Addr::mock(1),
-        funds: Coins::default(),
-    };
-
-    let msg = unwrap_into_generic_result!(from_json_slice(msg_bytes));
-
-    query_fn(mutalbe_ctx, msg).into()
+    0
 }
