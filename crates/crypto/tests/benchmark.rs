39f5fa6d770688c5eff22a1ac4929e28d4575b94
Rhaki <Giacomo.encicliati@gmail.com>
2024-07-04 19:59:14 +0200

    add benchmark benchmark_ed25519_verify_batch

--- crates/crypto/tests/benchmark.rs
+++ crates/crypto/tests/benchmark.rs
@@ -1,8 +1,8 @@
 use {
     ed25519_dalek::Signer,
     grug_crypto::{
-        ed25519_verify, secp256k1_pubkey_recover, secp256k1_verify, secp256r1_verify, sha2_256,
-        Identity256,
+        ed25519_batch_verify, ed25519_verify, secp256k1_pubkey_recover, secp256k1_verify,
+        secp256r1_verify, sha2_256, Identity256,
     },
     p256::ecdsa::signature::DigestSigner,
     rand::{rngs::OsRng, RngCore},
@@ -10,8 +10,16 @@ use {
     test_case::test_case,
 };
 
+fn gen_msg(i: usize) -> Vec<u8> {
+    let mut vec = vec![0; i];
+    OsRng.fill_bytes(&mut vec);
+    vec
+}
+
 // cargo test --release --package grug-crypto --test benchmark -- benchmark --show-output
-#[test_case(|msg: &[u8]| -> Duration {
+
+#[test_case(|msg: usize| -> Duration {
+    let msg = &gen_msg(msg);
     let sk = k256::ecdsa::SigningKey::random(&mut OsRng);
     let vk = k256::ecdsa::VerifyingKey::from(&sk);
     let msg = Identity256::from(sha2_256(msg));
@@ -20,7 +28,8 @@ use {
     secp256k1_verify(msg.as_bytes(), &sig.to_bytes(), &vk.to_sec1_bytes()).unwrap();
     now.elapsed()};
 "benchmark_secp256k1_verify")]
-#[test_case(|msg: &[u8]| -> Duration {
+#[test_case(|msg: usize| -> Duration {
+    let msg = &gen_msg(msg);
     let sk = k256::ecdsa::SigningKey::random(&mut OsRng);
     let msg = Identity256::from(sha2_256(msg));
     let (sig, recovery_id) = sk.sign_digest_recoverable(msg.clone()).unwrap();
@@ -34,7 +43,8 @@ use {
     .unwrap();
     now.elapsed()};
 "benchmark_secp256k1_pubkey_recover")]
-#[test_case(|msg: &[u8]| -> Duration {
+#[test_case(|msg: usize| -> Duration {
+    let msg = &gen_msg(msg);
     let sk = p256::ecdsa::SigningKey::random(&mut OsRng);
     let vk = p256::ecdsa::VerifyingKey::from(&sk);
     let msg = Identity256::from(sha2_256(msg));
@@ -43,7 +53,8 @@ use {
     secp256r1_verify(msg.as_bytes(), &sig.to_bytes(), &vk.to_sec1_bytes()).unwrap();
     now.elapsed()};
 "benchmark_secp256r1_verify")]
-#[test_case(|msg: &[u8]| -> Duration {
+#[test_case(|msg: usize| -> Duration {
+    let msg = &gen_msg(msg);
     let sk = ed25519_dalek::SigningKey::generate(&mut OsRng);
     let vk = ed25519_dalek::VerifyingKey::from(&sk);
     let msg = sha2_256(msg);
@@ -52,7 +63,7 @@ use {
     ed25519_verify(&msg, &sig.to_bytes(), vk.as_bytes()).unwrap();
     now.elapsed()};
 "benchmark_ed25519_verify")]
-fn benchmark<FN: Fn(&[u8]) -> Duration>(clos: FN) {
+fn benchmark<FN: Fn(usize) -> Duration>(clos: FN) {
     let mut tot_time = Duration::new(0, 0);
     let mut sum_log_time = 0.0;
     let iter = 100u32;
@@ -61,7 +72,7 @@ fn benchmark<FN: Fn(&[u8]) -> Duration>(clos: FN) {
         let i = i * 10;
         let mut vec = vec![0; i as usize];
         OsRng.fill_bytes(&mut vec);
-        let time = clos(&vec);
+        let time = clos(i as usize);
         tot_time += time;
         sum_log_time += (time.as_nanos() as f64).ln();
     }
@@ -71,5 +82,86 @@ fn benchmark<FN: Fn(&[u8]) -> Duration>(clos: FN) {
     println!(
         "Arithmetic mean: {:?} - Geometric mean: {:?} - iterations: {}",
         ari_mean, geo_mean, iter
+    );
+}
+
+// cargo test --release --package grug-crypto --test benchmark -- benchmark_ed25519_verify_batch --show-output
+
+#[test]
+fn benchmark_ed25519_verify_batch() {
+    let clos = |i: usize| {
+        let mut msgs: Vec<Vec<u8>> = vec![];
+        let mut sigs: Vec<Vec<u8>> = vec![];
+        let mut vks: Vec<Vec<u8>> = vec![];
+
+        for _ in i..1 + 1 {
+            let sk = ed25519_dalek::SigningKey::generate(&mut OsRng);
+            let vk = ed25519_dalek::VerifyingKey::from(&sk);
+            let msg = sha2_256(&gen_msg(i));
+            let sig = sk.sign(&msg);
+            msgs.push(msg.to_vec());
+            sigs.push(sig.to_bytes().to_vec());
+            vks.push(vk.to_bytes().to_vec());
+        }
+
+        let msgs = msgs.iter().map(|x| x.as_slice()).collect::<Vec<_>>();
+        let sigs = sigs.iter().map(|x| x.as_slice()).collect::<Vec<_>>();
+        let vks = vks.iter().map(|x| x.as_slice()).collect::<Vec<_>>();
+
+        let now = std::time::Instant::now();
+        ed25519_batch_verify(&msgs, &sigs, &vks).unwrap();
+        now.elapsed()
+    };
+
+    let mut tot_time = Duration::new(0, 0);
+    let mut sum_log_time = 0.0;
+    let iter = 100u32;
+
+    let mut last_iter: Option<Duration> = None;
+    let mut tot_base = Duration::new(0, 0);
+    let mut tot_per_item = Duration::new(0, 0);
+    let mut linear_counter = 0;
+    for i in 1..iter + 1 {
+        // Why not
+        let i = i * 100;
+        let mut vec = vec![0; i as usize];
+        OsRng.fill_bytes(&mut vec);
+        let time = clos(i as usize);
+        tot_time += time;
+        sum_log_time += (time.as_nanos() as f64).ln();
+
+        if let Some(pre_time) = &mut last_iter {
+            let dif = match time.checked_sub(*pre_time) {
+                Some(dif) => dif / 100,
+                None => {
+                    *pre_time = time;
+                    continue;
+                },
+            };
+
+            let items = dif * i;
+            let base = time.checked_sub(items).unwrap_or_default();
+            tot_base += base;
+            tot_per_item += items / i;
+            linear_counter += 1;
+            *pre_time = time;
+        } else {
+            last_iter = Some(time);
+        }
+    }
+    let ari_mean = tot_time / (iter);
+    let geo_mean = (sum_log_time / (iter) as f64).exp();
+    let geo_mean = Duration::from_nanos(geo_mean as u64);
+
+    let avg_linear = tot_base / linear_counter;
+    let avg_non_linear = tot_per_item / linear_counter;
+    println!(
+        "Arithmetic mean: {:?} - Geometric mean: {:?} - iterations: {}",
+        ari_mean, geo_mean, iter
+    );
+
+    println!(
+        "Arithmetic mean base: {:?} - Arithmetic mean per_item: {:?} - valid_iterations: {}",
+        avg_linear, avg_non_linear, linear_counter
     );
 }
