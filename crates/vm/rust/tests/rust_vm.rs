1be8779025e407c401010f210ed69c0c2bb03483
larry <26318510+larry0x@users.noreply.github.com>
2024-07-05 16:38:53 +0100

    add bank transfer test to rustvm

---
+++ crates/vm/rust/tests/rust_vm.rs
@@ -0,0 +1,51 @@
+use {
+    grug_testing::TestBuilder,
+    grug_types::{Coin, Coins, Message, Uint128},
+};
+
+const DENOM: &str = "ugrug";
+
+#[test]
+fn bank_transfers() -> anyhow::Result<()> {
+    let (mut suite, accounts) = TestBuilder::new()
+        .add_account("sender", Coins::new_one(DENOM, 100_u128))?
+        .add_account("receiver", Coins::new_empty())?
+        .build()?;
+
+    // Check that sender has been given 100 ugrug
+    suite
+        .query_balance(&accounts["sender"], DENOM)
+        .should_succeed_and_equal(Uint128::new(100))?;
+
+    // Sender sends 70 ugrug to the receiver across multiple messages
+    suite
+        .execute_messages(&accounts["sender"], 2_500_000, vec![
+            Message::Transfer {
+                to: accounts["receiver"].address.clone(),
+                coins: vec![Coin::new(DENOM, 10_u128)].try_into().unwrap(),
+            },
+            Message::Transfer {
+                to: accounts["receiver"].address.clone(),
+                coins: vec![Coin::new(DENOM, 15_u128)].try_into().unwrap(),
+            },
+            Message::Transfer {
+                to: accounts["receiver"].address.clone(),
+                coins: vec![Coin::new(DENOM, 20_u128)].try_into().unwrap(),
+            },
+            Message::Transfer {
+                to: accounts["receiver"].address.clone(),
+                coins: vec![Coin::new(DENOM, 25_u128)].try_into().unwrap(),
+            },
+        ])?
+        .should_succeed()?;
+
+    // Check balances again
+    suite
+        .query_balance(&accounts["sender"], DENOM)
+        .should_succeed_and_equal(Uint128::new(30))?;
+    suite
+        .query_balance(&accounts["receiver"], DENOM)
+        .should_succeed_and_equal(Uint128::new(70))?;
+
+    Ok(())
+}
