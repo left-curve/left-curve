b8c39a878e579fc7866425740e102562dab41a1d
larry <26318510+larry0x@users.noreply.github.com>
2024-07-10 00:49:54 +0200

    update constants

--- crates/crypto/benches/benchmarks.rs
+++ crates/crypto/benches/benchmarks.rs
@@ -15,25 +15,39 @@ use {
 };
 
 struct Settings {
-    iter: usize,
-    mul_iter: usize,
     warmup_time: Duration,
     measurement_time: Duration,
 }
 
 const HASH_SETTINGS: Settings = Settings {
-    iter: 30,
-    mul_iter: 10_000,
     warmup_time: Duration::from_millis(100),
     measurement_time: Duration::from_millis(100),
 };
 
 const CRYPTO_SETTINGS: Settings = Settings {
-    iter: 10,
-    mul_iter: 20,
     warmup_time: Duration::from_millis(2_000),
     measurement_time: Duration::from_millis(2_000),
 };
+
+/// Lengths of messages for benchmarking hashers.
+///
+/// In Grug contracts, the largest data that may need to hashed are transactions
+/// that contain `Message::Upload` messages. Contract size are usually a few
+/// hundred kilobytes, so here we choose messages of up to 1 MiB.
+const HASH_MSG_LENS: [usize; 5] = [200_000, 400_000, 600_000, 800_000, 1_000_000];
+
+/// Messages to be signed for benchmarking verifiers.
+///
+/// This length doesn't matter, because verifiers only concern hashes, so we
+/// just pick a small number.
+const SIGN_MSG_LEN: usize = 10;
+
+/// Batch sizes for benchmarking `ed25519_batch_verify`.
+///
+/// The most common situation this function is called is in the ICS-07 Tendermint
+/// light client, where it verifies block headers. Cosmos chains usually have up
+/// to 150 validators, so we choose a number of batch sizes up to that.
+const ED25519_BATCH_SIZES: [usize; 6] = [25, 50, 75, 100, 125, 150];
 
 fn generate_random_msg(i: usize) -> Vec<u8> {
     let mut vec = vec![0; i];
@@ -48,7 +62,7 @@ fn bench_hashers(c: &mut Criterion) {
     group.warm_up_time(HASH_SETTINGS.warmup_time);
     group.measurement_time(HASH_SETTINGS.measurement_time);
 
-    for size in (1..=HASH_SETTINGS.iter).map(|size| size * HASH_SETTINGS.mul_iter) {
+    for size in HASH_MSG_LENS {
         group.bench_with_input(BenchmarkId::new("sha2_256", size), &size, |b, size| {
             b.iter_batched(
                 || generate_random_msg(*size),
@@ -118,8 +132,6 @@ fn bench_hashers(c: &mut Criterion) {
 }
 
 fn bench_verifiers(c: &mut Criterion) {
-    const MSG_LEN: usize = 100;
-
     let mut group = c.benchmark_group("verifiers");
 
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Linear));
@@ -129,7 +141,7 @@ fn bench_verifiers(c: &mut Criterion) {
     group.bench_function("secp256r1_verify", |b| {
         b.iter_batched(
             || {
-                let msg = generate_random_msg(MSG_LEN);
+                let msg = generate_random_msg(SIGN_MSG_LEN);
                 let msg_hash = Identity256::from(sha2_256(&msg));
                 let sk = p256::ecdsa::SigningKey::random(&mut OsRng);
                 let vk = p256::ecdsa::VerifyingKey::from(&sk);
@@ -156,7 +168,7 @@ fn bench_verifiers(c: &mut Criterion) {
     group.bench_function("secp256k1_verify", |b| {
         b.iter_batched(
             || {
-                let msg = generate_random_msg(MSG_LEN);
+                let msg = generate_random_msg(SIGN_MSG_LEN);
                 let msg_hash = Identity256::from(sha2_256(&msg));
                 let sk = k256::ecdsa::SigningKey::random(&mut OsRng);
                 let vk = k256::ecdsa::VerifyingKey::from(&sk);
@@ -181,7 +193,7 @@ fn bench_verifiers(c: &mut Criterion) {
     group.bench_function("secp256k1_pubkey_recover", |b| {
         b.iter_batched(
             || {
-                let msg = generate_random_msg(MSG_LEN);
+                let msg = generate_random_msg(SIGN_MSG_LEN);
                 let msg_hash = Identity256::from(sha2_256(&msg));
                 let sk = k256::ecdsa::SigningKey::random(&mut OsRng);
                 let vk = k256::ecdsa::VerifyingKey::from(&sk);
@@ -208,7 +220,7 @@ fn bench_verifiers(c: &mut Criterion) {
     group.bench_function("ed25519_verify", |b| {
         b.iter_batched(
             || {
-                let msg = generate_random_msg(MSG_LEN);
+                let msg = generate_random_msg(SIGN_MSG_LEN);
                 let msg_hash = Identity512::from(sha2_512(&msg));
                 let sk = ed25519_dalek::SigningKey::generate(&mut OsRng);
                 let vk = ed25519_dalek::VerifyingKey::from(&sk);
@@ -227,38 +239,44 @@ fn bench_verifiers(c: &mut Criterion) {
         );
     });
 
-    group.bench_function("ed25519_batch_verify", |b| {
-        b.iter_batched(
-            || {
-                let mut prehash_msgs = vec![];
-                let mut sigs = vec![];
-                let mut vks = vec![];
+    for size in ED25519_BATCH_SIZES {
+        group.bench_with_input(
+            BenchmarkId::new("ed25519_batch_verify", size),
+            &size,
+            |b, size| {
+                b.iter_batched(
+                    || {
+                        let mut prehash_msgs = vec![];
+                        let mut sigs = vec![];
+                        let mut vks = vec![];
 
-                // We use a batch size of 100, because the typical use case of
-                // `ed25519_batch_verify` is in Tendermint light clients, while
-                // the average size of validator sets of Cosmos chains is ~100.
-                for _ in 0..100 {
-                    let prehash_msg = generate_random_msg(MSG_LEN);
-                    let sk = ed25519_dalek::SigningKey::generate(&mut OsRng);
-                    let vk = ed25519_dalek::VerifyingKey::from(&sk);
-                    let sig = sk.sign(&prehash_msg);
+                        // We use a batch size of 100, because the typical use case of
+                        // `ed25519_batch_verify` is in Tendermint light clients, while
+                        // the average size of validator sets of Cosmos chains is ~100.
+                        for _ in 0..*size {
+                            let prehash_msg = generate_random_msg(SIGN_MSG_LEN);
+                            let sk = ed25519_dalek::SigningKey::generate(&mut OsRng);
+                            let vk = ed25519_dalek::VerifyingKey::from(&sk);
+                            let sig = sk.sign(&prehash_msg);
 
-                    prehash_msgs.push(prehash_msg);
-                    sigs.push(sig.to_bytes().to_vec());
-                    vks.push(vk.to_bytes().to_vec());
-                }
+                            prehash_msgs.push(prehash_msg);
+                            sigs.push(sig.to_bytes().to_vec());
+                            vks.push(vk.to_bytes().to_vec());
+                        }
 
-                (prehash_msgs, sigs, vks)
+                        (prehash_msgs, sigs, vks)
+                    },
+                    |(msgs, sigs, vks)| {
+                        let msgs: Vec<_> = msgs.iter().map(|m| m.as_slice()).collect();
+                        let sigs: Vec<_> = sigs.iter().map(|s| s.as_slice()).collect();
+                        let vks: Vec<_> = vks.iter().map(|k| k.as_slice()).collect();
+                        assert!(ed25519_batch_verify(&msgs, &sigs, &vks).is_ok());
+                    },
+                    BatchSize::SmallInput,
+                );
             },
-            |(prehash_msgs, sigs, vks)| {
-                let prehash_msgs: Vec<_> = prehash_msgs.iter().map(|m| m.as_slice()).collect();
-                let sigs: Vec<_> = sigs.iter().map(|s| s.as_slice()).collect();
-                let vks: Vec<_> = vks.iter().map(|k| k.as_slice()).collect();
-                assert!(ed25519_batch_verify(&prehash_msgs, &sigs, &vks).is_ok());
-            },
-            BatchSize::SmallInput,
         );
-    });
+    }
 
     group.finish();
 }
