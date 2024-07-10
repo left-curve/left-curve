537d7cd5f6f265332446e0b2566272279a38d31e
larry <26318510+larry0x@users.noreply.github.com>
2024-07-10 17:16:59 +0200

    add gas costs for storage IO

--- crates/vm/wasm/src/gas.rs
+++ crates/vm/wasm/src/gas.rs
@@ -3,12 +3,31 @@
 /// TODO: We can put this in the chain's `Config`, such that this can be changed
 /// via a governance proposal, without having to fork the chain.
 pub const GAS_COSTS: GasCosts = GasCosts {
+    // Storage.
+    //
+    // For storage, we take the values from Cosmos SDK:
+    // https://github.com/cosmos/cosmos-sdk/blob/v0.50.7/store/types/gas.go#L232-L242
+    //
+    // Following the conversion:
+    // - 1 Cosmos SDK gas = 100 CosmWasm gas
+    // - 170 CosmWasm gas = 1 Wasmer point
+    // - 1 Wasmer point = 1 Grug gas
+    // This means: 1 Cosmos SDK gas = 0.588 Grug gas
+    storage_read: LinearGasCost::new(588, 2),
+    storage_scan: 588,
+    storage_next: 18,
+    storage_write: LinearGasCost::new(1176, 18),
+    storage_remove: 588,
+    // Verifiers
+    //
+    // For batch verification, there's a flat setup cost, and a cost per signature.
     secp256r1_verify_cost: 1_880_000,
     secp256k1_verify_cost: 770_000,
     secp256k1_pubkey_recover: 1_580_000,
     ed25519_verify_cost: 410_000,
-    // For batch verification, there's a flat setup cost, and a cost per signature.
     ed25519_batch_verify_cost: LinearGasCost::new(1_340_000, 188_000),
+    // Hashers.
+    //
     // For hashers, `per_item` means per byte.
     // The truncated versions have the same cost as the untruncated counterparts.
     sha2_256: LinearGasCost::new(0, 27_265),
@@ -24,11 +43,19 @@ pub const GAS_COSTS: GasCosts = GasCosts {
 };
 
 pub struct GasCosts {
+    // Storage
+    pub storage_read: LinearGasCost,
+    pub storage_scan: u64,
+    pub storage_next: u64,
+    pub storage_write: LinearGasCost,
+    pub storage_remove: u64,
+    // Signature verifiers
     pub secp256r1_verify_cost: u64,
     pub secp256k1_verify_cost: u64,
     pub secp256k1_pubkey_recover: u64,
     pub ed25519_verify_cost: u64,
     pub ed25519_batch_verify_cost: LinearGasCost,
+    // Hashers
     pub sha2_256: LinearGasCost,
     pub sha2_512: LinearGasCost,
     pub sha2_512_truncated: LinearGasCost,
