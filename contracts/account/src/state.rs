32c9c27a1a64bd184e4d14da70dd098f8a5da002
Rhaki <Giacomo.encicliati@gmail.com>
2024-07-10 19:41:21 +0200

    add a comment

--- contracts/account/src/state.rs
+++ contracts/account/src/state.rs
@@ -10,9 +10,10 @@ pub const PUBLIC_KEY: Item<PublicKey> = Item::new("pk");
 /// The account's sequence number, also known as "nonce" in Ethereum.
 pub const SEQUENCE: Counter<u32> = Counter::new("seq");
 
-/// Unordered transactions that have been included in a block.
+/// Unordered transactions with expiration in timestamp that have been included in a block.
 pub const UNORDERED_TXS_TIMESTAMP: Set<(u128, &Hash)> = Set::new("unordered_txs_timestamp");
 
+/// Unordered transactions with expiration in height that have been included in a block.
 pub const UNORDERED_TXS_HEIGHT: Set<(u64, &Hash)> = Set::new("unordered_txs_height");
 
 /// json key for account data in tx data
