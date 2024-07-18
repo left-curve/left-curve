4871c74f39a272e7f344e1426d4737558b21b4b6
larry <26318510+larry0x@users.noreply.github.com>
2024-07-18 18:26:53 +0100

    add raw methods to Set

--- crates/storage/src/set.rs
+++ crates/storage/src/set.rs
@@ -1,7 +1,7 @@
 use {
     crate::{Borsh, Bound, Key, PathBuf, Prefix, PrefixBound, Prefixer},
     grug_types::{Empty, Order, StdResult, Storage},
-    std::marker::PhantomData,
+    std::{borrow::Cow, marker::PhantomData},
 };
 
 /// Mimic the behavior of HashSet or BTreeSet.
@@ -28,10 +28,14 @@ impl<'a, T> Set<'a, T>
 where
     T: Key,
 {
-    fn path(&self, item: T) -> PathBuf<Empty> {
+    fn path_raw(&self, key_raw: &[u8]) -> PathBuf<Empty, Borsh> {
+        PathBuf::new(self.namespace, &[], Some(&Cow::Borrowed(key_raw)))
+    }
+
+    fn path(&self, item: T) -> PathBuf<Empty, Borsh> {
         let mut raw_keys = item.raw_keys();
         let last_raw_key = raw_keys.pop();
-        PathBuf::<Empty, Borsh>::new(self.namespace, &raw_keys, last_raw_key.as_ref())
+        PathBuf::new(self.namespace, &raw_keys, last_raw_key.as_ref())
     }
 
     fn no_prefix(&self) -> Prefix<T, Empty> {
@@ -50,12 +54,30 @@ where
 
     // ---------------------- methods for single entries -----------------------
 
+    pub fn has_raw(&self, storage: &dyn Storage, item_raw: &[u8]) -> bool {
+        self.path_raw(item_raw).as_path().exists(storage)
+    }
+
     pub fn has(&self, storage: &dyn Storage, item: T) -> bool {
         self.path(item).as_path().exists(storage)
     }
 
+    /// Using this function is not recommended. If the item isn't properly
+    /// serialized, later when you read the data, it will fail to deserialize
+    /// and error.
+    ///
+    /// We prefix the function name with the word "unsafe" to highlight this.
+    pub fn unsafe_insert_raw(&self, storage: &mut dyn Storage, item_raw: &[u8]) {
+        // `Empty` serializes to empty bytes when using borsh.
+        self.path_raw(item_raw).as_path().save_raw(storage, &[])
+    }
+
     pub fn insert(&self, storage: &mut dyn Storage, item: T) -> StdResult<()> {
         self.path(item).as_path().save(storage, &Empty {})
+    }
+
+    pub fn remove_raw(&self, storage: &mut dyn Storage, item_raw: &[u8]) {
+        self.path_raw(item_raw).as_path().remove(storage)
     }
 
     pub fn remove(&self, storage: &mut dyn Storage, item: T) {
