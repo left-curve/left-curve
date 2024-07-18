028337f2d837c0c3aec931350e334090fab984cf
larry <26318510+larry0x@users.noreply.github.com>
2024-07-18 23:33:35 +0100

    excluded grug_ffi from non-wasm targets

--- crates/std/src/lib.rs
+++ crates/std/src/lib.rs
@@ -2,14 +2,18 @@
 // rather just re-export contents from other crates. The objective is that
 // contract developers only needs to add one single dependency that has
 // everything they need.
-pub use {grug_ffi::*, grug_macros::*, grug_storage::*, grug_types::*};
+pub use {grug_macros::*, grug_storage::*, grug_types::*};
 
-// The testing crate must be excluded if the target is Wasm, because it contains
-// Wasm-incompatible operators, e.g. in `MockApi` which uses RNGs.
+// The FFI crate is only included when building for WebAssembly.
+#[cfg(target_arch = "wasm32")]
+pub use grug_ffi::*;
+
+// The testing crate is only included when _not_ building for WebAseembly.
+// It contains Wasm-incompatible operators, e.g. in `MockApi` which uses RNGs.
 #[cfg(not(target_arch = "wasm32"))]
 pub use grug_testing::*;
 
-// dependencies used by the procedural macros
+// Dependencies used by the procedural macros
 #[doc(hidden)]
 pub mod __private {
     pub use {::borsh, ::serde, ::serde_with};
