577d2c9964111d7d5e659b5d221693601af800e8
larry <26318510+larry0x@users.noreply.github.com>
2024-07-28 00:43:48 +0100

    update test suite

--- crates/testing/src/builder.rs
+++ crates/testing/src/builder.rs
@@ -5,8 +5,8 @@ use {
     grug_app::AppError,
     grug_types::{
         hash, Addr, Binary, BlockInfo, Coins, Config, Duration, GenesisState, Hash, Message,
-        Permission, Permissions, Timestamp, GENESIS_BLOCK_HASH, GENESIS_BLOCK_HEIGHT,
-        GENESIS_SENDER,
+        NumberConst, Permission, Permissions, Timestamp, Udec128, Uint128, GENESIS_BLOCK_HASH,
+        GENESIS_BLOCK_HEIGHT, GENESIS_SENDER,
     },
     grug_vm_rust::RustVm,
     std::{
@@ -20,6 +20,9 @@ const DEFAULT_TRACING_LEVEL: Level = Level::DEBUG;
 const DEFAULT_CHAIN_ID: &str = "dev-1";
 const DEFAULT_BLOCK_TIME: Duration = Duration::from_millis(250);
 const DEFAULT_BANK_SALT: &[u8] = b"bank";
+const DEFAULT_TAXMAN_SALT: &[u8] = b"taxman";
+const DEFAULT_FEE_DENOM: &str = "ugrug";
+const DEFAULT_FEE_RATE: Udec128 = Udec128::checked_from_ratio(Uint128::ONE, Uint128::TEN).unwrap(); // 0.1 ugrug per gas
 
 pub struct TestBuilder<VM = RustVm>
 where
@@ -38,6 +41,10 @@ where
     bank_code: Binary,
     bank_code_hash: Hash,
     balances: BTreeMap<Addr, Coins>,
+    taxman_code: Binary,
+    taxman_code_hash: Hash,
+    fee_denom: Option<String>,
+    fee_rate: Option<Udec128>,
 }
 
 // Clippy incorrectly thinks we can derive `Default` here, which we can't.
@@ -60,6 +67,9 @@ where
         let bank_code = VM::default_bank_code();
         let bank_code_hash = hash(&bank_code);
 
+        let taxman_code = VM::default_taxman_code();
+        let taxman_code_hash = hash(&taxman_code);
+
         Self {
             vm,
             tracing_level: Some(DEFAULT_TRACING_LEVEL),
@@ -73,6 +83,10 @@ where
             bank_code,
             bank_code_hash,
             balances: BTreeMap::new(),
+            taxman_code,
+            taxman_code_hash,
+            fee_denom: None,
+            fee_rate: None,
         }
     }
 
@@ -82,7 +96,10 @@ where
         self
     }
 
-    pub fn set_chain_id(mut self, chain_id: impl ToString) -> Self {
+    pub fn set_chain_id<T>(mut self, chain_id: T) -> Self
+    where
+        T: ToString,
+    {
         self.chain_id = Some(chain_id.to_string());
         self
     }
@@ -106,6 +123,19 @@ where
         self.owner = Some(owner.address.clone());
 
         Ok(self)
+    }
+
+    pub fn set_fee_denom<T>(mut self, fee_denom: T) -> Self
+    where
+        T: ToString,
+    {
+        self.fee_denom = Some(fee_denom.to_string());
+        self
+    }
+
+    pub fn set_fee_rate(mut self, fee_rate: Udec128) -> Self {
+        self.fee_rate = Some(fee_rate);
+        self
     }
 
     pub fn add_account<C>(mut self, name: &'static str, balances: C) -> anyhow::Result<Self>
@@ -154,16 +184,32 @@ where
             timestamp: genesis_time,
         };
 
-        // Upload account and bank codes, instantiate bank contract.
+        // Upload account, bank, and taxman codes,
+        // instantiate bank and taxman contracts.
         let mut msgs = vec![
             Message::upload(self.account_code),
             Message::upload(self.bank_code),
+            Message::upload(self.taxman_code),
             Message::instantiate(
                 self.bank_code_hash.clone(),
                 &grug_bank::InstantiateMsg {
                     initial_balances: self.balances,
                 },
                 DEFAULT_BANK_SALT,
+                Coins::new(),
+                None,
+            )?,
+            Message::instantiate(
+                &self.taxman_code_hash.clone(),
+                &grug_taxman::InstantiateMsg {
+                    config: grug_taxman::Config {
+                        fee_denom: self
+                            .fee_denom
+                            .unwrap_or_else(|| DEFAULT_FEE_DENOM.to_string()),
+                        fee_rate: self.fee_rate.unwrap_or(DEFAULT_FEE_RATE),
+                    },
+                },
+                DEFAULT_TAXMAN_SALT,
                 Coins::new(),
                 None,
             )?,
@@ -184,9 +230,11 @@ where
 
         // Create the app config
         let bank = Addr::compute(&GENESIS_SENDER, &self.bank_code_hash, DEFAULT_BANK_SALT);
+        let taxman = Addr::compute(&GENESIS_SENDER, &self.taxman_code_hash, DEFAULT_TAXMAN_SALT);
         let config = Config {
             owner: self.owner,
             bank,
+            taxman,
             cronjobs: BTreeMap::new(),
             permissions: Permissions {
                 upload: Permission::Everybody,
