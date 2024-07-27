76efdbf8a878a6cecd5d8663e975269570c2294c
larry <26318510+larry0x@users.noreply.github.com>
2024-07-28 00:35:15 +0100

    update rustvm

--- crates/vm/rust/src/traits.rs
+++ crates/vm/rust/src/traits.rs
@@ -5,8 +5,7 @@
 use {
     crate::VmResult,
     grug_types::{
-        Api, AuthCtx, BankMsg, BankQuery, BankQueryResponse, Context, GenericResult, ImmutableCtx,
-        Json, MutableCtx, Querier, Response, Storage, SubMsgResult, SudoCtx, Tx,
+        Api, AuthCtx, BankMsg, BankQuery, BankQueryResponse, Context, GenericResult, ImmutableCtx, Json, MutableCtx, Outcome, Querier, Response, Storage, SubMsgResult, SudoCtx, Tx
     },
 };
 
@@ -101,6 +100,25 @@ pub trait Contract {
         msg: BankQuery,
     ) -> VmResult<GenericResult<BankQueryResponse>>;
 
+    fn withhold_fee(
+        &self,
+        ctx: Context,
+        storage: &mut dyn Storage,
+        api: &dyn Api,
+        querier: &dyn Querier,
+        tx: Tx,
+    ) -> VmResult<GenericResult<Response>>;
+
+    fn finalize_fee(
+        &self,
+        ctx: Context,
+        storage: &mut dyn Storage,
+        api: &dyn Api,
+        querier: &dyn Querier,
+        tx: Tx,
+        outcome: Outcome,
+    ) -> VmResult<GenericResult<Response>>;
+
     fn cron_execute(
         &self,
         ctx: Context,
@@ -133,5 +151,9 @@ pub type AfterTxFn<E> = Box<dyn Fn(AuthCtx, Tx) -> Result<Response, E> + Send + 
 pub type BankExecuteFn<E> = Box<dyn Fn(SudoCtx, BankMsg) -> Result<Response, E> + Send + Sync>;
 
 pub type BankQueryFn<E> = Box<dyn Fn(ImmutableCtx, BankQuery) -> Result<BankQueryResponse, E> + Send + Sync>;
+
+pub type WithholdFeeFn<E> = Box<dyn Fn(SudoCtx, Tx) -> Result<Response, E> + Send + Sync>;
+
+pub type FinalizeFeeFn<E> = Box<dyn Fn(SudoCtx, Tx, Outcome) -> Result<Response, E> + Send + Sync>;
 
 pub type CronExecuteFn<E> = Box<dyn Fn(SudoCtx) -> Result<Response, E> + Send + Sync>;
