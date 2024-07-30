ef7537aa96e3e4e22e772830f79ee8dd548257c9
larry <26318510+larry0x@users.noreply.github.com>
2024-07-31 00:53:04 +0100

    do not re-export bnum types

--- crates/types/src/math.rs
+++ crates/types/src/math.rs
@@ -3,10 +3,4 @@ mod traits;
 mod udec;
 mod uint;
 
-pub use {
-    bnum::types::{I256, I512, U256, U512},
-    signed::*,
-    traits::*,
-    udec::*,
-    uint::*,
-};
+pub use {signed::*, traits::*, udec::*, uint::*};
