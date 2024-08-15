ad8624a1c8b176cff905c0d960b73cf2dc72e8f1
larry <26318510+larry0x@users.noreply.github.com>
2024-08-15 23:08:10 +0100

    counters (plural)

--- crates/storage/src/counter.rs
+++ crates/storage/src/counter.rs
@@ -1,13 +1,13 @@
 use {
-    crate::{Borsh, Codec, Item},
+    crate::{Borsh, Codec, Item, Key, Map},
     grug_types::{Number, StdResult, Storage},
 };
 
-/// An abstraction over `Item`. Stores a single number that is monotonically
-/// incremented one unit at a time.
+/// A single number that is monotonically incremented by the given step size.
+///
+/// In internally, this is an abstraction over an [`Item`](crate::Item).
 pub struct Counter<'a, T, C = Borsh>
 where
-    T: Number + Copy,
     C: Codec<T>,
 {
     item: Item<'a, T, C>,
@@ -35,13 +35,63 @@ where
             .map(|maybe_value| maybe_value.unwrap_or(self.base))
     }
 
-    /// Increment the value stored by one unit; return the values before and
-    /// after incrementing.
+    /// Increment the value by the step size; return the values before and after
+    /// incrementing.
     pub fn increment(&self, storage: &mut dyn Storage) -> StdResult<(T, T)> {
         let old_value = self.current(storage)?;
         let new_value = old_value.checked_add(self.step)?;
 
         self.item.save(storage, &new_value)?;
+
+        Ok((old_value, new_value))
+    }
+}
+
+/// A single number under each key, that is monotonically incremented by the
+/// given step size.
+///
+/// Internally, this is an abstraction over a [`Map`](crate::Map).
+pub struct Counters<'a, K, T, C = Borsh>
+where
+    C: Codec<T>,
+{
+    map: Map<'a, K, T, C>,
+    base: T,
+    step: T,
+}
+
+impl<'a, K, T, C> Counters<'a, K, T, C>
+where
+    C: Codec<T>,
+{
+    pub const fn new(storage_key: &'a str, base: T, step: T) -> Self {
+        Self {
+            map: Map::new(storage_key),
+            base,
+            step,
+        }
+    }
+}
+impl<'a, K, T, C> Counters<'a, K, T, C>
+where
+    K: Key + Copy,
+    T: Number + Copy,
+    C: Codec<T>,
+{
+    /// Load the current counter value under the given key.
+    pub fn current(&self, storage: &dyn Storage, key: K) -> StdResult<T> {
+        self.map
+            .may_load(storage, key)
+            .map(|maybe_value| maybe_value.unwrap_or(self.base))
+    }
+
+    /// Increment the value under the given key by the step size; return the
+    /// values before and after incrementing.
+    pub fn increment(&self, storage: &mut dyn Storage, key: K) -> StdResult<(T, T)> {
+        let old_value = self.current(storage, key)?;
+        let new_value = old_value.checked_add(self.step)?;
+
+        self.map.save(storage, key, &new_value)?;
 
         Ok((old_value, new_value))
     }
