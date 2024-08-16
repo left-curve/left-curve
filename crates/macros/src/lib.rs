47ed5b3b8824adbcd175f887a4a480d0865217a6
Rhaki <giacomo.encicliati@gmail.com>
2024-08-16 18:31:35 +0200

    fix

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,8 +1,7 @@
 use {
     proc_macro::TokenStream,
-    proc_macro2::Span,
     quote::quote,
-    std::str::FromStr,
+    std::{panic, str::FromStr},
     syn::{
         parse::{Parse, ParseStream},
         parse_macro_input, Data, DeriveInput, Ident, ItemFn, ItemStruct, Token,
@@ -140,11 +139,52 @@ pub fn export(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
     item
 }
 
+macro_rules! get_ident {
+    ($ty:ident, $s:expr, $worng_token_tree_err:expr) => {{
+        if let proc_macro2::TokenTree::$ty(ty) = $s.expect($worng_token_tree_err) {
+            ty
+        } else {
+            panic!($worng_token_tree_err);
+        }
+    }};
+}
+
+/// Implement the trait `IndexList` for a Index Struct:
+/// ```rust ignore
+/// #[grug::derive(borsh)]
+/// pub struct Test {
+///    pub foo: String,
+///    pub bar: u64,
+/// }
+///
+/// #[grug::index_list(Test, u64)]
+/// pub struct TestIndexes<'a> {
+///    pub foo: MultiIndex<'a, u64, String, Test, Borsh>,
+///    pub bar: UniqueIndex<'a, u64, Test, Borsh>,
+/// }
+/// ```
 #[proc_macro_attribute]
-pub fn index_list(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn index_list(attrs: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemStruct);
 
-    let ty = Ident::new(&attr.to_string(), Span::call_site());
+    let mut attrs = proc_macro2::TokenStream::from(attrs).into_iter();
+
+    let ty = get_ident!(
+        Ident,
+        attrs.next(),
+        "Main struct type is missing, expecting the following format: `#[index_list(Struct, PK)]"
+    );
+    // Check if the next token is a comma
+    get_ident!(
+        Punct,
+        attrs.next(),
+        "Comma is missing on: expecting the following format: `#[index_list(Struct, PK)]`"
+    );
+
+    // Transform the remaining attributes into a TokenStream.
+    // This will interpret the remaining attributes as the primary key.
+    let pk = proc_macro2::TokenStream::from_iter(attrs);
+
     let struct_ty = input.ident.clone();
 
     let names = input
@@ -160,9 +200,9 @@ pub fn index_list(attr: TokenStream, item: TokenStream) -> TokenStream {
     let expanded = quote! {
         #input
 
-        impl grug::IndexList<#ty> for #struct_ty<'_> {
-            fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn grug::Index<#ty>> + '_> {
-                let v: Vec<&dyn grug::Index<#ty>> = vec![#(#names),*];
+        impl grug::IndexList<#pk, #ty> for #struct_ty<'_> {
+            fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn grug::Index<#pk, #ty>> + '_> {
+                let v: Vec<&dyn grug::Index<#pk, #ty>> = vec![#(#names),*];
                 Box::new(v.into_iter())
             }
         }
