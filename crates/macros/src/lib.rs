dc2b0094a02f8c4a9a47a5c710db09701d43585c
larry <26318510+larry0x@users.noreply.github.com>
2024-08-16 18:59:51 +0100

    update comment

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -16,20 +16,33 @@ pub fn export(_attr: TokenStream, input: TokenStream) -> TokenStream {
 
 /// Implement the `IndexList` trait for an index struct.
 ///
+/// The macro takes exactly two attributes: the indexed map's primary key and
+/// data type, respectively. Each can be a tuple of arbitrarily many elements.
+///
 /// ## Example
 ///
+/// The following creates an `IndexedMap` with the primary key type `u32` and
+/// data type `Test`.
+///
 /// ```rust ignore
+/// use grug::{IndexedMap, MultiIndex, UniqueIndex};
+///
 /// #[grug::derive(borsh)]
 /// pub struct Test {
-///    pub foo: String,
-///    pub bar: u64,
+///     pub foo: String,
+///     pub bar: u64,
 /// }
 ///
-/// #[grug::index_list(Test, u64)]
+/// #[grug::index_list(u32, Test)]
 /// pub struct TestIndexes<'a> {
-///    pub foo: MultiIndex<'a, u64, String, Test, Borsh>,
-///    pub bar: UniqueIndex<'a, u64, Test, Borsh>,
+///     pub foo: MultiIndex<'a, u32, String, Test>,
+///     pub bar: UniqueIndex<'a, u32, u64, Test>,
 /// }
+///
+/// const TEST: IndexedMap<u64, String, TestIndexes> = IndexedMap::new("test", TestIndexes {
+///     foo: MultiIndex::new(|_, test| test.foo.clone(), "test", "test__foo"),
+///     bar: UniqueIndex::new(|_, test| test.bar, "test", "test__bar"),
+/// });
 /// ```
 #[proc_macro_attribute]
 pub fn index_list(attr: TokenStream, input: TokenStream) -> TokenStream {
