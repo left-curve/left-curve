dd581d77c0a744feeccf2426b19cc6575b762224
larry <26318510+larry0x@users.noreply.github.com>
2024-08-16 18:54:12 +0100

    use syn v2 attr parsing

--- crates/macros/src/index_list.rs
+++ crates/macros/src/index_list.rs
@@ -1,40 +1,34 @@
 use {
     proc_macro::TokenStream,
     quote::quote,
-    std::panic,
-    syn::{parse_macro_input, ItemStruct},
+    syn::{
+        parse::{Parse, ParseStream},
+        parse_macro_input,
+        token::Comma,
+        Expr, ItemStruct,
+    },
 };
 
-macro_rules! get_ident {
-    ($ty:ident, $s:expr, $worng_token_tree_err:expr) => {{
-        if let proc_macro2::TokenTree::$ty(ty) = $s.expect($worng_token_tree_err) {
-            ty
-        } else {
-            panic!($worng_token_tree_err);
-        }
-    }};
+struct Args {
+    /// Type of the `IndexedMap`'s primary key.
+    pk: Expr,
+    /// Type of the `IndexedMap`'s value.
+    ty: Expr,
+}
+
+impl Parse for Args {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let pk = input.parse()?;
+        input.parse::<Comma>()?;
+        let ty = input.parse()?;
+
+        Ok(Self { pk, ty })
+    }
 }
 
 pub fn process(attrs: TokenStream, item: TokenStream) -> TokenStream {
+    let Args { pk, ty } = parse_macro_input!(attrs as Args);
     let input = parse_macro_input!(item as ItemStruct);
-
-    let mut attrs = proc_macro2::TokenStream::from(attrs).into_iter();
-
-    let ty = get_ident!(
-        Ident,
-        attrs.next(),
-        "Main struct type is missing, expecting the following format: `#[index_list(Struct, PK)]"
-    );
-    // Check if the next token is a comma
-    get_ident!(
-        Punct,
-        attrs.next(),
-        "Comma is missing on: expecting the following format: `#[index_list(Struct, PK)]`"
-    );
-
-    // Transform the remaining attributes into a TokenStream.
-    // This will interpret the remaining attributes as the primary key.
-    let pk = proc_macro2::TokenStream::from_iter(attrs);
 
     let struct_ty = input.ident.clone();
 
