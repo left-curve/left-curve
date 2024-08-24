eac3ac1929ec3d2e1d45747cdf37e86f1636a70f
Rhaki <giacomo.encicliati@gmail.com>
2024-08-24 12:20:32 +0200

    init

--- crates/storage/src/counter.rs
+++ crates/storage/src/counter.rs
@@ -96,3 +96,53 @@ where
         Ok((old_value, new_value))
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use {
+        super::Counter,
+        borsh::{BorshDeserialize, BorshSerialize},
+        grug_types::{Dec128, Int128, MockStorage, Number, NumberConst, Uint128, Uint512},
+        std::{fmt::Debug, ops::Sub, str::FromStr},
+        test_case::test_case,
+    };
+
+    #[test_case(0_u8, 1_u8; "u8")]
+    #[test_case(Uint128::ZERO, Uint128::TEN; "uint128")]
+    #[test_case(Uint512::ONE, Uint512::ONE; "uint512")]
+    #[test_case(Int128::new_negative(Uint128::ONE), Int128::new_negative(Uint128::ONE); "int128")]
+    #[test_case(Dec128::from_str("0.5").unwrap(), Dec128::from_str("1.5").unwrap(); "dec128")]
+    fn counter<T>(base: T, increment: T)
+    where
+        T: BorshSerialize
+            + BorshDeserialize
+            + NumberConst
+            + Number
+            + PartialEq
+            + Debug
+            + Copy
+            + Sub<Output = T>,
+    {
+        let mut storage = MockStorage::new();
+        let counter = Counter::<T>::new("counter", base, increment);
+
+        let mut asserter = base;
+
+        assert_eq!(counter.current(&storage).unwrap(), asserter);
+
+        asserter = asserter.checked_add(increment).unwrap();
+        assert_eq!(
+            counter.increment(&mut storage).unwrap(),
+            (asserter - increment, asserter)
+        );
+        assert_eq!(counter.current(&storage).unwrap(), asserter);
+
+        asserter = asserter.checked_add(increment).unwrap();
+        assert_eq!(
+            counter.increment(&mut storage).unwrap(),
+            (asserter - increment, asserter)
+        );
+        assert_eq!(counter.current(&storage).unwrap(), asserter);
+    }
+}
