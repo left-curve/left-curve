2bb106a4f51002586588d9ef503f18cd4e93c42c
Rhaki <giacomo.encicliati@gmail.com>
2024-08-24 12:21:56 +0200

    init

--- crates/storage/src/item.rs
+++ crates/storage/src/item.rs
@@ -1,16 +1,15 @@
 use {
     crate::{Borsh, Codec, Path},
-    grug_types::{StdError, StdResult, Storage},
-    std::marker::PhantomData,
+    std::{marker::PhantomData, ops::Deref},
 };
 
 pub struct Item<'a, T, C = Borsh>
 where
     C: Codec<T>,
 {
-    storage_key: &'a [u8],
     data: PhantomData<T>,
     codec: PhantomData<C>,
+    path: Path<'a, T, C>,
 }
 
 impl<'a, T, C> Item<'a, T, C>
@@ -19,53 +18,17 @@ where
 {
     pub const fn new(storage_key: &'a str) -> Self {
         Self {
-            storage_key: storage_key.as_bytes(),
+            path: Path::from_raw(storage_key.as_bytes()),
             data: PhantomData,
             codec: PhantomData,
         }
     }
+}
 
-    fn path(&self) -> Path<T, C> {
-        Path::from_raw(self.storage_key)
-    }
+impl<'a, T, C: Codec<T>> Deref for Item<'a, T, C> {
+    type Target = Path<'a, T, C>;
 
-    pub fn exists(&self, storage: &dyn Storage) -> bool {
-        self.path().exists(storage)
-    }
-
-    pub fn may_load_raw(&self, storage: &dyn Storage) -> Option<Vec<u8>> {
-        self.path().may_load_raw(storage)
-    }
-
-    pub fn may_load(&self, storage: &dyn Storage) -> StdResult<Option<T>> {
-        self.path().may_load(storage)
-    }
-
-    pub fn load_raw(&self, storage: &dyn Storage) -> StdResult<Vec<u8>> {
-        self.path().load_raw(storage)
-    }
-
-    pub fn load(&self, storage: &dyn Storage) -> StdResult<T> {
-        self.path().load(storage)
-    }
-
-    pub fn unsafe_save_raw(&self, storage: &mut dyn Storage, data_raw: &[u8]) {
-        self.path().save_raw(storage, data_raw)
-    }
-
-    pub fn save(&self, storage: &mut dyn Storage, data: &T) -> StdResult<()> {
-        self.path().save(storage, data)
-    }
-
-    pub fn remove(&self, storage: &mut dyn Storage) {
-        self.path().remove(storage)
-    }
-
-    pub fn update<A, Err>(&self, storage: &mut dyn Storage, action: A) -> Result<Option<T>, Err>
-    where
-        A: FnOnce(Option<T>) -> Result<Option<T>, Err>,
-        Err: From<StdError>,
-    {
-        self.path().update(storage, action)
+    fn deref(&self) -> &Self::Target {
+        &self.path
     }
 }
