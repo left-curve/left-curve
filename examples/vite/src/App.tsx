4064b5391412ecf67931777785ab0b53fe8358d1
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2024-08-30 17:37:53 +0200

    chore: example vite (#14)

---
+++ examples/vite/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full h-full bg-stone-900">
+      <h1 className="text-3xl font-bold text-neutral-100">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
