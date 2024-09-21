4a6404fcbeafd3f5897ea500b38975f61e65f79a
larry <26318510+larry0x@users.noreply.github.com>
2024-09-21 15:29:01 +0100

    fix typos

--- crates/std/src/lib.rs
+++ crates/std/src/lib.rs
@@ -6,13 +6,13 @@ pub use {grug_macros::*, grug_math::*, grug_storage::*, grug_types::*};
 #[cfg(target_arch = "wasm32")]
 pub use grug_ffi::*;
 
-// The cleint and testing crates are only included when _not_ building for
-// WebAseembly. They contain Wasm-incompatible feature, such as async runtime,
+// The client and testing crates are only included when _not_ building for
+// WebAssembly. They contain Wasm-incompatible feature, such as async runtime,
 // threads, and RNGs.
 #[cfg(not(target_arch = "wasm32"))]
 pub use {grug_client::*, grug_testing::*};
 
-// Dependencies used by the procedural macros
+// Dependencies used by the macros.
 #[doc(hidden)]
 pub mod __private {
     pub use {::borsh, ::paste, ::serde, ::serde_with};
