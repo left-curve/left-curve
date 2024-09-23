f6e06f705571327771dffb226fc98d3ce5f39bd4
Rhaki <Giacomo.encicliati@gmail.com>
2024-09-23 13:54:55 +0200

    init

---
+++ crates/math/src/is_zero.rs
@@ -0,0 +1,22 @@
+use bnum::types::{U256, U512};
+
+use crate::NumberConst;
+
+pub trait IsZero {
+    fn is_zero(&self) -> bool;
+}
+
+macro_rules! impl_is_zero {
+    ($($t:ty),+) => {
+        $(impl IsZero for $t
+        where
+            $t: NumberConst ,
+        {
+            fn is_zero(&self) -> bool {
+                *self == Self::ZERO
+            }
+        })+
+    };
+}
+
+impl_is_zero!(u8, u16, u32, u64, u128, U256, U512);
