c635f55068280bc0b359f0ea5b2df484d07e0324
larry <26318510+larry0x@users.noreply.github.com>
2024-09-23 22:01:13 +0100

    prettify `IsZero`

--- crates/math/src/is_zero.rs
+++ crates/math/src/is_zero.rs
@@ -3,20 +3,26 @@ use {
     bnum::types::{U256, U512},
 };
 
+/// Describes a number that can be compared to zero.
 pub trait IsZero {
     fn is_zero(&self) -> bool;
 }
 
 macro_rules! impl_is_zero {
-    ($($t:ty),+) => {
-        $(impl IsZero for $t
+    ($t:ty) => {
+        impl IsZero for $t
         where
-            $t: NumberConst ,
+            $t: NumberConst,
         {
             fn is_zero(&self) -> bool {
                 *self == Self::ZERO
             }
-        })+
+        }
+    };
+    ($($t:ty),+) => {
+        $(
+            impl_is_zero!($t);
+        )+
     };
 }
 
