01571b381c3b509a6a161370bee2d7355f5a2427
Rhaki <giacomo.encicliati@gmail.com>
2024-09-23 23:55:14 +0200

    fix merge

--- crates/math/src/fixed_point.rs
+++ crates/math/src/fixed_point.rs
@@ -1,6 +1,6 @@
 use {
-    crate::{Udec128, Udec256, Uint, Uint128, Uint256},
-    bnum::types::U256,
+    crate::{Dec128, Dec256, Int128, Int256, Udec128, Udec256, Uint, Uint128, Uint256},
+    bnum::types::{I256, U256},
 };
 
 /// Describes a [fixed-point decimal](https://en.wikipedia.org/wiki/Fixed-point_arithmetic)
@@ -21,5 +21,15 @@ impl FixedPoint<u128> for Udec128 {
 
 impl FixedPoint<U256> for Udec256 {
     const DECIMAL_FRACTION: Uint256 = Uint256::new_from_u128(1_000_000_000_000_000_000);
+    const DECIMAL_PLACES: u32 = 18;
+}
+
+impl FixedPoint<i128> for Dec128 {
+    const DECIMAL_FRACTION: Int128 = Int128::new(1_000_000_000_000_000_000);
+    const DECIMAL_PLACES: u32 = 18;
+}
+
+impl FixedPoint<I256> for Dec256 {
+    const DECIMAL_FRACTION: Int256 = Int256::new_from_i128(1_000_000_000_000_000_000);
     const DECIMAL_PLACES: u32 = 18;
 }
