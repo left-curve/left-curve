65934318d7f5ad980d2a75bc920ce15d2764803d
larry <26318510+larry0x@users.noreply.github.com>
2024-09-25 03:13:15 +0100

    remove an outdated comment

--- crates/math/src/fraction.rs
+++ crates/math/src/fraction.rs
@@ -1,10 +1,6 @@
 use crate::{FixedPoint, MathResult, MultiplyRatio, Udec, Uint};
 
 /// Describes a number that can be expressed as the quotient of two integers.
-///
-/// Note that here we only concern the fraction's absolute value. Both the
-/// numerator and denominator here are negative. This trait is intended to be
-/// used together with [`Sign`] To account for negative fractions.
 pub trait Fraction<U>: Sized {
     fn numerator(&self) -> Uint<U>;
 
