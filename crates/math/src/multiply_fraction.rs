94e2ad2277d8bfdeaa24081199e5dfd3698c9ce4
Larry <26318510+larry0x@users.noreply.github.com>
2024-09-28 18:12:47 +0100

    simplify trait bounds for MultiplyRatio and MultipleFraction (#176)

--- crates/math/src/multiply_fraction.rs
+++ crates/math/src/multiply_fraction.rs
@@ -1,6 +1,4 @@
-use crate::{
-    Fraction, Int, IsZero, MathError, MathResult, MultiplyRatio, Number, NumberConst, Sign,
-};
+use crate::{Dec, Fraction, Int, IsZero, MathError, MathResult, MultiplyRatio, NumberConst};
 
 /// Describes operations between a number and a decimal type.
 pub trait MultiplyFraction<F, U>: Sized
@@ -16,29 +14,29 @@ where
     fn checked_div_dec_ceil(self, rhs: F) -> MathResult<Self>;
 }
 
-impl<U, AsU, F> MultiplyFraction<F, AsU> for Int<U>
+impl<U> MultiplyFraction<Dec<U>, U> for Int<U>
 where
-    Int<U>: NumberConst + Number + IsZero + MultiplyRatio + From<Int<AsU>> + ToString,
-    F: Number + Fraction<AsU> + Sign + ToString + IsZero,
+    Int<U>: IsZero + NumberConst + MultiplyRatio + ToString + Copy,
+    Dec<U>: IsZero + Fraction<U>,
 {
-    fn checked_mul_dec_floor(self, rhs: F) -> MathResult<Self> {
+    fn checked_mul_dec_floor(self, rhs: Dec<U>) -> MathResult<Self> {
         // If either left or right hand side is zero, then simply return zero.
         if self.is_zero() || rhs.is_zero() {
             return Ok(Self::ZERO);
         }
 
-        self.checked_multiply_ratio_floor(rhs.numerator(), F::denominator())
+        self.checked_multiply_ratio_floor(*rhs.numerator(), Dec::<U>::denominator())
     }
 
-    fn checked_mul_dec_ceil(self, rhs: F) -> MathResult<Self> {
+    fn checked_mul_dec_ceil(self, rhs: Dec<U>) -> MathResult<Self> {
         if self.is_zero() || rhs.is_zero() {
             return Ok(Self::ZERO);
         }
 
-        self.checked_multiply_ratio_ceil(rhs.numerator(), F::denominator())
+        self.checked_multiply_ratio_ceil(*rhs.numerator(), Dec::<U>::denominator())
     }
 
-    fn checked_div_dec_floor(self, rhs: F) -> MathResult<Self> {
+    fn checked_div_dec_floor(self, rhs: Dec<U>) -> MathResult<Self> {
         // If right hand side is zero, throw error, because you can't divide any
         // number by zero.
         if rhs.is_zero() {
@@ -51,10 +49,10 @@ where
             return Ok(Self::ZERO);
         }
 
-        self.checked_multiply_ratio_floor(F::denominator(), rhs.numerator())
+        self.checked_multiply_ratio_floor(Dec::<U>::denominator(), *rhs.numerator())
     }
 
-    fn checked_div_dec_ceil(self, rhs: F) -> MathResult<Self> {
+    fn checked_div_dec_ceil(self, rhs: Dec<U>) -> MathResult<Self> {
         if rhs.is_zero() {
             return Err(MathError::division_by_zero(self));
         }
@@ -63,6 +61,6 @@ where
             return Ok(Self::ZERO);
         }
 
-        self.checked_multiply_ratio_ceil(F::denominator(), rhs.numerator())
+        self.checked_multiply_ratio_ceil(Dec::<U>::denominator(), *rhs.numerator())
     }
 }
