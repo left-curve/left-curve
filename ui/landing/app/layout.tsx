88036c7d952f4013ee81c97743f59bc7ae60874b
j0nl1 <jonlitech@gmail.com>
2024-10-04 12:04:05 +0100

    feat: landing page

--- ui/landing/app/layout.tsx
+++ ui/landing/app/layout.tsx
@@ -16,12 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen w-full h-full bg-white relative overflow-y-auto overflow-x-hidden scrollbar-none items-center justify-center pt-[166px] md:pt-[110px]">
-        <img
-          src="/images/background.png"
-          alt="bg-image"
-          className="object-cover h-[80vh] absolute top-[15%] left-1/2 transform -translate-x-1/2 z-0 blur-2xl "
-        />
+      <body className="flex flex-col min-h-screen w-full h-full bg-white relative overflow-y-auto overflow-x-hidden scrollbar-none items-center justify-center">
         <Providers>
           <main className="flex flex-1 w-full">{children}</main>
         </Providers>
