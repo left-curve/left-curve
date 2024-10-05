9874167e0c62c5c22290e0aac8883fcd0984e3ae
larry <26318510+larry0x@users.noreply.github.com>
2024-10-05 03:06:15 +0100

    add done message

--- docker/devnet/run.sh
+++ docker/devnet/run.sh
@@ -3,10 +3,12 @@
 # Start grug first. Redirect the stdout/stderr to the container's stdout/stderr.
 echo "starting grug..."
 grug start > >(tee -a /proc/self/fd/1) 2> >(tee -a /proc/self/fd/2 >&2) &
+echo "done! PID: $!"
 
 # Start cometbft next. Hide its stdout/stderr.
 echo "starting cometbft..."
 cometbft start > /dev/null 2>&1 &
+echo "done! PID: $!"
 
 # Keep the container running until SIGTERM.
 tail -f /dev/null
