ebaf490de182c7d5f5cf8ce46281ea6a732a69e9
j0nl1 <jonlitech@gmail.com>
2024-10-07 17:56:30 +0100

    feat: connect button in development mode

--- ui/portal/src/App.tsx
+++ ui/portal/src/App.tsx
@@ -5,6 +5,7 @@ import { AppProvider } from "./AppProvider";
 import { AppRouter } from "./AppRouter";
 
 import "../public/global.css";
+import "@dango/shared/fonts/ABCDiatypeRounded/index.css";
 
 export const App: React.FC = () => {
   return (
