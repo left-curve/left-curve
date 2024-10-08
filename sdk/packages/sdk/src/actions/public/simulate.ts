81de5fee0ea1b7c1ac3826183584a8a76adfb373
j0nl1 <jonlitech@gmail.com>
2024-10-08 16:04:36 +0100

    fix: simulation calculation

--- sdk/packages/sdk/src/actions/public/simulate.ts
+++ sdk/packages/sdk/src/actions/public/simulate.ts
@@ -12,6 +12,8 @@ import { queryAbci } from "./queryAbci";
 
 export type SimulateParameters = {
   simulate: SimulateRequest;
+  scale?: number;
+  base?: number;
   height?: number;
 };
 
@@ -21,6 +23,8 @@ export type SimulateReturnType = Promise<SimulateResponse>;
  * Simulate a transaction.
  * @param parameters
  * @param parameters.simulate The simulation request.
+ * @param parameters.scale The scale factor to apply to the gas used.
+ * @param parameters.base Base increase to apply for signature verification.
  * @param parameters.height The height at which to simulate the transaction.
  * @returns The simulation response.
  */
@@ -28,7 +32,7 @@ export async function simulate<chain extends Chain | undefined, signer extends S
   client: Client<Transport, chain, signer>,
   parameters: SimulateParameters,
 ): SimulateReturnType {
-  const { simulate, height = 0 } = parameters;
+  const { simulate, scale = 1.3, base = 750_000, height = 0 } = parameters;
 
   const { value } = await queryAbci(client, {
     data: serialize(simulate),
@@ -37,5 +41,9 @@ export async function simulate<chain extends Chain | undefined, signer extends S
     prove: false,
   });
 
-  return deserialize<SimulateResponse>(decodeBase64(value ?? ""));
+  const { gasLimit, gasUsed } = deserialize<SimulateResponse>(decodeBase64(value ?? ""));
+  return {
+    gasLimit,
+    gasUsed: Math.round((gasUsed + base) * scale),
+  };
 }
