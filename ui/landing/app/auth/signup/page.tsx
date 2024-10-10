943702d9eef106bd6bd16c3b5c27a11ccaa72db1
j0nl1 <jonlitech@gmail.com>
2024-10-10 15:08:18 +0100

    refactor: delete nextjs project

--- ui/landing/app/auth/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { WizardSignup } from "~/components/WizardSignup";
-
-function SignupPage() {
-  return <WizardSignup />;
-}
-
-export default SignupPage;
--- ui/landing/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    loader: "custom",
-    loaderFile: "./loaders/imageLoader.js"
-  }
-};
-
-export default nextConfig;
