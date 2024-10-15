9b9acf25c75e1cd0bbb4c35b31ed8053ae8c27b3
larry <26318510+larry0x@users.noreply.github.com>
2024-10-15 22:33:07 +0100

    remove duplicate change set

--- dango/types/src/lib.rs
+++ dango/types/src/lib.rs
@@ -3,10 +3,7 @@ pub mod account_factory;
 pub mod amm;
 pub mod auth;
 pub mod bank;
-mod changeset;
 pub mod config;
 pub mod mock_ibc_transfer;
 pub mod taxman;
 pub mod token_factory;
-
-pub use changeset::*;
