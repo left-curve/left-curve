a1d7bf5ec81068e755d5af9679ebc173dde1f0f5
Rhaki <86773669+Rhaki@users.noreply.github.com>
2024-10-16 18:05:18 +0200

    instantiate double address

--- grug/testing/tests/tx.rs
+++ grug/testing/tests/tx.rs
@@ -26,11 +26,42 @@ fn upload_same_code() {
 
     suite
         .upload(&mut accounts["sender"], contract)
-        .outcome
         .should_succeed();
 
     suite.
         upload(&mut accounts["sender"], contract)
-        .outcome
         .should_fail_with_error("code with hash `35BE322D094F9D154A8ABA4733B8497F180353BD7AE7B0A15F90B586B549F28B` already exists");
+}
+
+#[test]
+fn instantiate_same_address() {
+    let (mut suite, mut accounts) = setup();
+    let contract = empty_contract();
+
+    let code_hash = suite
+        .upload_and_instantiate(
+            &mut accounts["sender"],
+            contract,
+            &Empty {},
+            "salt",
+            None::<String>,
+            None,
+            Coins::new(),
+        )
+        .should_succeed()
+        .code_hash;
+
+    suite
+        .instantiate(
+            &mut accounts["sender"],
+            code_hash,
+            &Empty {},
+            "salt",
+            None::<String>,
+            None,
+            Coins::new(),
+        )
+        .should_fail_with_error(
+            "account with address `0xcf580adbd298e67ee43ce479b533aa6db6e4f7e4` already exists",
+        );
 }
