0719675e0701162893e9593d316009188fa9263b
j0nl1 <jonlitech@gmail.com>
2024-10-28 12:30:39 +0000

    feat: avoid splitting and bundle when dev-mode

--- sdk/packages/types/tsup.config.ts
+++ sdk/packages/types/tsup.config.ts
@@ -2,10 +2,25 @@ import config from "@leftcurve/config/tsup/common.json" with { type: "json" };
 
 import { type Options, defineConfig } from "tsup";
 
+const isDev = process.env.NODE_ENV === "development";
+
+const extraOptions: Partial<Options> = isDev
+  ? {
+      treeshake: false,
+      splitting: false,
+      bundle: false,
+    }
+  : {
+      treeshake: "recommended",
+      splitting: true,
+      bundle: true,
+    };
+
 /**
  * @see https://tsup.egoist.dev/#usage
  */
 export default defineConfig({
   ...(config as Options),
-  entry: ["src"],
+  ...extraOptions,
+  entry: ["src/**"],
 });
