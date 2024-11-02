e6a89607ad57c58637fe712fdc517f82002b9016
larry <26318510+larry0x@users.noreply.github.com>
2024-11-02 16:57:24 +0000

    fix a typo

--- dango/types/src/oracle.rs
+++ dango/types/src/oracle.rs
@@ -1,8 +1,8 @@
-mod byte_analizer;
+mod bytes_analyzer;
 mod msg;
 mod price;
 mod price_source;
 mod pyth;
 mod wormhole;
 
-pub use {byte_analizer::*, msg::*, price::*, price_source::*, pyth::*, wormhole::*};
+pub use {bytes_analyzer::*, msg::*, price::*, price_source::*, pyth::*, wormhole::*};
