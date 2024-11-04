e046a53dde6460630d69203252636abdc5d452de
larry <26318510+larry0x@users.noreply.github.com>
2024-11-04 18:32:53 +0000

    change storage namespace

--- dango/oracle/src/state.rs
+++ dango/oracle/src/state.rs
@@ -3,6 +3,6 @@ use {
     grug::{Denom, Map},
 };
 
-pub const GUARDIAN_SETS: Map<u32, GuardianSetInfo> = Map::new("guardian_sets");
+pub const GUARDIAN_SETS: Map<u32, GuardianSetInfo> = Map::new("guardian_set");
 
-pub const PRICE_SOURCES: Map<&Denom, PriceSourceCollector> = Map::new("price_sources");
+pub const PRICE_SOURCES: Map<&Denom, PriceSourceCollector> = Map::new("price_source");
