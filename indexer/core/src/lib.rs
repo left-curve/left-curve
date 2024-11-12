3b3ea0bcce20fada5037ef56dafe4b51b86fa177
Fabien Penso <git@pen.so>
2024-11-12 15:25:52 +0100

    Sync

--- indexer/core/src/lib.rs
+++ indexer/core/src/lib.rs
@@ -8,8 +8,33 @@ use tokio::task;
 //static RUNTIME: OnceLock<Arc<Runtime>> = OnceLock::new();
 
 #[derive(Debug, Clone)]
-pub struct App {
+pub struct Context {
     db: DatabaseConnection,
+}
+
+impl Context {
+    pub async fn migrate_db(&self) -> Result<(), sea_orm::DbErr> {
+        Migrator::up(&self.db, None).await
+    }
+
+    async fn connect_db() -> Result<DatabaseConnection, sea_orm::DbErr> {
+        // TODO: Use the settings to connect to the database
+        let database_url = "sqlite::memory:";
+
+        let mut opt = ConnectOptions::new(database_url.to_owned());
+        opt.max_connections(10);
+        // .min_connections(5)
+        //.connect_timeout(Duration::from_secs(settings.timeout))
+        //.idle_timeout(Duration::from_secs(8))
+        //.max_lifetime(Duration::from_secs(20))
+        //.sqlx_logging(settings.logging);
+        Database::connect(opt).await
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct App {
+    context: Context,
     runtime: Arc<Runtime>,
 }
 
@@ -31,29 +56,27 @@ impl App {
 
         //let runtime = Builder::new_multi_thread().enable_all().build()?;
 
-        let db = runtime.block_on(async { Self::connect_db().await })?;
+        let db = runtime.block_on(async { Context::connect_db().await })?;
 
         Ok(App {
-            db,
+            context: Context { db },
             runtime: runtime.clone(),
         })
     }
 
     pub fn migrate_db(&self) -> Result<(), sea_orm::DbErr> {
         self.runtime
-            .block_on(async { self.migrate_db_async().await })
+            .block_on(async { self.context.migrate_db().await })
     }
 
-    pub async fn migrate_db_async(&self) -> Result<(), sea_orm::DbErr> {
-        Migrator::up(&self.db, None).await
-    }
-
-    pub fn precommit(&self) -> Result<DatabaseTransaction, anyhow::Error> {
+    pub fn db_txn(&self) -> Result<DatabaseTransaction, anyhow::Error> {
         //let Some(runtime) = RUNTIME.get() else {
         //    anyhow::bail!("Runtime not initialized");
         //};
 
-        let db_transaction = self.runtime.block_on(async { self.db.begin().await })?;
+        let db_transaction = self
+            .runtime
+            .block_on(async { self.context.db.begin().await })?;
 
         Ok(db_transaction)
     }
@@ -62,7 +85,7 @@ impl App {
 
     pub fn index_transaction(&self) {}
 
-    pub fn commit(&self, txn: DatabaseTransaction) -> Result<(), anyhow::Error> {
+    pub fn save_db_txn(&self, txn: DatabaseTransaction) -> Result<(), sea_orm::DbErr> {
         //let Some(runtime) = RUNTIME.get() else {
         //    anyhow::bail!("Runtime not initialized");
         //};
@@ -71,25 +94,15 @@ impl App {
 
         Ok(())
     }
+
+    /// This will be used to ensure the tokio has no more tasks to run, when we gracefully stop
+    /// Grug.
+    pub fn wait_for_all_tasks(&self) {
+        self.runtime.block_on(async {});
+    }
 }
 
 impl App {
-    async fn connect_db() -> Result<DatabaseConnection, sea_orm::DbErr> {
-        // TODO: Use the settings to connect to the database
-        let database_url = "sqlite::memory:";
-
-        // TODO: migrate the database
-
-        let mut opt = ConnectOptions::new(database_url.to_owned());
-        opt.max_connections(10);
-        // .min_connections(5)
-        //.connect_timeout(Duration::from_secs(settings.timeout))
-        //.idle_timeout(Duration::from_secs(8))
-        //.max_lifetime(Duration::from_secs(20))
-        //.sqlx_logging(settings.logging);
-        Database::connect(opt).await
-    }
-
     #[allow(dead_code)]
     // Function to run an async task without blocking
     fn run_async_task<F>(&self, task: F)
@@ -103,40 +116,66 @@ impl App {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use sea_orm::entity::prelude::DateTime;
+    use assertor::*;
     use sea_orm::ActiveModelTrait;
+    use sea_orm::ConnectionTrait;
     use sea_orm::EntityTrait;
     use sea_orm::Set;
-    use uuid::Uuid;
 
+    /// This is when used from Grug, which isn't async. In such case `App` has its own Tokio
+    /// runtime and we need to inject async functions
     #[test]
-    fn should_migrate_db() {
-        let app = App::new().unwrap();
-        app.migrate_db().expect("Can't migrate DB");
+    fn should_migrate_db_and_create_block() {
+        let app = app();
         app.runtime.block_on(async {
-            let blocks = indexer_entity::blocks::Entity::find()
-                .all(&app.db)
-                .await
-                .expect("Can't fetch blocks");
-            assert_eq!(blocks.len(), 0);
-            let new_block = indexer_entity::blocks::ActiveModel {
-                id: Set(Uuid::new_v4()),
-                //created_at: Set(DateTime::new()),
-                block_height: Set(0),
-                ..Default::default()
-            };
-            new_block.insert(&app.db).await.expect("Can't save block");
-            let blocks = indexer_entity::blocks::Entity::find()
-                .all(&app.db)
-                .await
-                .expect("C'ant fetch blocks");
-            assert_eq!(blocks.len(), 1);
+            check_empty_and_create_block(&app.context.db).await;
         });
     }
 
+    /// This is when used from the httpd API, which is async. We dont need to use `App` runtime.
     #[tokio::test]
-    async fn should_migrate_db_and_have_no_block() {
-        let app = App::new().unwrap();
+    async fn async_should_migrate_db_and_create_block() {
+        let db = Context::connect_db().await.expect("Can't get DB");
+        Migrator::up(&db, None).await.expect("Can't run migration");
+        check_empty_and_create_block(&db).await;
+    }
+
+    #[test]
+    fn should_use_db_transaction() {
+        let app = app();
+        let txn = app.db_txn().expect("Can't get db txn");
+        app.runtime
+            .block_on(async {
+                check_empty_and_create_block(&txn).await;
+                txn.commit().await?;
+                Ok::<(), sea_orm::DbErr>(())
+            })
+            .expect("Can't commit txn");
+    }
+
+    fn app() -> App {
+        let app = App::new().expect("Can't create app");
         app.migrate_db().expect("Can't migrate DB");
+        app
+    }
+
+    async fn check_empty_and_create_block<C: ConnectionTrait>(db: &C) {
+        let blocks = indexer_entity::blocks::Entity::find()
+            .all(db)
+            .await
+            .expect("Can't fetch blocks");
+        assert_that!(blocks).is_empty();
+        let new_block = indexer_entity::blocks::ActiveModel {
+            id: Set(Default::default()),
+            block_height: Set(10),
+            created_at: Set(Default::default()),
+        };
+        new_block.insert(db).await.expect("Can't save block");
+        let block = indexer_entity::blocks::Entity::find()
+            .one(db)
+            .await
+            .expect("Can't fetch blocks")
+            .expect("Non existing block");
+        assert_that!(block.block_height).is_equal_to(10);
     }
 }
--- indexer/entity/src/entity/messages.rs
+++ indexer/entity/src/entity/messages.rs
@@ -2,7 +2,7 @@
 
 use sea_orm::entity::prelude::*;
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Default)]
 #[sea_orm(table_name = "messages")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
