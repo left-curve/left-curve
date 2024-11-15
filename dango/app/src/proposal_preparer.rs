578e8678d1433019b8545fd8b376bc64ddc8a23a
Zexsor33 <c.mirco89@gmail.com>
2024-11-15 17:48:56 +0100

    fix: apply suggestions

--- dango/app/src/proposal_preparer.rs
+++ dango/app/src/proposal_preparer.rs
@@ -7,17 +7,30 @@ use {
     grug_app::Shared,
     prost::bytes::Bytes,
     std::{
+        cmp::min,
+        ops::Mul,
         thread::{self, JoinHandle},
         time,
     },
     thiserror::Error,
-    tracing::error,
+    tracing::{error, info},
 };
 
 const PYTH_URL: &str = "https://hermes.pyth.network";
-const REQUEST_TIMEOUT: time::Duration = time::Duration::from_millis(500);
-const THREAD_SLEEP: time::Duration = time::Duration::from_millis(500);
+const REQUEST_TIMEOUT: time::Duration = time::Duration::from_millis(1000);
+const THREAD_SLEEP: time::Duration = time::Duration::from_millis(1000);
+const MAX_THREAD_SLEEP: time::Duration = time::Duration::from_secs(30);
 const GAS_LIMIT: u64 = 50_000_000;
+
+#[grug::derive(Serde)]
+struct LatestVaaResponse {
+    pub binary: LatestVaaBinaryResponse,
+}
+
+#[grug::derive(Serde)]
+struct LatestVaaBinaryResponse {
+    pub data: Vec<Binary>,
+}
 
 #[derive(Debug, Error)]
 pub enum ProposerError {
@@ -35,7 +48,7 @@ impl From<ProposerError> for grug_app::AppError {
 }
 
 pub struct ProposalPreparer {
-    params: Shared<Vec<(String, String)>>,
+    params: Shared<Vec<(&'static str, String)>>,
     latest_vaas: Shared<Vec<Binary>>,
     // Option since we don't want to clone the thread handle.
     // Store the thread to keep it alive.
@@ -68,24 +81,28 @@ impl ProposalPreparer {
         let _handle = thread::spawn(move || {
             let update_func = || -> Result<(), ProposerError> {
                 // Copy the params to unlock the mutex.
-                let params = thread_params.read_access().clone();
+                let mut params = thread_params.read_access().clone();
                 if params.is_empty() {
                     return Ok(());
                 }
+
+                // Set the encoding to base64 to match the oracle contract.
+                params.push(("encoding", "base64".to_string()));
+                // Set the parsed to false since we don't use parsed data.
+                params.push(("parsed", "false".to_string()));
 
                 // Retrieve vaas from pyth node.
                 let vaas = reqwest::blocking::Client::builder()
                     .timeout(REQUEST_TIMEOUT)
                     .build()?
-                    .get(format!("{PYTH_URL}/api/latest_vaas"))
+                    .get(format!("{PYTH_URL}/v2/updates/price/latest"))
                     .query(&params)
                     .send()?
-                    .json::<Vec<Binary>>()?;
+                    .json::<LatestVaaResponse>()?
+                    .binary
+                    .data;
 
-                tracing::info!(
-                    "Prepare proposal: fetched latest vaas - len: {}",
-                    vaas.len()
-                );
+                info!(len = vaas.len(), "Prepare proposal: fetched latest Vaas",);
 
                 // Update the prices.
                 thread_latest_vaas.write_with(|mut latest_vaas| {
@@ -95,14 +112,24 @@ impl ProposalPreparer {
                 Ok(())
             };
 
+            let mut failed_requests: u32 = 0;
+
             loop {
                 // Update the vaas.
-                update_func().unwrap_or_else(|err| {
-                    error!(err = err.to_string(), "Failed to update the latest vaas")
-                });
+                match update_func() {
+                    Ok(_) => failed_requests = 0,
+                    Err(err) => {
+                        error!(err = err.to_string(), "Failed to update the latest vaas");
+                        failed_requests += 1;
+                    },
+                }
 
-                // Wait `THREAD_SLEEP` before the next iteration.
-                thread::sleep(THREAD_SLEEP);
+                // Wait some time before the next request.
+                // Exponentially increases the sleep time on failed requests.
+                thread::sleep(min(
+                    MAX_THREAD_SLEEP,
+                    THREAD_SLEEP.mul(2u32.pow(failed_requests)),
+                ));
             }
         });
 
@@ -137,7 +164,7 @@ impl grug_app::ProposalPreparer for ProposalPreparer {
                 // For now there is only Pyth as PriceSource, but there could be more.
                 #[allow(irrefutable_let_patterns)]
                 if let PriceSource::Pyth { id, .. } = price_source {
-                    Some(("ids[]".to_string(), id.to_string()))
+                    Some(("ids[]", id.to_string()))
                 } else {
                     None
                 }
@@ -178,5 +205,23 @@ impl grug_app::ProposalPreparer for ProposalPreparer {
         txs.insert(0, tx.to_json_vec()?.into());
 
         Ok(txs)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use {super::LatestVaaResponse, grug::JsonDeExt};
+
+    #[test]
+    fn pyth_response() {
+        let res = r#"{
+            "binary": {
+                "encoding": "base64",
+                "data": [
+                "UE5BVQEAAAADuAEAAAAEDQBkr0PLb+gk8uvpb1vCCnSzrkNBWuAKD+4/oHA1HhL2rywRRNl4NEsUMyNDHVVFJz7sb2TqUXbIVKSDR+cXZk12AQHIF5lSaXo4Br8HN9I8FxSKI+k39d0G/hfGGcg42L1lmhol2f3hJRw32z9e9ktuCvOIClAe0U0t8hQk2meeHWReAARHzS7dEnqYLo5cM0ct+0lmMftM+SER9GP/Kr/l1nnUaRNff+2443LwCqOay1A0DSn6sOa6FO16w5mbgsNiUuMlAAhiIiNh1QIxaoKUydS3R0MnKoBkdt7ixtVCvK/GPi0PeC3goY+ZgaheHaVYt6lfjD0nwITz2bdYFZNq2SqO510VAAoHTixQaLHPPgi72kww0j5hOlJn11W1Rz8LAGGl0gk7/GZwEhiBUCuUCfTFwpHqX4UHJIXftR0SV1mS6UB3XLV6AAtAVczuOpFCCPiH9Sg9I0l1xitkQotpP8h+di11JJ3CVkm6vRU7zy1KrhYFsoV082IZTi0XN0Xdv5fWZZdg3Hx5AAzpW38X3a88bFIoys/jeflLAN+A9VeABd5HN9D6snOgI3o5FXftoZjNP0c8Xd/J8rTlUgBIOgsyGFh2vRjJbe6JAQ2+2P9NCAAhBnOYoRjASsA/XlI2FDEGK58ati8kz4vJGyO9B8O6sdZiKE70ieoDirBl8puIkYb0sPU8vov/xF1kAA4cPqmQw6zUazq7F8VE0FSe9C2KD3bD2drA/5rkFExyABw+4XL/4KGbA02YEvp4rGpQvBHKjyS7HhdfuWaspCtjAQ8IoVHbmdLHcnc1dJLgduCtMOPVB3+kpJZBLRfu962mRhG6zLmIr5ioO8/HSsDSQWLWGfi7u2z+g9Nw0CVVhmwmARBAmbiFQ3nI4Idyu9XHuY9r9FaQa1rAcdCuDEGCZPdbphqX7kckA6r95J01ERAqLHwNvZDzuTTgY00Qcuh4v7bfABELTI+OKdU8zctRIb+HJxqQJKeCuBc2+zFo0Tfjs7A8Ix4mszdY/c+1iKZjQLP8MvDekc1rPgjCbhJeZrvoz7xHABJoPDAMGFjauAjWB3g8EJHV+V+oTH5pYLXfZrXdUzzHxTj+JdMaJNIl7EN9MQ7hPzwKkkvwEKuHx661sLdMwGt1AWc3bGMAAAAAABrhAfrtrFhR4yubI7X5QRqMK6xKrj7U3XuBHdGnLqSqcQAAAAAFkcwYAUFVV1YAAAAAAAqelroAACcQy4n8eAYQTYIOJi7hMVMnT8+xofsBAFUA5i32yLSoX+GmfbRNwS3l2zMPesZrctxliv7fD0pBW0MAAAgFdR9DkgAAAAErfT7c////+AAAAABnN2xjAAAAAGc3bGMAAAgVwbgMwAAAAAErXavAC2svmTTo0dM2ChAXPyvygP0QyQvSR/BV5sMFociUhrsruHLNPmvHkOxzZMH4NQzepTk6Wc6cGiVU1RVKdqDYcPPsm+N2jsKYH5zp65jeHXG0h5pR53BajAgRmNmq7xzBP5e1vT6gv5CHEbv1QGAvbGXSReuOOj1LR29RgjJiM/A/PCysDvPuhI5r9QqT69mMvlraw2QdqlRGmTsq/1LlskABMC+bL64zMUlwLmo0N3kxXuS3Y906SY9J733py3EPMQMRrGD5C185kIK9iYlvyEOx5Tq0wvQeaB2/J7Q="
+                ]
+            }}"#;
+
+        res.deserialize_json::<LatestVaaResponse>().unwrap();
     }
 }
