dfdaf6002b6abf6d114bb67864179bc3bdc4c17a
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2024-11-26 17:18:44 +0200

    feat(margin): implement liquidation

--- dango/genesis/src/lib.rs
+++ dango/genesis/src/lib.rs
@@ -346,7 +346,7 @@ where
             lending,
             oracle,
         },
-        collateral_powers: btree_map! {},
+        ..Default::default()
     };
 
     let genesis_state = GenesisState {
