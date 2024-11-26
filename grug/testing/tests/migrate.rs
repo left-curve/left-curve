d1836dadf1e8681da9159f82825c7bc2414d00f3
Rhaki <86773669+Rhaki@users.noreply.github.com>
2024-11-26 15:38:06 +0100

    finalize tests

--- grug/testing/tests/migrate.rs
+++ grug/testing/tests/migrate.rs
@@ -2,7 +2,7 @@ use {
     grug_testing::{TestBuilder, UploadAndInstantiateOutcomeSuccess},
     grug_types::{Coins, Empty, ResultExt},
     grug_vm_rust::ContractBuilder,
-    tester::{MigrateMsg, QueryV1Request, QueryV2Request},
+    tester::{MigrateMsg, QueryV1, QueryV2RequestV1, QueryV2RequestV2},
 };
 
 mod tester {
@@ -29,7 +29,7 @@ mod tester {
         Ok(Response::new())
     }
 
-    pub fn migrate(ctx: SudoCtx, msg: MigrateMsg) -> StdResult<Response> {
+    pub fn migrate_v2(ctx: SudoCtx, msg: MigrateMsg) -> StdResult<Response> {
         match msg {
             MigrateMsg::Ok => {
                 ITEM_V2.save(ctx.storage, &"v2".to_string())?;
@@ -39,50 +39,62 @@ mod tester {
         }
     }
 
-    pub fn query(ctx: ImmutableCtx, msg: QueryMsg) -> StdResult<Json> {
+    pub fn query_v1(ctx: ImmutableCtx, _msg: QueryV1) -> StdResult<Json> {
+        ITEM_V1.load(ctx.storage)?.to_json_value()
+    }
+
+    pub fn query_v2(ctx: ImmutableCtx, msg: QueryV2) -> StdResult<Json> {
         match msg {
-            QueryMsg::V1 => ITEM_V1.load(ctx.storage)?.to_json_value(),
-            QueryMsg::V2 => ITEM_V2.load(ctx.storage)?.to_json_value(),
+            QueryV2::V1 => ITEM_V1.load(ctx.storage)?.to_json_value(),
+            QueryV2::V2 => ITEM_V2.load(ctx.storage)?.to_json_value(),
         }
     }
 
     #[derive(Serialize, Deserialize)]
-    pub enum QueryMsg {
+    pub struct QueryV1;
+
+    impl QueryRequest for QueryV1 {
+        type Message = Self;
+        type Response = String;
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub enum QueryV2 {
         V1,
         V2,
     }
 
     #[derive(Serialize, Deserialize)]
-    pub struct QueryV1Request;
+    pub struct QueryV2RequestV1;
 
-    impl QueryRequest for QueryV1Request {
-        type Message = QueryMsg;
+    impl QueryRequest for QueryV2RequestV1 {
+        type Message = QueryV2;
         type Response = String;
     }
 
-    impl From<QueryV1Request> for QueryMsg {
-        fn from(_: QueryV1Request) -> Self {
-            QueryMsg::V1
+    impl From<QueryV2RequestV1> for QueryV2 {
+        fn from(_: QueryV2RequestV1) -> Self {
+            QueryV2::V1
         }
     }
 
     #[derive(Serialize, Deserialize)]
-    pub struct QueryV2Request;
+    pub struct QueryV2RequestV2;
 
-    impl QueryRequest for QueryV2Request {
-        type Message = QueryMsg;
+    impl QueryRequest for QueryV2RequestV2 {
+        type Message = QueryV2;
         type Response = String;
     }
 
-    impl From<QueryV2Request> for QueryMsg {
-        fn from(_: QueryV2Request) -> Self {
-            QueryMsg::V2
+    impl From<QueryV2RequestV2> for QueryV2 {
+        fn from(_: QueryV2RequestV2) -> Self {
+            QueryV2::V2
         }
     }
 }
 
 #[test]
-fn test() {
+fn migrate() {
     let (mut suite, mut accounts) = TestBuilder::new()
         .add_account("owner", Coins::new())
         .add_account("sender", Coins::new())
@@ -90,12 +102,12 @@ fn test() {
         .build();
 
     let v1 = ContractBuilder::new(Box::new(tester::instantiate))
-        .with_query(Box::new(tester::query))
+        .with_query(Box::new(tester::query_v1))
         .build();
 
     let v2 = ContractBuilder::new(Box::new(tester::instantiate))
-        .with_query(Box::new(tester::query))
-        .with_migrate(Box::new(tester::migrate))
+        .with_query(Box::new(tester::query_v2))
+        .with_migrate(Box::new(tester::migrate_v2))
         .build();
 
     let mut admin = accounts.remove("owner").unwrap();
@@ -141,8 +153,12 @@ fn test() {
     }
 
     // Ok migrate
-    // Migrate but a failure happens during migrate execution
     {
+        // Query current contract
+        suite
+            .query_wasm_smart(contract, QueryV1)
+            .should_succeed_and_equal("v1");
+
         suite
             .migrate(&mut admin, contract, v2_code_hash, &MigrateMsg::Ok)
             .should_succeed();
@@ -153,11 +169,52 @@ fn test() {
             .should_succeed_and(|info| info.code_hash == v2_code_hash);
 
         suite
-            .query_wasm_smart(contract, QueryV1Request)
+            .query_wasm_smart(contract, QueryV2RequestV1)
             .should_succeed_and_equal("v1");
 
         suite
-            .query_wasm_smart(contract, QueryV2Request)
+            .query_wasm_smart(contract, QueryV2RequestV2)
             .should_succeed_and_equal("v2");
     }
+}
+
+#[test]
+fn migrate_no_admin() {
+    let (mut suite, mut accounts) = TestBuilder::new()
+        .add_account("owner", Coins::new())
+        .add_account("sender", Coins::new())
+        .set_owner("owner")
+        .build();
+
+    let v1 = ContractBuilder::new(Box::new(tester::instantiate))
+        .with_query(Box::new(tester::query_v1))
+        .build();
+
+    let v2 = ContractBuilder::new(Box::new(tester::instantiate))
+        .with_query(Box::new(tester::query_v2))
+        .with_migrate(Box::new(tester::migrate_v2))
+        .build();
+
+    let mut admin = accounts.remove("owner").unwrap();
+
+    let UploadAndInstantiateOutcomeSuccess {
+        address: contract, ..
+    } = suite
+        .upload_and_instantiate(
+            &mut admin,
+            v1,
+            &Empty {},
+            "salt",
+            None::<String>,
+            None,
+            Coins::default(),
+        )
+        .should_succeed();
+
+    let v2_code_hash = suite.upload(&mut admin, v2).should_succeed().code_hash;
+
+    // Admin not set, migrate fails
+    suite
+        .migrate(&mut admin, contract, v2_code_hash, &MigrateMsg::Ok)
+        .should_fail_with_error("sender does not have permission to perform this action");
 }
