dc31a7bd27bc51583058399d8fe6fd25c389f6ea
larry <26318510+larry0x@users.noreply.github.com>
2024-11-29 04:37:19 +0000

    improve/fix some comments

--- dango/types/src/vesting.rs
+++ dango/types/src/vesting.rs
@@ -93,22 +93,23 @@ pub struct InstantiateMsg {
 #[grug::derive(Serde)]
 pub enum ExecuteMsg {
     /// Create a vesting position for a user with the given schedule.
-    /// Sender must attach a non-zero amount of Dango token and nothing else.
-    Create {
-        user: Addr,
-        schedule: Schedule,
-    },
-    // Terminate a user's vesting position.
-    Terminate {
-        user: Addr,
-    },
+    ///
+    /// Sender must be the chain owner, and attach a non-zero amount of Dango
+    /// token and nothing else.
+    Create { user: Addr, schedule: Schedule },
+    /// Terminate a user's vesting position.
+    ///
+    /// Sender must be the chain owner.
+    Terminate { user: Addr },
     /// Claim the withdrawable amount from the vesting position.
+    ///
+    /// Sender must have a non-zero amount of claimable tokens.
     Claim {},
 }
 
 #[grug::derive(Serde, QueryRequest)]
 pub enum QueryMsg {
-    /// Query a single vesting position by index.
+    /// Query a single vesting position by user address.
     #[returns(PositionResponse)]
     Position { user: Addr },
     /// Enumerate all vesting positions.
