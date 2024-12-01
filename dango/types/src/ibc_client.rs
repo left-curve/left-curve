00000000734d409fdadbdf791fb6709a9bca7350
srdtrk <srdtrk@hotmail.com>
2024-12-01 16:48:49 +0800

    imp: refactored and added query response types

--- dango/types/src/ibc_client.rs
+++ dango/types/src/ibc_client.rs
@@ -1,9 +1,10 @@
 //! This crate defines the types required by all IBC light clients.
 
-#![deny(clippy::nursery, clippy::pedantic, warnings, missing_docs)]
+#![deny(clippy::nursery, clippy::pedantic, warnings)]
 
+pub use execute::*;
 use grug::Binary;
-use ibc_proto::ibc::core::client::v1::Height;
+pub use query::*;
 
 /// Instantiate message for all light client contracts
 #[grug::derive(Serde)]
@@ -26,76 +27,115 @@ pub enum ExecuteMsg {
 }
 
 /// Query messages for all light client contracts
-#[grug::derive(Serde)]
+#[grug::derive(Serde, QueryRequest)]
 pub enum QueryMsg {
     /// Query the client status.
+    #[returns(StatusResponse)]
     Status(StatusMsg),
     /// Query the timestamp at a given height.
+    #[returns(TimestampAtHeightResponse)]
     TimestampAtHeight(TimestampAtHeightMsg),
     /// Verify membership of a key-value pair in a Merkle tree.
+    #[returns(MembershipResponse)]
     VerifyMembership(VerifyMembershipMsg),
     /// Verify non-membership of a key-value pair in a Merkle tree.
+    #[returns(MembershipResponse)]
     VerifyNonMembership(VerifyNonMembershipMsg),
 }
 
-/// Status Query message.
-#[grug::derive(Serde)]
-pub struct StatusMsg {}
+mod execute {
+    use super::Binary;
 
-/// Query the timestamp at a given height.
-#[grug::derive(Serde)]
-pub struct TimestampAtHeightMsg {
-    /// The counterparty chain height.
-    pub height: Height,
-}
+    /// Update the client state.
+    #[grug::derive(Serde)]
+    pub struct UpdateClientMsg {
+        /// Update message.
+        pub client_message: Binary,
+    }
 
-/// Verify membership of a key-value pair in a Merkle tree.
-#[grug::derive(Serde)]
-pub struct VerifyMembershipMsg {
-    /// The proof to verify.
-    pub proof: Binary,
-    /// The path at which the value is stored.
-    pub path: Vec<Binary>,
-    /// The value to verify the membership of.
-    pub value: Binary,
-    /// The height of the proof.
-    pub height: Height,
-}
-
-/// Verify non-membership of a key-value pair in a Merkle tree.
-#[grug::derive(Serde)]
-pub struct VerifyNonMembershipMsg {
-    /// The proof to verify.
-    pub proof: Binary,
-    /// The path to verify non-membership at.
-    pub path: Vec<Binary>,
-    /// The height of the proof.
-    pub height: Height,
-}
-
-/// Update the client state.
-#[grug::derive(Serde)]
-pub struct UpdateClientMsg {
-    /// Update message.
-    pub client_message: Binary,
-}
-
-/// Misbehaviour message.
-#[grug::derive(Serde)]
-pub struct MisbehaviourMsg {
     /// Misbehaviour message.
-    pub client_message: Binary,
+    #[grug::derive(Serde)]
+    pub struct MisbehaviourMsg {
+        /// Misbehaviour message.
+        pub client_message: Binary,
+    }
+
+    /// Update client on upgrade.
+    #[grug::derive(Serde)]
+    pub struct UpgradeClientMsg {
+        /// Client state after upgrade.
+        pub upgrade_client_state: Binary,
+        /// Consensus state after upgrade.
+        pub upgrade_consensus_state: Binary,
+        /// Proof of upgrade client state.
+        pub proof_upgrade_client_state: Binary,
+        /// Proof of upgrade consensus state.
+        pub proof_upgrade_consensus_state: Binary,
+    }
 }
 
-/// Update client on upgrade.
-#[grug::derive(Serde)]
-pub struct UpgradeClientMsg {
-    /// Client state after upgrade.
-    pub upgrade_client_state: Binary,
-    /// Consensus state after upgrade.
-    pub upgrade_consensus_state: Binary,
-    /// Proof of upgrade client state.
-    pub proof_upgrade_client_state: Binary,
-    /// Proof of upgrade consensus state.
-    pub proof_upgrade_consensus_state: Binary,
+mod query {
+    use super::Binary;
+    use ibc_proto::ibc::core::client::v1::Height;
+    pub use responses::*;
+
+    /// Status Query message.
+    #[grug::derive(Serde)]
+    pub struct StatusMsg {}
+
+    /// Query the timestamp at a given height.
+    #[grug::derive(Serde)]
+    pub struct TimestampAtHeightMsg {
+        /// The counterparty chain height.
+        pub height: Height,
+    }
+
+    /// Verify membership of a key-value pair in a Merkle tree.
+    #[grug::derive(Serde)]
+    pub struct VerifyMembershipMsg {
+        /// The proof to verify.
+        pub proof: Binary,
+        /// The path at which the value is stored.
+        pub path: Vec<Binary>,
+        /// The value to verify the membership of.
+        pub value: Binary,
+        /// The height of the proof.
+        pub height: Height,
+    }
+
+    /// Verify non-membership of a key-value pair in a Merkle tree.
+    #[grug::derive(Serde)]
+    pub struct VerifyNonMembershipMsg {
+        /// The proof to verify.
+        pub proof: Binary,
+        /// The path to verify non-membership at.
+        pub path: Vec<Binary>,
+        /// The height of the proof.
+        pub height: Height,
+    }
+
+    mod responses {
+        use ibc_core_client_types::Status;
+
+        /// The response to `QueryMsg::Status`
+        #[grug::derive(Serde)]
+        pub struct StatusResponse {
+            /// The status of the client
+            pub status: Status,
+        }
+
+        /// The response to `QueryMsg::TimestampAtHeight`
+        #[grug::derive(Serde)]
+        pub struct TimestampAtHeightResponse {
+            /// The timestamp at the given height
+            pub timestamp: u64,
+        }
+
+        /// The response to both membership queries.
+        #[grug::derive(Serde)]
+        pub struct MembershipResponse {
+            /// The timestamp at height of the proof.
+            pub timestamp: u64,
+        }
+    }
 }
