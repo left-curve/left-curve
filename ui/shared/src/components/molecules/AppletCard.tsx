acc810635052c5ee04af04921f71cdb05d0c9d15
j0nl1 <jonlitech@gmail.com>
2024-12-03 21:30:36 +0000

    fix: colors in command bar

--- ui/shared/src/components/molecules/AppletCard.tsx
+++ ui/shared/src/components/molecules/AppletCard.tsx
@@ -11,13 +11,13 @@ export const AppletCard: React.FC<Props> = ({ metadata }) => {
 
   return (
     <div className="applet-card w-full rounded-[1.25rem] bg-surface-rose-200 flex gap-2 cursor-pointer relative text-black my-2">
-      <div className="w-[100px] bg-white absolute min-h-[80px] rounded-l-[1.25rem] rounded-r-[3.5rem] ] group-data-[selected=true]:w-full group-data-[selected=true]:rounded-[1.25rem] transition-all" />
+      <div className="w-[100px] bg-surface-off-white-200 absolute min-h-[80px] rounded-l-[1.25rem] rounded-r-[3.5rem] ] group-data-[selected=true]:w-full group-data-[selected=true]:rounded-[1.25rem] transition-all" />
       <div className="py-2 pl-4 pr-5 relative rounded-[1.25rem] flex items-center justify-center z-10">
         <img src={img} alt={title} className="h-[54px] w-[54px]" />
       </div>
       <div className="flex flex-col px-5 py-4 relative z-10 flex-1 overflow-hidden">
-        <p className="sm:text-xl font-bold">{title}</p>
-        <p className="text-sm text-gray-500 truncate">{description}</p>
+        <p className="sm:typography-body-l !font-bold">{title}</p>
+        <p className="sm:typography-body-m text-gray-500 truncate">{description}</p>
       </div>
     </div>
   );
