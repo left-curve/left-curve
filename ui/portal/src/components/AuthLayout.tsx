185fcfc6f8ad5ca977527db9f70f002ed1d0e8db
j0nl1 <jonlitech@gmail.com>
2024-12-03 17:46:45 +0000

    fix: auth layout header

--- ui/portal/src/components/AuthLayout.tsx
+++ ui/portal/src/components/AuthLayout.tsx
@@ -14,17 +14,21 @@ export const AuthLayout: React.FC = () => {
   }
 
   return (
-    <main className="flex flex-col min-h-screen w-full h-full bg-surface-off-white-200 relative overflow-y-auto overflow-x-hidden scrollbar-none items-center justify-center">
-      <div className="min-h-full w-full flex-1 flex flex-col justify-center z-10 relative">
-        <div
-          className={twMerge(
-            "header w-full sticky h-[80px] md:h-[130px] top-0 left-0 pt-4 pl-4 md:pt-7 md:pl-12 z-50",
-            isSignup ? "header-signup" : "header-login",
-          )}
-        >
-          <a href="/">
-            <img src="/images/logo.webp" alt="logo" className="h-6 md:h-[31px] object-contain" />
+    <main className="flex flex-col min-h-screen w-full h-full bg-surface-off-white-200 overflow-y-auto overflow-x-hidden scrollbar-none items-center justify-center">
+      <div className="min-h-full w-full flex-1 flex flex-col justify-center z-10">
+        <div className="relative h-[70px] md:h-[112px] px-12 py-7">
+          <a
+            href="/"
+            className="w-full h-full flex items-center justify-center md:justify-start md:items-start"
+          >
+            <img src="/images/dango.svg" alt="logo" className="h-6 md:h-[31px] object-contain" />
           </a>
+          <div
+            className={twMerge(
+              "header w-full absolute h-[75px] md:h-[112px] top-0 left-0 z-[-1]",
+              isSignup ? "header-signup" : "header-login",
+            )}
+          />
         </div>
         <div className="flex flex-1 w-full items-center justify-center p-4">
           <Outlet />
