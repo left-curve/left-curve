c91736110065c4789d82b9185cfb090ef4a10322
j0nl1 <jonlitech@gmail.com>
2024-12-03 18:48:51 +0000

    feat: container size transition in signup and login

--- ui/portal/src/components/WizardSignup/Wrapper.tsx
+++ ui/portal/src/components/WizardSignup/Wrapper.tsx
@@ -1,4 +1,5 @@
-import { Button, twMerge, useWizard } from "@dango/shared";
+import { Button, twMerge, useMeasure, useWizard } from "@dango/shared";
+import { motion } from "framer-motion";
 import type React from "react";
 import type { PropsWithChildren } from "react";
 import { useNavigate } from "react-router-dom";
@@ -6,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 export const WizardSignupWrapper: React.FC<PropsWithChildren> = ({ children }) => {
   const { activeStep, previousStep } = useWizard();
   const navigate = useNavigate();
+  const [containerRef, { height }] = useMeasure<HTMLDivElement>();
 
   const isFirstStep = activeStep === 0;
   const isSecondStep = activeStep === 1;
@@ -13,54 +15,64 @@ export const WizardSignupWrapper: React.FC<PropsWithChildren> = ({ children }) =
 
   return (
     <div className="flex flex-col items-center justify-center w-full gap-8">
-      <div className="flex flex-col items-center w-full bg-surface-rose-100 px-8 py-6 rounded-3xl max-w-2xl gap-12 shadow-md">
-        <div className="flex flex-col gap-8 md:gap-10 w-full">
-          <div className="flex flex-col gap-4 items-center">
-            <p className="text-typography-rose-700 typography-headline-m uppercase">
-              {[0, 1].includes(activeStep) ? "sign up" : null}
-              {isThirdStep ? "new spot account" : null}
-            </p>
-            <p className="text-typography-rose-600 typography-body-l text-center">
-              {isFirstStep
-                ? "Choose your username. It will be public onchain and cannot be changed afterwards."
-                : null}
-              {isSecondStep
-                ? "Choose a sign-in credential. You can add or remove credentials afterwards."
-                : null}
-              {isThirdStep
-                ? "Fund your first spot account with USDC from other existing wallets of yours."
-                : null}
-            </p>
+      <motion.div
+        transition={{ duration: 0.3 }}
+        animate={{ height }}
+        className="overflow-hidden w-full bg-surface-rose-100 rounded-3xl max-w-2xl shadow-md"
+      >
+        <div ref={containerRef}>
+          <div className="flex flex-col items-center px-8 py-6 gap-12">
+            <div className="flex flex-col gap-8 md:gap-10 w-full">
+              <div className="flex flex-col gap-4 items-center">
+                <p className="text-typography-rose-700 typography-headline-m uppercase">
+                  {[0, 1].includes(activeStep) ? "sign up" : null}
+                  {isThirdStep ? "new spot account" : null}
+                </p>
+                <p className="text-typography-rose-600 typography-body-l text-center">
+                  {isFirstStep
+                    ? "Choose your username. It will be public onchain and cannot be changed afterwards."
+                    : null}
+                  {isSecondStep
+                    ? "Choose a log in credential. You can add or remove credentials afterwards."
+                    : null}
+                  {isThirdStep
+                    ? "Fund your first spot account with USDC from other existing wallets of yours."
+                    : null}
+                </p>
+              </div>
+
+              <div className="flex flex-1 justify-center items-center w-full">{children}</div>
+            </div>
+            <div className="flex gap-8">
+              <p
+                className={twMerge(
+                  "text-[10px] font-semibold tracking-[0.125rem]",
+                  isFirstStep ? "text-typography-purple-400" : "text-typography-purple-300",
+                )}
+              >
+                1 USERNAME
+              </p>
+              <p
+                className={twMerge(
+                  "text-[10px] font-semibold tracking-[0.125rem]",
+                  isSecondStep ? "text-typography-purple-400" : "text-typography-purple-300",
+                )}
+              >
+                2 CREDENTIAL
+              </p>
+              <p
+                className={twMerge(
+                  "text-[10px] font-semibold tracking-[0.125rem]",
+                  isThirdStep ? "text-typography-purple-400" : "text-typography-purple-300",
+                )}
+              >
+                3 DEPOSIT
+              </p>
+            </div>
           </div>
-          <div className="flex flex-1 justify-center items-center w-full">{children}</div>
         </div>
-        <div className="flex gap-8">
-          <p
-            className={twMerge(
-              "text-[10px] font-semibold tracking-[0.125rem]",
-              isFirstStep ? "text-typography-purple-400" : "text-typography-purple-300",
-            )}
-          >
-            1 USERNAME
-          </p>
-          <p
-            className={twMerge(
-              "text-[10px] font-semibold tracking-[0.125rem]",
-              isSecondStep ? "text-typography-purple-400" : "text-typography-purple-300",
-            )}
-          >
-            2 CREDENTIAL
-          </p>
-          <p
-            className={twMerge(
-              "text-[10px] font-semibold tracking-[0.125rem]",
-              isThirdStep ? "text-typography-purple-400" : "text-typography-purple-300",
-            )}
-          >
-            3 DEPOSIT
-          </p>
-        </div>
-      </div>
+      </motion.div>
+
       <Button
         type="button"
         variant="light"
