b69bac84ef69ef3dd513b89c50e9e75cfadaa099
j0nl1 <jonlitech@gmail.com>
2024-12-03 18:48:01 +0000

    feat: login and signup animation

--- ui/portal/src/views/Login.tsx
+++ ui/portal/src/views/Login.tsx
@@ -1,7 +1,17 @@
+import { motion } from "framer-motion";
 import { WizardLogin } from "~/components/WizardLogin";
 
 const Login: React.FC = () => {
-  return <WizardLogin />;
+  return (
+    <motion.div
+      className="flex flex-1 w-full h-full"
+      animate={{ height: "auto" }}
+      initial={{ height: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <WizardLogin />
+    </motion.div>
+  );
 };
 
 export default Login;
--- ui/portal/src/views/Signup.tsx
+++ ui/portal/src/views/Signup.tsx
@@ -1,7 +1,17 @@
+import { motion } from "framer-motion";
 import { WizardSignup } from "~/components/WizardSignup";
 
 const Signup: React.FC = () => {
-  return <WizardSignup />;
+  return (
+    <motion.div
+      className="flex flex-1 w-full h-full"
+      animate={{ height: "auto" }}
+      initial={{ height: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <WizardSignup />
+    </motion.div>
+  );
 };
 
 export default Signup;
