7b1ba4fb00c3b9f9da6daff1aff9c8114f51f3c3
larry <26318510+larry0x@users.noreply.github.com>
2024-12-10 20:01:33 +0000

    format some macros

--- grug/app/src/event.rs
+++ grug/app/src/event.rs
@@ -101,33 +101,39 @@ impl From<EventResult<Event>> for HandleEventStatus {
 
 #[macro_export]
 macro_rules! catch_event {
-    ($evt: expr, $block: block) => {{
+    ($evt: expr, $block: block) => {
         match (|| $block)() {
             Ok(val) => val,
-            Err(err) => return $crate::EventResult::Err {
-                event: $evt,
-                error: err,
+            Err(err) => {
+                return $crate::EventResult::Err {
+                    event: $evt,
+                    error: err,
+                };
             },
         }
-    }};
+    };
 }
 
 #[macro_export]
 macro_rules! update_event_field {
     ($result: expr, $evt: expr => $field: ident) => {
         match $result {
-            EventResult::Ok(i) => $evt.$field = grug_types::EventStatus::Ok(i),
+            EventResult::Ok(i) => {
+                $evt.$field = grug_types::EventStatus::Ok(i);
+            },
             EventResult::Err { event, error } => {
                 $evt.$field = grug_types::EventStatus::Failed {
                     event,
                     error: error.to_string(),
                 };
+
                 return EventResult::SubErr { event: $evt, error };
             },
             EventResult::SubErr { event, error } => {
                 $evt.$field = grug_types::EventStatus::NestedFailed(event);
+
                 return EventResult::SubErr { event: $evt, error };
             },
-        };
+        }
     };
 }
