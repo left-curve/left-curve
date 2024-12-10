9aaa4f52c742dc61af8d6b2cc6ff6cdb63e0736a
larry <26318510+larry0x@users.noreply.github.com>
2024-12-10 21:47:22 +0000

    reorder some methods

--- grug/app/src/event.rs
+++ grug/app/src/event.rs
@@ -11,64 +11,12 @@ pub enum EventResult<T> {
 }
 
 impl<T> EventResult<T> {
-    /// Create a new `EventResult<R>`.
-    pub fn map_merge<R, C>(self, merge: R, callback: C) -> EventResult<R>
-    where
-        C: Fn(T, R) -> R,
-    {
-        match self {
-            EventResult::Ok(event) => EventResult::Ok(callback(event, merge)),
-            EventResult::Err { event, error } | EventResult::SubErr { event, error } => {
-                EventResult::SubErr {
-                    event: callback(event, merge),
-                    error,
-                }
-            },
-        }
-    }
-
     pub fn err(event: T, error: AppError) -> Self {
         EventResult::Err { event, error }
     }
 
-    pub fn debug<O>(&self, ok_closure: O, error_msg: &str)
-    where
-        O: Fn(&T),
-    {
-        match self {
-            EventResult::Ok(val) => ok_closure(val),
-            EventResult::Err { error, .. } => {
-                tracing::warn!(err = error.to_string(), error_msg);
-            },
-            EventResult::SubErr { .. } => {
-                tracing::warn!("Sub Error encountered");
-            },
-        }
-    }
-
-    pub fn as_result(self) -> Result<T, (T, AppError)> {
-        match self {
-            EventResult::Ok(val) => Ok(val),
-            EventResult::Err { event, error } | EventResult::SubErr { event, error } => {
-                Err((event, error))
-            },
-        }
-    }
-
     pub fn is_ok(&self) -> bool {
         matches!(self, EventResult::Ok(_))
-    }
-
-    pub fn as_committment(self) -> CommitmentStatus<T> {
-        match self {
-            EventResult::Ok(event) => CommitmentStatus::Committed(event),
-            EventResult::Err { event, error } | EventResult::SubErr { event, error } => {
-                CommitmentStatus::Failed {
-                    event,
-                    error: error.to_string(),
-                }
-            },
-        }
     }
 
     pub fn map<C, R>(self, callback: C) -> EventResult<R>
@@ -84,6 +32,57 @@ impl<T> EventResult<T> {
             EventResult::SubErr { event, error } => EventResult::SubErr {
                 event: callback(event),
                 error,
+            },
+        }
+    }
+
+    pub fn map_merge<R, C>(self, merge: R, callback: C) -> EventResult<R>
+    where
+        C: Fn(T, R) -> R,
+    {
+        match self {
+            EventResult::Ok(event) => EventResult::Ok(callback(event, merge)),
+            EventResult::Err { event, error } | EventResult::SubErr { event, error } => {
+                EventResult::SubErr {
+                    event: callback(event, merge),
+                    error,
+                }
+            },
+        }
+    }
+
+    pub fn as_result(self) -> Result<T, (T, AppError)> {
+        match self {
+            EventResult::Ok(val) => Ok(val),
+            EventResult::Err { event, error } | EventResult::SubErr { event, error } => {
+                Err((event, error))
+            },
+        }
+    }
+
+    pub fn as_committment(self) -> CommitmentStatus<T> {
+        match self {
+            EventResult::Ok(event) => CommitmentStatus::Committed(event),
+            EventResult::Err { event, error } | EventResult::SubErr { event, error } => {
+                CommitmentStatus::Failed {
+                    event,
+                    error: error.to_string(),
+                }
+            },
+        }
+    }
+
+    pub fn debug<O>(&self, ok_closure: O, error_msg: &str)
+    where
+        O: Fn(&T),
+    {
+        match self {
+            EventResult::Ok(val) => ok_closure(val),
+            EventResult::Err { error, .. } => {
+                tracing::warn!(err = error.to_string(), error_msg);
+            },
+            EventResult::SubErr { .. } => {
+                tracing::warn!("Sub Error encountered");
             },
         }
     }
