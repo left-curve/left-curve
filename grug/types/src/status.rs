3785f24e87fee19068626a6b114fee200ccf6463
larry <26318510+larry0x@users.noreply.github.com>
2024-12-11 16:59:47 +0000

    add comments

--- grug/types/src/status.rs
+++ grug/types/src/status.rs
@@ -4,12 +4,21 @@ use {
     serde::{Deserialize, Serialize},
 };
 
+/// Describes whether a set of states changes have been committed to the chain
+/// state.
 #[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum CommitmentStatus<T> {
+    /// The state changes have been committed.
     Committed(T),
+    /// The state changes have been discarded because its execution failed.
     Failed { event: T, error: String },
+    /// The state changes have been discarded, despite its execution was
+    /// successful, but some other parts of the transaction execution flow
+    /// failed; specifically, the `finalize_fee` call on taxman.
     Reverted { event: T, revert_by: String },
+    /// The execution was not reached because earlier parts of the transaction
+    /// execution flow failed.
     NotReached,
 }
 
