e0f0928f8262ae7d5920bfc2d4d6099a29eaa853
larry <26318510+larry0x@users.noreply.github.com>
2024-12-14 02:32:06 +0000

    add hybrid vm

---
+++ grug/vm-hybrid/src/lib.rs
@@ -0,0 +1,145 @@
+use {
+    grug_app::{GasTracker, Instance, QuerierProvider, StorageProvider, Vm},
+    grug_types::{Context, Hash256},
+    grug_vm_rust::{RustInstance, RustVm},
+    grug_vm_wasm::{WasmInstance, WasmVm},
+    std::collections::HashSet,
+    thiserror::Error,
+};
+
+#[derive(Debug, Error)]
+pub enum VmError {
+    #[error("RustVm error: {0}")]
+    Rust(#[from] grug_vm_rust::VmError),
+
+    #[error("WasmVm error: {0}")]
+    Wasm(#[from] grug_vm_wasm::VmError),
+}
+
+#[derive(Clone)]
+pub struct HybridVm {
+    pub rust: RustVm,
+    pub wasm: WasmVm,
+    /// A set of code hashes that will be run in the Rust VM.
+    /// Any code hash that's not in this set will be run in the Wasm VM.
+    pub code_hashes_for_rust: HashSet<Hash256>,
+}
+
+impl HybridVm {
+    pub fn new<T>(wasm_cache_capacity: usize, code_hashes_for_rust: T) -> Self
+    where
+        T: Into<HashSet<Hash256>>,
+    {
+        Self {
+            rust: RustVm::new(),
+            wasm: WasmVm::new(wasm_cache_capacity),
+            code_hashes_for_rust: code_hashes_for_rust.into(),
+        }
+    }
+}
+
+impl Vm for HybridVm {
+    type Error = VmError;
+    type Instance = HybridInstance;
+
+    fn build_instance(
+        &mut self,
+        code: &[u8],
+        code_hash: Hash256,
+        storage: StorageProvider,
+        state_mutable: bool,
+        querier: QuerierProvider<Self>,
+        query_depth: usize,
+        gas_tracker: GasTracker,
+    ) -> Result<Self::Instance, Self::Error> {
+        if self.code_hashes_for_rust.contains(&code_hash) {
+            let instance = self.rust.build_instance(
+                code,
+                code_hash,
+                storage,
+                state_mutable,
+                querier,
+                query_depth,
+                gas_tracker,
+            )?;
+            Ok(HybridInstance::Rust(instance))
+        } else {
+            let instance = self.wasm.build_instance(
+                code,
+                code_hash,
+                storage,
+                state_mutable,
+                querier,
+                query_depth,
+                gas_tracker,
+            )?;
+            Ok(HybridInstance::Wasm(instance))
+        }
+    }
+}
+
+pub enum HybridInstance {
+    Rust(RustInstance),
+    Wasm(WasmInstance),
+}
+
+impl Instance for HybridInstance {
+    type Error = VmError;
+
+    fn call_in_0_out_1(self, name: &'static str, ctx: &Context) -> Result<Vec<u8>, Self::Error> {
+        match self {
+            HybridInstance::Rust(instance) => {
+                let res = instance.call_in_0_out_1(name, ctx)?;
+                Ok(res)
+            },
+            HybridInstance::Wasm(instance) => {
+                let res = instance.call_in_0_out_1(name, ctx)?;
+                Ok(res)
+            },
+        }
+    }
+
+    fn call_in_1_out_1<P>(
+        self,
+        name: &'static str,
+        ctx: &Context,
+        param: &P,
+    ) -> Result<Vec<u8>, Self::Error>
+    where
+        P: AsRef<[u8]>,
+    {
+        match self {
+            HybridInstance::Rust(instance) => {
+                let res = instance.call_in_1_out_1(name, ctx, param)?;
+                Ok(res)
+            },
+            HybridInstance::Wasm(instance) => {
+                let res = instance.call_in_1_out_1(name, ctx, param)?;
+                Ok(res)
+            },
+        }
+    }
+
+    fn call_in_2_out_1<P1, P2>(
+        self,
+        name: &'static str,
+        ctx: &Context,
+        param1: &P1,
+        param2: &P2,
+    ) -> Result<Vec<u8>, Self::Error>
+    where
+        P1: AsRef<[u8]>,
+        P2: AsRef<[u8]>,
+    {
+        match self {
+            HybridInstance::Rust(instance) => {
+                let res = instance.call_in_2_out_1(name, ctx, param1, param2)?;
+                Ok(res)
+            },
+            HybridInstance::Wasm(instance) => {
+                let res = instance.call_in_2_out_1(name, ctx, param1, param2)?;
+                Ok(res)
+            },
+        }
+    }
+}
