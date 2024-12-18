6b6243cccf69536d9c54609b44b7ad2838776cca
j0nl1 <jonlitech@gmail.com>
2024-12-18 22:37:42 +0000

    fix: private routes

--- ui/portal/src/AppRouter.tsx
+++ ui/portal/src/AppRouter.tsx
@@ -1,12 +1,118 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  Outlet,
+  RouterProvider,
+  createRootRouteWithContext,
+  createRoute,
+  createRouter,
+} from "@tanstack/react-router";
 
 import { useAccount, useConfig, usePublicClient } from "@left-curve/react";
 
-import { MainRouteWithChildren } from "./pages/layout";
+import { AccountsRoute } from "./pages/accounts";
+import { AuthRoute } from "./pages/auth";
+
+import { Spinner } from "@dango/shared";
+import { AppLayout } from "./components/AppLayout";
+
+import type {
+  UseAccountReturnType,
+  UseConfigReturnType,
+  UsePublicClientReturnType,
+} from "@left-curve/react";
+
+export const AppRoute = createRoute({
+  id: "app-layout",
+  getParentRoute: () => RootRouter,
+  component: () => {
+    return (
+      <>
+        <AppLayout>
+          <Outlet />
+        </AppLayout>
+      </>
+    );
+  },
+});
+
+export const AppRouteWithChildren = AppRoute.addChildren([
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "/",
+  }),
+
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "/accounts",
+  }).update(AccountsRoute),
+
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "/transfer",
+  }).lazy(() => import("./pages/transfer").then((d) => d.TransferRoute)),
+
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "/swap",
+  }).lazy(() => import("./pages/swap").then((d) => d.SwapRoute)),
+
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "/block-explorer",
+  }).lazy(() => import("./pages/block-explorer").then((d) => d.BlockExplorerRoute)),
+
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "/amm",
+  }).lazy(() => import("./pages/amm").then((d) => d.AmmRoute)),
+
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "/account-creation",
+  }).lazy(() => import("./pages/account-creation").then((d) => d.AccountCreationRoute)),
+
+  createRoute({
+    getParentRoute: () => AppRoute,
+    path: "$",
+    component: () => {
+      return (
+        <div className="w-full flex flex-1 justify-center items-center p-4">
+          <h3 className="text-center max-w-4xl typography-display-xs md:typography-display-xl">
+            Sorry, we couldn't find the page you were looking for.
+          </h3>
+        </div>
+      );
+    },
+  }),
+]);
+
+export interface RouterContext {
+  client?: UsePublicClientReturnType;
+  account?: UseAccountReturnType;
+  config?: UseConfigReturnType;
+}
+
+export const RootRouter = createRootRouteWithContext<RouterContext>()({
+  beforeLoad: async ({ context }) => {
+    const { config } = context;
+    if (!config?.state.isMipdLoaded) {
+      await new Promise((resolve) => {
+        config?.subscribe(
+          (x) => x.isMipdLoaded,
+          (isMipdLoaded) => isMipdLoaded && resolve(null),
+        );
+      });
+    }
+  },
+});
 
 const router = createRouter({
-  routeTree: MainRouteWithChildren,
+  routeTree: RootRouter.addChildren([AppRoute, AuthRoute]),
   defaultPreload: "intent",
+  defaultPendingComponent: () => (
+    <div className="h-screen w-full flex justify-center items-center">
+      <Spinner size="lg" color="pink" />
+    </div>
+  ),
 });
 
 export const AppRouter: React.FC = () => {
