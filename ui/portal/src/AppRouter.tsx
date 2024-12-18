1379be6b88c12ade5a335204bb301b33e7c57db4
j0nl1 <jonlitech@gmail.com>
2024-12-18 20:12:32 +0000

    feat: ts router config

--- ui/portal/src/AppRouter.tsx
+++ ui/portal/src/AppRouter.tsx
@@ -1,47 +1,18 @@
-import { Spinner } from "@dango/shared";
-import { Suspense, lazy } from "react";
-import { Route, Routes } from "react-router-dom";
-import { AppLayout } from "./components/AppLayout";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import { NotFoundView } from "./views/NotFound";
+import { useAccount, useConfig, usePublicClient } from "@left-curve/react";
 
-// Auth routes
-const AuthView = lazy(() => import(/* webpackPrefetch: true */ "./views/Auth"));
+import { MainRouteWithChildren } from "./pages/layout";
 
-// Portal routes
-const AccountView = lazy(() => import(/* webpackPrefetch: true */ "./views/Account"));
-const TransferView = lazy(() => import(/* webpackPrefetch: true */ "./views/Transfer"));
-const SwapView = lazy(() => import(/* webpackPrefetch: true */ "./views/Swap"));
-const PoolView = lazy(() => import(/* webpackPrefetch: true */ "./views/Pool"));
-const BlockExplorerView = lazy(() => import(/* webpackPrefetch: true */ "./views/BlockExplorer"));
-const AccountCreationView = lazy(
-  () => import(/* webpackPrefetch: true */ "./views/AccountCreation"),
-);
+const router = createRouter({
+  routeTree: MainRouteWithChildren,
+  defaultPreload: "intent",
+});
 
 export const AppRouter: React.FC = () => {
-  return (
-    <Suspense
-      fallback={
-        <div className="h-screen w-full flex justify-center items-center bg-surface-off-white-200">
-          <Spinner size="lg" color="pink" />
-        </div>
-      }
-    >
-      <Routes>
-        <Route path="/auth">
-          <Route path="login" element={<AuthView />} />
-          <Route path="signup" element={<AuthView />} />
-        </Route>
-        <Route path="/" element={<AppLayout />}>
-          <Route path="accounts" element={<AccountView />} />
-          <Route path="/account-creation" element={<AccountCreationView />} />
-          <Route path="/block-explorer" element={<BlockExplorerView />} />
-          <Route path="/transfer" element={<TransferView />} />
-          <Route path="/swap" element={<SwapView />} />
-          <Route path="/amm" element={<PoolView />} />
-          <Route path="*" element={<NotFoundView />} />
-        </Route>
-      </Routes>
-    </Suspense>
-  );
+  const account = useAccount();
+  const config = useConfig();
+  const client = usePublicClient();
+
+  return <RouterProvider router={router} context={{ account, config, client }} />;
 };
