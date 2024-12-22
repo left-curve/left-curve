75f2a91d5f613c515fe09840676f4b052070be37
larry <26318510+larry0x@users.noreply.github.com>
2024-12-22 05:33:03 +0000

    add placeholder ism contract

---
+++ hyperlane/ism/src/lib.rs
@@ -0,0 +1,33 @@
+use {
+    grug::{Empty, HexBinary, ImmutableCtx, Json, JsonSerExt, MutableCtx, Response, StdResult},
+    hyperlane_types::ism::QueryMsg,
+};
+
+#[cfg_attr(not(feature = "library"), grug::export)]
+pub fn instantiate(_ctx: MutableCtx, _msg: Empty) -> StdResult<Response> {
+    Ok(Response::new())
+}
+
+#[cfg_attr(not(feature = "library"), grug::export)]
+pub fn query(ctx: ImmutableCtx, query: QueryMsg) -> StdResult<Json> {
+    match query {
+        QueryMsg::Verify {
+            raw_message,
+            metadata,
+        } => {
+            let res = query_verify(ctx, raw_message, metadata)?;
+            res.to_json_value()
+        },
+    }
+}
+
+#[inline]
+fn query_verify(
+    _ctx: ImmutableCtx,
+    _raw_message: HexBinary,
+    _metadata: HexBinary,
+) -> StdResult<()> {
+    // TODO
+
+    Ok(())
+}
