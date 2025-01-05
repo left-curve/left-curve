0c83d79c8192a52029799b7762214d858c9559d6
larry <26318510+larry0x@users.noreply.github.com>
2025-01-05 01:29:38 +0000

    add main function

--- grug/vm/rust/examples/threads.rs
+++ grug/vm/rust/examples/threads.rs
@@ -17,14 +17,23 @@
 //! prefix is the trading pair ({base_denom, quote_denom}) and the KV pairs are
 //! the orders. We want to clear orders in multiple trading pairs in parallel.
 
+use {
+    grug_testing::TestBuilder,
+    grug_types::{btree_map, BorshDeExt, Coins, ResultExt},
+    grug_vm_rust::ContractBuilder,
+    rand::Rng,
+    std::collections::BTreeMap,
+};
+
 mod example {
     use {
         grug_storage::{Item, Map},
         grug_types::{MutableCtx, Order, Response, StdResult, Storage},
+        serde::{Deserialize, Serialize},
         std::{collections::BTreeMap, thread},
     };
 
-    pub const PREFIXES: [&str; 5] = ["alice", "bob", "charlie", "dave", "eve"];
+    pub const PREFIXES: [&str; 5] = ["a", "b", "c", "d", "e"];
 
     pub const DATA: Map<(&str, u64), u64> = Map::new("data");
 
@@ -32,10 +41,12 @@ mod example {
 
     pub const SINGLE_THREAD_RESULT: Item<BTreeMap<String, u64>> = Item::new("single_thread_result");
 
+    #[derive(Serialize, Deserialize)]
     pub struct InstantiateMsg {
-        pub data: BTreeMap<&'static str, BTreeMap<u64, u64>>,
+        pub data: BTreeMap<String, BTreeMap<u64, u64>>,
     }
 
+    #[derive(Serialize, Deserialize)]
     pub enum ExecuteMsg {
         ComputeMultiThread {},
         ComputeSingleThread {},
@@ -44,7 +55,7 @@ mod example {
     pub fn instantiate(ctx: MutableCtx, msg: InstantiateMsg) -> StdResult<Response> {
         for (prefix, keys) in msg.data {
             for (key, value) in keys {
-                DATA.save(ctx.storage, (prefix, key), &value)?;
+                DATA.save(ctx.storage, (&prefix, key), &value)?;
             }
         }
 
@@ -85,6 +96,7 @@ mod example {
                     // can be shared among threads.
                     let storage = ctx.storage as &dyn Storage;
                     s.spawn(move || {
+                        println!("thread started! prefix: {prefix}");
                         DATA.prefix(prefix)
                             .values(storage, None, None, Order::Ascending)
                             .try_fold(0, |sum, x| x.map(|x| sum + x))
@@ -102,4 +114,94 @@ mod example {
     }
 }
 
-fn main() {}
+fn timed<F, R>(name: &str, f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    println!("starting... name: {name}");
+    let start = std::time::Instant::now();
+    let out = f();
+    let elapsed = start.elapsed();
+    println!("done! elapsed: {elapsed:?}");
+    out
+}
+
+fn generate_random_data() -> BTreeMap<u64, u64> {
+    let mut rng = rand::thread_rng();
+    let count = rng.gen_range(50000..100000);
+    (0..count).map(|k| (k, rng.gen_range(0..100))).collect()
+}
+
+fn main() {
+    let (mut suite, mut accounts) = TestBuilder::new()
+        .add_account("sender", Coins::new())
+        .set_owner("sender")
+        .build();
+
+    let example_code = ContractBuilder::new(Box::new(example::instantiate))
+        .with_execute(Box::new(example::execute))
+        .build();
+
+    let example = timed("instantiate", || {
+        suite
+            .upload_and_instantiate(
+                &mut accounts["sender"],
+                example_code,
+                &example::InstantiateMsg {
+                    data: btree_map! {
+                        "a".to_string() => generate_random_data(),
+                        "b".to_string() => generate_random_data(),
+                        "c".to_string() => generate_random_data(),
+                        "d".to_string() => generate_random_data(),
+                        "e".to_string() => generate_random_data(),
+                    },
+                },
+                "example",
+                Some("example"),
+                None,
+                Coins::new(),
+            )
+            .should_succeed()
+            .address
+    });
+
+    timed("compute_single_thread", || {
+        suite
+            .execute(
+                &mut accounts["sender"],
+                example,
+                &example::ExecuteMsg::ComputeSingleThread {},
+                Coins::new(),
+            )
+            .should_succeed();
+    });
+
+    timed("compute_multi_thread", || {
+        suite
+            .execute(
+                &mut accounts["sender"],
+                example,
+                &example::ExecuteMsg::ComputeMultiThread {},
+                Coins::new(),
+            )
+            .should_succeed();
+    });
+
+    let single_thread_result = suite
+        .query_wasm_raw(example, example::SINGLE_THREAD_RESULT.path())
+        .should_succeed()
+        .unwrap()
+        .deserialize_borsh::<BTreeMap<String, u64>>()
+        .unwrap();
+    let multi_thread_result = suite
+        .query_wasm_raw(example, example::MULTI_THREAD_RESULT.path())
+        .should_succeed()
+        .unwrap()
+        .deserialize_borsh::<BTreeMap<String, u64>>()
+        .unwrap();
+
+    dbg!(&single_thread_result);
+    dbg!(&multi_thread_result);
+
+    assert_eq!(single_thread_result, multi_thread_result);
+}
