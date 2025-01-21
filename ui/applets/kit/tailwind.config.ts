8260fe056903e9f403d3a7a32032218ea0139723
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2025-01-21 17:37:58 +0100

    feat(ui): portal app package (#426)
    * feat: portal-app
    
    * refactor: adjust shared library for portal and rename dango packages
    
    * chore: update ui readme
    
    * feat: applets-kit
    
    * fix: tw path
    
    * chore: update pipeline paths
    
    * chore: update ui readme
    
    * fix: build command portal web

---
+++ ui/applets/kit/tailwind.config.ts
@@ -0,0 +1,15 @@
+import { tailwindConfig } from "@left-curve/ui-config/tailwind/config.js";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./.storybook/**/*.{js,ts,jsx,tsx,stories.tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,stories.tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  presets: [tailwindConfig],
+};
+
+export default config;
