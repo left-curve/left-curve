393fea7006609c3941fc89d956766504e39d1808
j0nl1 <jonlitech@gmail.com>
2025-01-22 01:36:11 +0000

    fix: build command for portal web

--- ui/store/react/src/hooks/useConfig.ts
+++ ui/store/react/src/hooks/useConfig.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useContext } from "react";
-import { GrunnectContext } from "../context.js";
+import { DangoStoreContext } from "../context.js";
 
 import type { Config } from "@left-curve/store/types";
 
@@ -12,7 +12,7 @@ export type UseConfigReturnType<config extends Config = Config> = config;
 export function useConfig<config extends Config = Config>(
   parameters: UseConfigParameters<config> = {},
 ): UseConfigReturnType<config> {
-  const config = parameters.config ?? useContext(GrunnectContext);
+  const config = parameters.config ?? useContext(DangoStoreContext);
   if (!config) throw new Error("GrunnectProvider not found");
   return config as UseConfigReturnType<config>;
 }
