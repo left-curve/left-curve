d5294452c6ae75b80db93164763e740e4fd7fa66
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2025-01-23 11:18:29 +0100

    refactor: split lending types into separate files

---
+++ dango/types/src/lending/market.rs
@@ -0,0 +1,11 @@
+/// Configurations and state of a market.
+#[grug::derive(Serde, Borsh)]
+pub struct Market {
+    // TODO
+}
+
+/// A set of updates to be applied to a market.
+#[grug::derive(Serde)]
+pub struct MarketUpdates {
+    // TODO
+}
