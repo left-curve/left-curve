4006def2addd2a9162557421bd102f47933cad4b
Zexsor33 <c.mirco89@gmail.com>
2025-01-27 17:24:29 +0100

    fix conflicts

--- dango/cli/src/start.rs
+++ dango/cli/src/start.rs
@@ -83,6 +83,7 @@ impl StartCmd {
             codes.hyperlane.ism.to_bytes().hash256(),
             codes.hyperlane.mailbox.to_bytes().hash256(),
             codes.hyperlane.merkle.to_bytes().hash256(),
+            codes.hyperlane.va.to_bytes().hash256(),
             codes.ibc_transfer.to_bytes().hash256(),
             codes.lending.to_bytes().hash256(),
             codes.oracle.to_bytes().hash256(),
