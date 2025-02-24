f10dc3aad03d4f11d36fdfffc5b5cc19c9fbd6fd
j0nl1 <jonlitech@gmail.com>
2025-02-24 17:57:26 +0000

    fix: missing carousel in signup

--- ui/portal/web/src/components/signup/SignupWrapper.tsx
+++ ui/portal/web/src/components/signup/SignupWrapper.tsx
@@ -1,6 +1,7 @@
 import {
   Birdo,
   Button,
+  Carousel,
   IconLeft,
   ResizerContainer,
   Stepper,
@@ -85,12 +86,32 @@ export const SignupWrapper: React.FC<React.PropsWithChildren> = ({ children }) =
           </div>
         </ResizerContainer>
       </div>
-      <div className="h-full min-w-[720px] w-[720px] hidden xl:flex bg-[url('./images/frame-rounded.svg')] bg-no-repeat bg-cover bg-center items-center justify-center gap-12 flex-col">
-        <Birdo className="max-w-[450px] h-auto" />
-        <div className="flex flex-col items-center justify-center gap-1">
-          <h3 className="exposure-h3-italic">Welcome home</h3>
-          <p className="text-gray-500 text-md">The good old days are here to stay.</p>
-        </div>
+      <div className="h-full min-w-[720px] w-[720px] hidden xl:flex bg-[url('./images/frame-rounded.svg')] bg-no-repeat bg-cover bg-center items-center justify-center">
+        <Carousel>
+          <div className="flex flex-col items-center justify-center gap-12">
+            <Birdo className="max-w-[28.125rem] h-auto" />
+            <div className="flex flex-col items-center justify-center gap-1 max-w-[25rem]">
+              <h3 className="exposure-h3-italic">Welcome home</h3>
+              <p className="text-gray-500 text-md">The good old days are here to stay.</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-12">
+            <Birdo className="max-w-[28.125rem] h-auto" />
+            <div className="flex flex-col items-center justify-center gap-1 max-w-[25rem]">
+              <h3 className="exposure-h3-italic">Use Dango</h3>
+              <p className="text-gray-500 text-md">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-12">
+            <Birdo className="max-w-[28.125rem] h-auto" />
+            <div className="flex flex-col items-center justify-center gap-1 max-w-[25rem]">
+              <h3 className="exposure-h3-italic">How to use it</h3>
+              <p className="text-gray-500 text-md">Fusce purus justo, lobortis aliquet orci.</p>
+            </div>
+          </div>
+        </Carousel>
       </div>
     </div>
   );
