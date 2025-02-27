af5c750aae53f410b9498cc54d58dada8bdae592
larry <26318510+larry0x@users.noreply.github.com>
2025-02-27 21:10:40 +0000

    dex refactor

--- dango/dex/src/query.rs
+++ dango/dex/src/query.rs
@@ -1,8 +1,8 @@
 use {
     crate::{ORDERS, PAIRS},
     dango_types::dex::{
-        OrderId, OrderResponse, OrdersByPairResponse, OrdersByUserResponse, Pair, PairParams,
-        PairUpdate, QueryMsg,
+        OrderId, OrderResponse, OrdersByPairResponse, OrdersByUserResponse, PairPageParams,
+        PairParams, PairUpdate, QueryMsg,
     },
     grug::{
         Addr, Bound, Denom, ImmutableCtx, Json, JsonSerExt, Order as IterationOrder, StdResult,
@@ -62,7 +62,7 @@ fn query_pair(ctx: ImmutableCtx, base_denom: Denom, quote_denom: Denom) -> StdRe
 #[inline]
 fn query_pairs(
     ctx: ImmutableCtx,
-    start_after: Option<Pair>,
+    start_after: Option<PairPageParams>,
     limit: Option<u32>,
 ) -> StdResult<Vec<PairUpdate>> {
     let start = start_after
