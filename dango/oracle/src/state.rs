3ce35e9249018dfb6b50b6db300b7b992e648e6f
larry <26318510+larry0x@users.noreply.github.com>
2025-02-27 21:24:30 +0000

    move PRICES map from types to oracle

--- dango/oracle/src/state.rs
+++ dango/oracle/src/state.rs
@@ -1,8 +1,10 @@
 use {
-    dango_types::oracle::{GuardianSet, GuardianSetIndex, PriceSource},
+    dango_types::oracle::{GuardianSet, GuardianSetIndex, PrecisionlessPrice, PriceSource, PythId},
     grug::{Denom, Map},
 };
 
 pub const GUARDIAN_SETS: Map<GuardianSetIndex, GuardianSet> = Map::new("guardian_set");
 
 pub const PRICE_SOURCES: Map<&Denom, PriceSource> = Map::new("price_source");
+
+pub const PRICES: Map<PythId, PrecisionlessPrice> = Map::new("price");
