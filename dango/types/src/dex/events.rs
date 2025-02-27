ff43fe8d1c29d99a2b55c4c0b58d3d8629502ec6
Larry <26318510+larry0x@users.noreply.github.com>
2025-02-27 22:45:15 +0000

    refactor(dango): renames and moving things around (#488)
    * bank refactors
    
    * dex refactor
    
    * delete the unused ibc module
    
    * remove unintended public exports
    
    * vesting refactors
    
    * refactor warp types
    
    * rename
    
    * use alias
    
    * fix
    
    * move PRICES map from types to oracle
    
    * fix

---
+++ dango/types/src/dex/events.rs
@@ -0,0 +1,57 @@
+use {
+    crate::dex::{Direction, OrderId},
+    grug::{Addr, Coin, Coins, Denom, Udec128, Uint128},
+};
+
+#[grug::derive(Serde)]
+#[grug::event("pair_updated")]
+pub struct PairUpdated {
+    pub base_denom: Denom,
+    pub quote_denom: Denom,
+}
+
+#[grug::derive(Serde)]
+#[grug::event("order_submitted")]
+pub struct OrderSubmitted {
+    pub order_id: OrderId,
+    pub user: Addr,
+    pub base_denom: Denom,
+    pub quote_denom: Denom,
+    pub direction: Direction,
+    pub price: Udec128,
+    pub amount: Uint128,
+    pub deposit: Coin,
+}
+
+#[grug::derive(Serde)]
+#[grug::event("order_canceled")]
+pub struct OrderCanceled {
+    pub order_id: OrderId,
+    pub remaining: Uint128,
+    pub refund: Coin,
+}
+
+#[grug::derive(Serde)]
+#[grug::event("orders_matched")]
+pub struct OrdersMatched {
+    pub base_denom: Denom,
+    pub quote_denom: Denom,
+    pub clearing_price: Udec128,
+    pub volume: Uint128,
+}
+
+#[grug::derive(Serde)]
+#[grug::event("order_filled")]
+pub struct OrderFilled {
+    pub order_id: OrderId,
+    /// The price at which the order was executed.
+    pub clearing_price: Udec128,
+    /// The amount (measured in base asset) that was filled.
+    pub filled: Uint128,
+    /// The amount of coins returned to the user.
+    pub refund: Coins,
+    /// The amount of protocol fee collected.
+    pub fee: Option<Coin>,
+    /// Whether the order was _completed_ filled and cleared from the book.
+    pub cleared: bool,
+}
---
+++ dango/types/src/dex/msgs.rs
@@ -0,0 +1,124 @@
+use {
+    crate::dex::{Direction, OrderId, PairParams, PairUpdate},
+    grug::{Addr, Denom, Udec128, Uint128},
+    std::collections::{BTreeMap, BTreeSet},
+};
+
+#[grug::derive(Serde)]
+pub struct InstantiateMsg {
+    pub pairs: Vec<PairUpdate>,
+}
+
+#[grug::derive(Serde)]
+/// A set of order IDs, either a specific set or all. Used to cancel orders.
+pub enum OrderIds {
+    Some(BTreeSet<OrderId>),
+    All,
+}
+
+#[grug::derive(Serde)]
+pub enum ExecuteMsg {
+    /// Create new, or modify the parametes of existing, trading pairs.
+    ///
+    /// Can only be called by the chain owner.
+    BatchUpdatePairs(Vec<PairUpdate>),
+    /// Submit a new order.
+    ///
+    /// - For SELL orders, sender must attach `base_denom` of `amount` amount.
+    ///
+    /// - For BUY orders, sender must attach `quote_denom` of the amount
+    ///   calculated as:
+    ///
+    ///   ```plain
+    ///   ceil(amount * price)
+    ///   ```
+    SubmitOrder {
+        base_denom: Denom,
+        quote_denom: Denom,
+        direction: Direction,
+        amount: Uint128,
+        price: Udec128,
+    },
+    /// Cancel one or more orders by IDs.
+    CancelOrders { order_ids: OrderIds },
+}
+
+#[grug::derive(Serde, QueryRequest)]
+pub enum QueryMsg {
+    /// Query the parameters of a single trading pair.
+    #[returns(PairParams)]
+    Pair {
+        base_denom: Denom,
+        quote_denom: Denom,
+    },
+    /// Enumerate all trading pairs and their parameters.
+    #[returns(Vec<PairUpdate>)]
+    Pairs {
+        start_after: Option<PairPageParam>,
+        limit: Option<u32>,
+    },
+    /// Query a single active order by ID.
+    #[returns(OrderResponse)]
+    Order { order_id: OrderId },
+    /// Enumerate active orders across all pairs and from users.
+    #[returns(BTreeMap<OrderId, OrderResponse>)]
+    Orders {
+        start_after: Option<OrderId>,
+        limit: Option<u32>,
+    },
+    /// Enumerate active orders in a single pair from all users.
+    #[returns(BTreeMap<OrderId, OrdersByPairResponse>)]
+    OrdersByPair {
+        base_denom: Denom,
+        quote_denom: Denom,
+        start_after: Option<OrderId>,
+        limit: Option<u32>,
+    },
+    /// Enumerate active orders from a single user across all pairs.
+    #[returns(BTreeMap<OrderId, OrdersByUserResponse>)]
+    OrdersByUser {
+        user: Addr,
+        start_after: Option<OrderId>,
+        limit: Option<u32>,
+    },
+}
+
+/// Pagination parameters of the `QueryMsg::Pairs` query.
+#[grug::derive(Serde)]
+pub struct PairPageParam {
+    pub base_denom: Denom,
+    pub quote_denom: Denom,
+}
+
+/// Response type of the `QueryMsg::Order` and `Orders` queries.
+#[grug::derive(Serde)]
+pub struct OrderResponse {
+    pub user: Addr,
+    pub base_denom: Denom,
+    pub quote_denom: Denom,
+    pub direction: Direction,
+    pub price: Udec128,
+    pub amount: Uint128,
+    pub remaining: Uint128,
+}
+
+/// Response type of the `QueryMsg::OrdersByPair` query.
+#[grug::derive(Serde)]
+pub struct OrdersByPairResponse {
+    pub user: Addr,
+    pub direction: Direction,
+    pub price: Udec128,
+    pub amount: Uint128,
+    pub remaining: Uint128,
+}
+
+/// Response type of the `QueryMsg::OrdersByUser` query.
+#[grug::derive(Serde)]
+pub struct OrdersByUserResponse {
+    pub base_denom: Denom,
+    pub quote_denom: Denom,
+    pub direction: Direction,
+    pub price: Udec128,
+    pub amount: Uint128,
+    pub remaining: Uint128,
+}
