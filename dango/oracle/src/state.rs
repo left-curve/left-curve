d3dc2d8d9bb7ea77304ea347ad9088b186b70a6a
larry <26318510+larry0x@users.noreply.github.com>
2025-02-27 21:17:37 +0000

    use alias

--- dango/oracle/src/state.rs
+++ dango/oracle/src/state.rs
@@ -1,8 +1,8 @@
 use {
-    dango_types::oracle::{GuardianSet, PriceSource},
+    dango_types::oracle::{GuardianSet, GuardianSetIndex, PriceSource},
     grug::{Denom, Map},
 };
 
-pub const GUARDIAN_SETS: Map<u32, GuardianSet> = Map::new("guardian_set");
+pub const GUARDIAN_SETS: Map<GuardianSetIndex, GuardianSet> = Map::new("guardian_set");
 
 pub const PRICE_SOURCES: Map<&Denom, PriceSource> = Map::new("price_source");
