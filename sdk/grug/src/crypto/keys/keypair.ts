7570484c3869988059e12f23871d82f59461c00f
j0nl1 <jonlitech@gmail.com>
2025-02-27 10:39:54 +0000

    feat: support recoveryId when creating signature

--- sdk/grug/src/crypto/keys/keypair.ts
+++ sdk/grug/src/crypto/keys/keypair.ts
@@ -1,5 +1,5 @@
 export type KeyPair = {
   getPublicKey(compressed?: boolean): Uint8Array;
-  createSignature: (hash: Uint8Array) => Uint8Array;
+  createSignature: (hash: Uint8Array, recoveryId?: boolean) => Uint8Array;
   verifySignature: (hash: Uint8Array, signature: Uint8Array) => boolean;
 };
