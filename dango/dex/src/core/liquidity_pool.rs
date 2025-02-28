4a043d53ae205a59ac94718d56c4242289ee4201
larry <26318510+larry0x@users.noreply.github.com>
2025-02-28 20:03:51 +0000

    split passive liquidity core into two files

--- dango/dex/src/core/liquidity_pool.rs
+++ dango/dex/src/core/liquidity_pool.rs
@@ -1,36 +1,10 @@
 use {
-    dango_types::dex::{CurveInvariant, PairParams},
+    crate::TradingFunction,
+    dango_types::dex::PairParams,
     grug::{CoinPair, IsZero, MultiplyFraction, Number, NumberConst, Udec128, Uint128},
 };
 
 const HALF: Udec128 = Udec128::new_percent(50);
-
-pub trait TradingFunction {
-    /// Calculate the value of the trading invariant.
-    fn invariant(&self, reserve: &CoinPair) -> anyhow::Result<Uint128>;
-
-    fn normalized_invariant(&self, reserve: &CoinPair) -> anyhow::Result<Uint128>;
-}
-
-impl TradingFunction for CurveInvariant {
-    fn invariant(&self, reserve: &CoinPair) -> anyhow::Result<Uint128> {
-        match self {
-            // k = x * y
-            CurveInvariant::Xyk => {
-                let first = *reserve.first().amount;
-                let second = *reserve.second().amount;
-                Ok(first.checked_mul(second)?)
-            },
-        }
-    }
-
-    fn normalized_invariant(&self, reserve: &CoinPair) -> anyhow::Result<Uint128> {
-        match self {
-            // sqrt(k)
-            CurveInvariant::Xyk => Ok(self.invariant(reserve)?.checked_sqrt()?),
-        }
-    }
-}
 
 pub trait PassiveLiquidityPool {
     /// Provide liquidity to the pool. This function mutates the pool reserves.
