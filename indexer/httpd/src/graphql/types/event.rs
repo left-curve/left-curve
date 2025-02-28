5a16ff78737af7081eef84ab56eaf3670c413ee1
Fabien Penso <git@pen.so>
2025-02-28 12:33:38 +0000

    Add more subscription types

--- indexer/httpd/src/graphql/types/event.rs
+++ indexer/httpd/src/graphql/types/event.rs
@@ -13,6 +13,11 @@ pub struct Event {
     pub block_height: u64,
     pub created_at: DateTime<Utc>,
     pub event_idx: u32,
+    pub r#type: String,
+    pub method: Option<String>,
+    pub event_status: i16,
+    pub commitment_status: i16,
+    pub data: serde_json::Value,
 }
 
 impl From<entity::events::Model> for Event {
@@ -21,6 +26,11 @@ impl From<entity::events::Model> for Event {
             block_height: item.block_height as u64,
             created_at: Utc.from_utc_datetime(&item.created_at),
             event_idx: item.event_idx as u32,
+            r#type: item.r#type.clone(),
+            method: item.method,
+            event_status: item.event_status,
+            commitment_status: item.commitment_status,
+            data: item.data,
         }
     }
 }
