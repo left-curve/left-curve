505379e99d755fd8e3f5e69e7dbc465553a3683e
j0nl1 <jonlitech@gmail.com>
2025-03-04 22:44:20 +0000

    fix: search menu

---
+++ ui/portal/web/src/components/menu/SearchMenuBody.tsx
@@ -0,0 +1,67 @@
+import { useNavigate } from "@tanstack/react-router";
+
+import { Command } from "cmdk";
+import { AnimatePresence, motion } from "framer-motion";
+import { AppletItem } from "./AppletItem";
+
+import { applets } from "../../../applets";
+
+import type React from "react";
+
+interface SearchMenuBodyProps {
+  isVisible: boolean;
+  hideMenu: () => void;
+}
+
+export const SearchMenuBody: React.FC<SearchMenuBodyProps> = ({ isVisible, hideMenu }) => {
+  const navigate = useNavigate();
+  return (
+    <AnimatePresence mode="wait" custom={isVisible}>
+      {isVisible && (
+        <motion.div
+          layout
+          initial={{ height: 0 }}
+          animate={{ height: "auto" }}
+          exit={{ height: 0 }}
+          transition={{ duration: 0.1 }}
+          className="menu w-full overflow-hidden"
+        >
+          <motion.div
+            className="p-1 w-full flex items-center flex-col gap-1"
+            variants={{
+              hidden: {},
+              visible: {
+                transition: {
+                  delayChildren: 0.1,
+                  staggerChildren: 0.1,
+                },
+              },
+            }}
+            initial="hidden"
+            animate="visible"
+          >
+            <Command.List className="w-full">
+              <Command.Empty>
+                <p className="rounded-[20px] py-4 px-5 font-semibold text-[1.25rem]">
+                  No results found
+                </p>
+              </Command.Empty>
+              <Command.Group value="Applets">
+                {applets.map((applet) => (
+                  <Command.Item
+                    key={applet.title}
+                    value={applet.title}
+                    className="group"
+                    onSelect={() => [navigate({ to: applet.path }), hideMenu()]}
+                  >
+                    <AppletItem key={applet.title} {...applet} />
+                  </Command.Item>
+                ))}
+              </Command.Group>
+            </Command.List>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
