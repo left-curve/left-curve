c1ec852b3a3e2e3384f1ec2554042f3b6dd88a16
j0nl1 <jonlitech@gmail.com>
2025-03-05 21:53:38 +0000

    feat: navbar bg when scrolling

--- ui/portal/web/src/pages/(app)/_app.tsx
+++ ui/portal/web/src/pages/(app)/_app.tsx
@@ -1,18 +1,48 @@
+import { twMerge } from "@left-curve/applets-kit";
 import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { useEffect, useRef, useState } from "react";
 import { Header } from "~/components/Header";
 
 export const Route = createFileRoute("/(app)/_app")({
   component: function Layout() {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const mainRef = useRef<HTMLElement | null>(null);
+
+    useEffect(() => {
+      const main = mainRef.current;
+      if (!main) return;
+
+      const handleScroll = () => {
+        setIsScrolled(main.scrollTop > 70);
+      };
+
+      main.addEventListener("scroll", handleScroll);
+      return () => main.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-      <main className="flex flex-col h-screen w-screen relative items-center justify-start overflow-y-auto overflow-x-hidden scrollbar-none">
+      <main
+        ref={mainRef}
+        className="flex flex-col h-screen w-screen relative items-center justify-start overflow-y-auto overflow-x-hidden scrollbar-none"
+      >
         <img
           src="/images/union.png"
           alt="bg-image"
-          className="drag-none select-none h-[20vh] w-full fixed bottom-0 lg:top-0 left-0 z-40 lg:z-0 rotate-180 lg:rotate-0"
+          className={twMerge(
+            "drag-none select-none h-[15vh] lg:h-[20vh] w-full fixed lg:absolute bottom-0 lg:top-0 left-0 z-40 lg:z-0 rotate-180 lg:rotate-0",
+            isScrolled ? "z-40 lg:z-0" : "z-20 lg:z-0",
+          )}
         />
 
-        <Header />
-        <div className="flex items-start justify-center w-full z-10 h-full relative">
+        <header
+          className={twMerge(
+            "fixed lg:sticky bottom-0 lg:top-0 left-0 bg-transparent z-50 w-full p-4 transition-all",
+            isScrolled ? "lg:bg-white-100 lg:shadow-card-shadow" : "bg-transparent shadow-none",
+          )}
+        >
+          <Header />
+        </header>
+        <div className="flex items-start justify-start w-full z-30 h-full relative flex-col">
           <Outlet />
         </div>
       </main>
