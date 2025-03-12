1ae0dcf6e315de6364ed31fd4a22692976344cf5
larry <26318510+larry0x@users.noreply.github.com>
2025-03-12 01:47:46 +0000

    reorder

--- hyperlane/va/src/state.rs
+++ hyperlane/va/src/state.rs
@@ -2,7 +2,7 @@ use grug::{Addr, Coin, HexByteArray, Item, Map, UniqueVec};
 
 pub const MAILBOX: Item<Addr> = Item::new("mailbox");
 
+pub const ANNOUNCE_FEE_PER_BYTE: Item<Coin> = Item::new("announce_fee_per_byte");
+
 pub const STORAGE_LOCATIONS: Map<HexByteArray<20>, UniqueVec<String>> =
     Map::new("storage_location");
-
-pub const ANNOUNCE_FEE_PER_BYTE: Item<Coin> = Item::new("announce_fee_per_byte");
