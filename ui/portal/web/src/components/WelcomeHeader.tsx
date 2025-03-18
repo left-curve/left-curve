1cfde31819eca3903f4aada56cf03cc3b5d32c56
j0nl1 <jonlitech@gmail.com>
2025-03-18 03:23:35 +0000

    feat: welcome header

---
+++ ui/portal/web/src/components/WelcomeHeader.tsx
@@ -0,0 +1,89 @@
+import { Button } from "@left-curve/applets-kit";
+import { useAccount, useBalances } from "@left-curve/store-react";
+import { Link } from "@tanstack/react-router";
+import type React from "react";
+import { useApp } from "~/hooks/useApp";
+import { m } from "~/paraglide/messages";
+import { AssetsPreview } from "./AssetsPreview";
+import { ButtonLink } from "./ButtonLink";
+import { SwippeableAccountCard } from "./SwippeableAccountCard";
+
+interface Props {
+  cardMobileVisible: number;
+  setCardMobileVisible: (value: number) => void;
+}
+
+export const WelcomeHeader: React.FC<Props> = ({ cardMobileVisible, setCardMobileVisible }) => {
+  const { account, isConnected } = useAccount();
+  const { setSidebarVisibility } = useApp();
+  const { data: balances = {} } = useBalances({ address: account?.address });
+
+  if (!isConnected) {
+    return (
+      <div className="rounded-3xl relative shadow-card-shadow flex gap-4 w-full p-4 items-center flex-col lg:flex-row justify-end overflow-hidden min-h-[20rem] lg:min-h-[14.5rem] bg-[linear-gradient(236.46deg,_#FFF9F0_21.76%,_#E7D1B9_77.58%)]">
+        <img
+          src="/images/characters/group.svg"
+          alt="group"
+          className="absolute left-0 top-[-1rem] lg:top-auto max-h-44 lg:max-h-80 right-0 mx-auto lg:right-auto"
+        />
+        <div className=" lg:pr-[4.75rem]">
+          <div className="flex flex-col gap-4 items-center max-w-[19.5rem] text-center">
+            <p className="text-rice-800 exposure-h3-italic lg:exposure-h2-italic">
+              {m["common.motto"]()}
+            </p>
+            <div className="flex items-center justify-center gap-4 w-full lg:px-6">
+              <Button as={Link} fullWidth to="/login">
+                {m["common.signin"]()}
+              </Button>
+
+              <Button
+                as={Link}
+                fullWidth
+                variant="secondary"
+                to="/signup"
+                className="hidden lg:block"
+              >
+                {m["common.signup"]()}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="rounded-3xl bg-rice-50 shadow-card-shadow flex flex-col lg:flex-row gap-4 w-full p-4 items-center lg:items-start">
+      <SwippeableAccountCard
+        cardVisible={cardMobileVisible}
+        setCardVisible={setCardMobileVisible}
+      />
+
+      <div className="w-full flex flex-col lg:gap-4 items-center">
+        <div className="hidden lg:flex w-full">
+          <AssetsPreview
+            balances={balances}
+            showAllAssets={isConnected ? () => setSidebarVisibility(true) : undefined}
+          />
+        </div>
+
+        {isConnected ? (
+          <div className="lg:self-end flex gap-4 items-center justify-center w-full lg:max-w-[256px]">
+            <ButtonLink fullWidth size="md" to="/send-and-receive" search={{ action: "receive" }}>
+              {m["common.funds"]()}
+            </ButtonLink>
+            <ButtonLink
+              fullWidth
+              variant="secondary"
+              size="md"
+              to="/send-and-receive"
+              search={{ action: "send" }}
+            >
+              {m["common.send"]()}
+            </ButtonLink>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
