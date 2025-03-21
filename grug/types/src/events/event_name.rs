6ed5383ab576ba64fd4bf73ddaffee41dcce955e
larry <26318510+larry0x@users.noreply.github.com>
2025-03-21 17:47:09 +0000

    add comment

--- grug/types/src/events/event_name.rs
+++ grug/types/src/events/event_name.rs
@@ -1,3 +1,5 @@
+/// Represents a type that has a name associated with it. Necessary for the type
+/// to be emitted as a [`ContractEvent`](crate::ContractEvent).
 pub trait EventName {
     const NAME: &'static str;
 }
