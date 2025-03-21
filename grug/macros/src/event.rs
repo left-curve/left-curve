2b3f414b32d98dfadac4f34d1c1540fb49c26aba
larry <26318510+larry0x@users.noreply.github.com>
2025-03-21 18:06:01 +0000

    use `EventName` trait in `ContractEvent::new`

--- grug/macros/src/event.rs
+++ grug/macros/src/event.rs
@@ -28,30 +28,11 @@ pub fn process(attr: TokenStream, input: TokenStream) -> TokenStream {
     let name = attrs.0;
     let input_name = input.ident.clone();
 
-    // We implement `TryInto<ContractEvent>` for both `T` and `&T`, such that
-    // when calling `Response::add_event`, there is flexibility whether to pass
-    // by reference or by value, at the developer's choice.
     quote::quote! {
         #input
 
         impl ::grug::EventName for #input_name {
             const NAME: &'static str = #name;
-        }
-
-        impl TryFrom<&#input_name> for ::grug::ContractEvent {
-            type Error = ::grug::StdError;
-
-            fn try_from(value: &#input_name) -> Result<Self, Self::Error> {
-                ::grug::ContractEvent::new(#name, value)
-            }
-        }
-
-        impl TryFrom<#input_name> for ::grug::ContractEvent {
-            type Error = ::grug::StdError;
-
-            fn try_from(value: #input_name) -> Result<Self, Self::Error> {
-                ::grug::ContractEvent::new(#name, &value)
-            }
         }
     }
     .into()
