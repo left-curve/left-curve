088128b46e7ebba25251f9be49ec2faa5757242d
Fabien Penso <git@pen.so>
2025-03-25 11:35:36 +0100

    Add from/to address filtering

--- dango/httpd/src/graphql/subscription/transfer.rs
+++ dango/httpd/src/graphql/subscription/transfer.rs
@@ -15,9 +15,21 @@ impl TransferSubscription {
     async fn get_transfers(
         app_ctx: &indexer_httpd::context::Context,
         block_height: i64,
+        from_address: Option<String>,
+        to_address: Option<String>,
     ) -> Vec<Transfer> {
+        let mut filter = entity::transfers::Column::BlockHeight.eq(block_height);
+
+        if let Some(from_address) = from_address {
+            filter = filter.and(entity::transfers::Column::FromAddress.eq(from_address));
+        }
+
+        if let Some(to_address) = to_address {
+            filter = filter.and(entity::transfers::Column::ToAddress.eq(to_address));
+        }
+
         entity::transfers::Entity::find()
-            .filter(entity::transfers::Column::BlockHeight.eq(block_height))
+            .filter(filter)
             .order_by_asc(entity::transfers::Column::Idx)
             .all(&app_ctx.db)
             .await
@@ -35,21 +47,29 @@ impl TransferSubscription {
         &self,
         ctx: &Context<'a>,
         // The from address of the transfer
-        _from_address: Option<String>,
+        from_address: Option<String>,
         // The to address of the transfer
-        _to_address: Option<String>,
+        to_address: Option<String>,
     ) -> Result<impl Stream<Item = Vec<Transfer>> + 'a> {
         let app_ctx = ctx.data::<indexer_httpd::context::Context>()?;
 
         let latest_block_height = latest_block_height(&app_ctx.db).await?.unwrap_or_default();
 
+        let f = from_address.clone();
+        let t = to_address.clone();
+
         Ok(
-            once(async move { Self::get_transfers(app_ctx, latest_block_height).await }).chain(
-                app_ctx.pubsub.subscribe_block_minted().await?.then(
-                    move |block_height| async move {
-                        Self::get_transfers(app_ctx, block_height as i64).await
-                    },
-                ),
+            once(async move { Self::get_transfers(app_ctx, latest_block_height, f, t).await })
+                .chain(
+                app_ctx
+                    .pubsub
+                    .subscribe_block_minted()
+                    .await?
+                    .then(move |block_height| {
+                        let f = from_address.clone();
+                        let t = to_address.clone();
+                        async move { Self::get_transfers(app_ctx, block_height as i64, f, t).await }
+                    }),
             ),
         )
     }
