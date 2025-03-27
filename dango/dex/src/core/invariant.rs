595e2ca3f37b51c9159d2de21813e43cc01dfef5
Pacman <pacman@apollo.farm>
2025-03-27 17:10:58 +0100

    feat(dango): implement solve amount in and out for solidly

--- dango/dex/src/core/invariant.rs
+++ dango/dex/src/core/invariant.rs
@@ -93,7 +93,7 @@ impl TradingFunction for CurveInvariant {
         let ask_reserves = reserves.amount_of(&coin_out.denom)?;
         ensure!(offer_reserves > coin_out.amount, "insufficient liquidity");
 
-        match *self {
+        let amount_in = match *self {
             CurveInvariant::Xyk => {
                 // Apply swap fee. In SwapExactIn we multiply ask by (1 - fee) to get the
                 // offer amount after fees. So in this case we need to divide ask by (1 - fee)
@@ -110,12 +110,38 @@ impl TradingFunction for CurveInvariant {
                     ask_reserves - coin_out_after_fee,
                 )? - offer_reserves;
 
-                Ok(Coin {
-                    denom: denom_in.clone(),
-                    amount: amount_in,
-                })
+                amount_in
             },
-        }
+            CurveInvariant::Solidly => {
+                let offer_reserves_dec256: Dec256 = offer_reserves
+                    .into_next()
+                    .checked_into_dec()?
+                    .checked_into_signed()?;
+                let amount_in = solve_amount(
+                    solidly_log_invariant,
+                    (offer_reserves, ask_reserves),
+                    ask_reserves.checked_sub(coin_out.amount)?,
+                    |amount| Ok(offer_reserves_dec256.checked_add(amount)?),
+                    coin_out
+                        .amount
+                        .into_next()
+                        .checked_into_dec()?
+                        .checked_into_signed()?,
+                    Dec256::new_bps(1),
+                )?;
+
+                amount_in
+                // let amount_in = Int::ONE.checked_multiply_ratio_ceil(
+                //     offer_reserves * ask_reserves,
+                //     ask_reserves - coin_out_after_fee,
+                // )? - offer_reserves;
+            },
+        };
+
+        Ok(Coin {
+            denom: denom_in.clone(),
+            amount: amount_in,
+        })
     }
 
     fn solve_amount_out(
@@ -129,25 +155,106 @@ impl TradingFunction for CurveInvariant {
             reserves.has(&coin_in.denom) && reserves.has(denom_out),
             "invalid reserves"
         );
-        match self {
-            CurveInvariant::Xyk => {
-                let a = reserves.amount_of(&coin_in.denom)?;
-                let b = reserves.amount_of(denom_out)?;
 
+        let a = reserves.amount_of(&coin_in.denom)?;
+        let b = reserves.amount_of(denom_out)?;
+
+        let amount_out = match self {
+            CurveInvariant::Xyk => {
                 // Solve A * B = (A + offer.amount) * (B - amount_out) for amount_out
                 // => amount_out = B - (A * B) / (A + offer.amount)
                 // Round so that user takes the loss
-                let amount_out =
-                    b - Int::ONE.checked_multiply_ratio_ceil(a * b, a + coin_in.amount)?;
+                let amount_out = b - Int::ONE.checked_multiply_ratio_ceil(
+                    a.checked_mul(b)?,
+                    a.checked_add(coin_in.amount)?,
+                )?;
 
                 // Apply swap fee. Round so that user takes the loss
                 let amount_out = amount_out.checked_mul_dec_floor(Udec128::ONE - swap_fee)?;
 
-                Ok(Coin {
-                    denom: denom_out.clone(),
-                    amount: amount_out,
-                })
+                amount_out
             },
-        }
+            CurveInvariant::Solidly => {
+                let b_dec256: Dec256 = b.into_next().checked_into_dec()?.checked_into_signed()?;
+                let amount_out = solve_amount(
+                    solidly_log_invariant,
+                    (a, b),
+                    a.checked_add(coin_in.amount)?,
+                    |amount| Ok(b_dec256.checked_sub(amount)?),
+                    coin_in
+                        .amount
+                        .into_next()
+                        .checked_into_dec()?
+                        .checked_into_signed()?,
+                    Dec256::new_bps(1),
+                )?;
+
+                amount_out
+            },
+        };
+
+        Ok(Coin {
+            denom: denom_out.clone(),
+            amount: amount_out,
+        })
+    }
+}
+
+fn solve_amount<F, G>(
+    invariant: F,
+    reserves_before: (Uint128, Uint128),
+    a_after: Uint128,
+    calc_b_after: G,
+    initial_guess: Dec256,
+    tolerance: Dec256,
+) -> anyhow::Result<Uint128>
+where
+    F: Fn(Dec256, Dec256) -> anyhow::Result<Dec256>,
+    G: Fn(Dec256) -> anyhow::Result<Dec256>,
+{
+    let a = reserves_before
+        .0
+        .into_next()
+        .checked_into_dec()?
+        .checked_into_signed()?;
+    let b = reserves_before
+        .1
+        .into_next()
+        .checked_into_dec()?
+        .checked_into_signed()?;
+
+    let invariant_before = invariant(a, b)?;
+
+    let a_after: Dec256 = a_after
+        .into_next()
+        .checked_into_dec()?
+        .checked_into_signed()?;
+
+    // Set up the function to solve for amount out using secant method which is
+    // invariant(a_after, b_after) - invariant(a, b) = 0
+    let g = |amount: Dec256| -> anyhow::Result<Dec256> {
+        let b_after = calc_b_after(amount)?;
+        let invariant_after = invariant(a_after, b_after)?;
+
+        Ok(invariant_after.checked_sub(invariant_before)?)
+    };
+
+    let amount_out = secant_method(
+        g,
+        initial_guess,
+        initial_guess.checked_sub(Dec256::ONE)?,
+        tolerance,
+        100,
+    )?;
+
+    println!("amount_out before floor: {}", amount_out);
+
+    Ok(amount_out
+        .checked_floor()?
+        .into_int()
+        .checked_into_unsigned()?
+        .checked_into_prev()?)
+}
+
     }
 }
