4ae8ece9b0eb08543071d2271fb6355e518fe253
Pacman <pacman@apollo.farm>
2025-03-27 16:59:58 +0100

    feat: implement log2 for I256

--- dango/dex/src/core.rs
+++ dango/dex/src/core.rs
@@ -1,6 +1,7 @@
 mod invariant;
 mod liquidity_pool;
+mod math;
 mod order_filling;
 mod order_matching;
 
-pub use {invariant::*, liquidity_pool::*, order_filling::*, order_matching::*};
+pub use {invariant::*, liquidity_pool::*, math::*, order_filling::*, order_matching::*};
