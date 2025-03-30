c567b7cd2f2404198ed6c6acf5f297ec4a9c6abf
j0nl1 <jonlitech@gmail.com>
2025-03-30 02:17:52 +0100

    feat: update proxy to support quests

--- ui/workers/proxy/src/index.ts
+++ ui/workers/proxy/src/index.ts
@@ -1,11 +1,16 @@
 interface Env {
-  RPC_HOST: string;
+  SERVER_URI: string;
 }
+
+const getPort = (pathname: string) => {
+  if (pathname.includes("/rpc")) return "26657";
+  if (pathname.includes("/graphql")) return "8080";
+  if (pathname.includes("/quests")) return "8081";
+  throw new Error("Invalid path");
+};
 
 export default {
   async fetch(request: Request, env: Env) {
-    if (request.method === "GET") return new Response("Ok", { status: 200 });
-
     if (request.method === "OPTIONS") {
       return new Response(null, {
         status: 204,
@@ -20,13 +25,36 @@ export default {
     }
 
     const url = new URL(request.url);
+    if (!["/rpc", "/graphql", "/quests"].includes(url.pathname)) {
+      const [questsStatus, graphqlStatus, rpcStatus] = await Promise.all([
+        fetch(`${env.SERVER_URI}:8081/check_username/none`),
+        fetch(`${env.SERVER_URI}:8080`),
+        fetch(`${env.SERVER_URI}:26657`),
+      ]);
 
-    const newRequest = new Request(`${env.RPC_HOST}${url.pathname}${url.search}`, {
-      method: request.method,
-      headers: request.headers,
-      body: request.body,
-      redirect: "follow",
-    });
+      return new Response(
+        JSON.stringify({
+          health: {
+            quests: questsStatus.ok ? "up" : "down",
+            graphql: graphqlStatus.ok ? "up" : "down",
+            rpc: rpcStatus.ok ? "up" : "down",
+          },
+        }),
+        { status: 200 },
+      );
+    }
+
+    const PORT = getPort(url.pathname);
+
+    const newRequest = new Request(
+      `${env.SERVER_URI}:${PORT}${url.pathname.replace("/rpc", "").replace("/quests", "")}${url.search}`,
+      {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+        redirect: "follow",
+      },
+    );
 
     const response = await fetch(newRequest);
 
