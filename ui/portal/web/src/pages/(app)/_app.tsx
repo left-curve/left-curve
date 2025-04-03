ffde9028ec6f4e80542498dc3399033982dc344c
j0nl1 <jonlitech@gmail.com>
2025-04-03 15:15:47 +0100

    feat: error page

--- ui/portal/web/src/pages/(app)/_app.tsx
+++ ui/portal/web/src/pages/(app)/_app.tsx
@@ -44,4 +44,25 @@ export const Route = createFileRoute("/(app)/_app")({
       </main>
     );
   },
+  errorComponent: () => {
+    return (
+      <main className="flex flex-col h-screen w-screen relative items-center justify-start overflow-y-auto overflow-x-hidden scrollbar-none bg-white-100">
+        <img
+          src="/images/union.png"
+          alt="bg-image"
+          className={twMerge(
+            "drag-none select-none h-[15vh] lg:h-[20vh] w-full fixed lg:absolute bottom-0 lg:top-0 left-0 z-40 lg:z-0 rotate-180 lg:rotate-0",
+          )}
+        />
+        <Header isScrolled={false} />
+        <div className="flex items-center justify-start w-full z-30 h-full relative flex-col">
+          <div className="w-full flex flex-1 justify-center items-center p-4">
+            <h3 className="text-center max-w-4xl diatype text-[40px] md:text-[80px] font-extrabold text-gray-500">
+              Sorry, we couldn't find the page you were looking for.
+            </h3>
+          </div>
+        </div>
+      </main>
+    );
+  },
 });
