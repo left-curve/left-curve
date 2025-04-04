6aea59f58e8a60e091fa3fbc67746ce0d7abb274
Zexsor33 <93092171+Zexsor33@users.noreply.github.com>
2025-04-04 07:23:43 +0200

    feat(pyth): better error handling (#611)
    * improve error handling in pyth_handler
    
    * add test for pyth sse client
    
    * minor formatting
    
    ---------
    
    Co-authored-by: larry <26318510+larry0x@users.noreply.github.com>

--- pyth/client/src/client.rs
+++ pyth/client/src/client.rs
@@ -15,7 +15,7 @@ use {
         time::Duration,
     },
     tokio_stream::StreamExt,
-    tracing::{debug, error},
+    tracing::{error, info},
 };
 
 /// PythClient is a client to interact with the Pyth network.
@@ -108,9 +108,11 @@ impl PythClientTrait for PythClient {
                             return;
                         }
 
-                        if let Some(event) = data{
+                        if let Some(event) = data {
                             match event {
-                                Ok(Event::Open) => debug!("Pyth SSE connection open"),
+                                Ok(Event::Open) => {
+                                    info!("Pyth SSE connection open");
+                                },
                                 Ok(Event::Message(message)) => {
                                     match message.data.deserialize_json::<LatestVaaResponse>() {
                                         Ok(vaas) => {
@@ -119,7 +121,7 @@ impl PythClientTrait for PythClient {
                                         Err(err) => {
                                             error!(
                                                 err = err.to_string(),
-                                                "Failed to deserialize Pyth event into LatestVaaResponse"
+                                                "Failed to deserialize Pyth event into `LatestVaaResponse`"
                                             );
                                         },
                                     }
@@ -131,10 +133,10 @@ impl PythClientTrait for PythClient {
                                     );
                                 },
                             }
+                        } else {
+                            error!("Pyth SSE connection close");
                         }
-
                     }
-
                 }
             }
         };
