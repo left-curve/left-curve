c566232e5c0b29ea349e387bccbf40beb41b4acc
Rhaki <86773669+Rhaki@users.noreply.github.com>
2025-04-11 15:33:04 +0200

    dango: Lending improvement

---
+++ dango/types/src/lending/events.rs
@@ -0,0 +1,21 @@
+use std::collections::BTreeMap;
+
+use grug::{Addr, Coins, Denom, Udec256};
+
+/// An event indicating a user has borrowed coins from the lending contract.
+#[grug::derive(Serde)]
+#[grug::event("borrowed")]
+pub struct Borrowed {
+    pub user: Addr,
+    pub coins: Coins,
+}
+
+/// An event indicating a user has repaid coins to the lending contract.
+#[grug::derive(Serde)]
+#[grug::event("repaid")]
+pub struct Repaid {
+    pub user: Addr,
+    pub repaid: Coins,
+    pub refunds: Coins,
+    pub remaining_scaled_debts: BTreeMap<Denom, Udec256>,
+}
