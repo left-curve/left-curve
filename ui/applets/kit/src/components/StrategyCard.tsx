d2d425918cfd96f549baeba0fa2b98ef614c44fc
j0nl1 <jonlitech@gmail.com>
2025-04-14 18:47:15 +0100

    feat: align strategy card with design

--- ui/applets/kit/src/components/StrategyCard.tsx
+++ ui/applets/kit/src/components/StrategyCard.tsx
@@ -1,42 +1,49 @@
 import type React from "react";
+import { Button } from "#components/Button.js";
 
 export const StrategyCard: React.FC = () => {
   return (
-    <div className="relative p-4  min-h-[8.5rem] min-w-[17.375rem] bg-rice-50 shadow-card-shadow rounded-2xl overflow-hidden">
+    <div className="relative p-4  min-h-[21.125rem] min-w-[17.375rem] bg-rice-50 shadow-card-shadow rounded-xl overflow-hidden">
       <img
         src="/images/strategy-card/cocodrile.svg"
         alt=""
         className="absolute z-0 bottom-0 right-0 "
       />
       <div className="flex flex-col gap-2 justify-between z-10 w-full h-full relative">
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-2 text-lg">
-            <div className="flex">
-              <img
-                src="https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png"
-                alt=""
-                className="h-6 w-6 rounded-full"
-              />
-              <img
-                src="https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png"
-                alt=""
-                className="h-6 w-6 -ml-1 rounded-full"
-              />
-            </div>
-            <p>ETH-USD</p>
+        <div className="flex flex-col gap-6 items-center justify-center text-center">
+          <div className="flex">
+            <img
+              src="https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png"
+              alt=""
+              className="h-12 w-12 rounded-full"
+            />
+            <img
+              src="https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png"
+              alt=""
+              className="h-12 w-12 -ml-1 rounded-full"
+            />
           </div>
-          <div className="text-xs bg-green-bean-200 text-gray-500 py-1 px-2 rounded-[4px] h-fit w-fit">
-            Stable Strategy
+          <div className="flex flex-col gap-1">
+            <p className="exposure-h3-italic">ETH Party!</p>
+            <p className="diatype-lg-medium text-gray-500">
+              Deposit <span className="font-bold">ETH-USDT</span>
+            </p>
+            <p className="diatype-lg-medium text-gray-500">Earn USDT</p>
           </div>
         </div>
-        <div className="p-2 rounded-xl bg-rice-100 flex items-center justify-between text-xs">
-          <div className="flex gap-2">
-            <span className="text-gray-500">APY</span>
-            <span>17.72%</span>
-          </div>
-          <div className="flex gap-2">
-            <span className="text-gray-500">TVL</span>
-            <span>15.63%</span>
+        <div className="flex flex-col gap-4">
+          <Button size="lg" variant="secondary" fullWidth>
+            Select
+          </Button>
+          <div className="p-2 rounded-xl bg-rice-100/80 flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+              <span className="text-gray-500 diatype-xs-medium">APY</span>
+              <span className="text-gray-700 diatype-sm-bold">17.72%</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <span className="text-gray-500 diatype-xs-medium">TVL</span>
+              <span className="text-gray-700 diatype-sm-bold">15.63%</span>
+            </div>
           </div>
         </div>
       </div>
