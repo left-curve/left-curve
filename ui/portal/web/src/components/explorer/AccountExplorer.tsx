3cc5b1bfe3c0f9267c72dae85db952adb1da54f5
j0nl1 <jonlitech@gmail.com>
2025-04-15 20:13:04 +0100

    feat: account explorer

--- ui/portal/web/src/components/explorer/AccountExplorer.tsx
+++ ui/portal/web/src/components/explorer/AccountExplorer.tsx
@@ -1,93 +1,78 @@
 import { useMediaQuery } from "@left-curve/applets-kit";
-import { useConfig, usePrices, usePublicClient } from "@left-curve/store";
-import { useQuery } from "@tanstack/react-query";
+import { usePrices, usePublicClient } from "@left-curve/store";
+import { type UseQueryResult, useQuery } from "@tanstack/react-query";
 import { createContext, useContext } from "react";
 import { useApp } from "~/hooks/useApp";
 
-import { camelToTitleCase } from "@left-curve/dango/utils";
 import { m } from "~/paraglide/messages";
 
 import { Badge, TextCopy, TruncateText } from "@left-curve/applets-kit";
-import { ContractCard } from "../foundation/ContractCard";
+import { AccountCard } from "../foundation/AccountCard";
 import { AssetsTable } from "./AssetsTable";
 import { HeaderExplorer } from "./HeaderExplorer";
 
-import type { Address, Coins, ContractInfo } from "@left-curve/dango/types";
-import type { UseQueryResult } from "@tanstack/react-query";
+import type { Account, Address, Coins, ContractInfo } from "@left-curve/dango/types";
 import type React from "react";
 import type { PropsWithChildren } from "react";
 
-const ContractExplorerContext = createContext<
-  | (UseQueryResult<(ContractInfo & { name: string; balances: Coins }) | null, Error> & {
+const AccountExplorerContext = createContext<
+  | (UseQueryResult<(Account & ContractInfo & { balances: Coins }) | null, Error> & {
       address: string;
     })
   | null
 >(null);
 
-const useContractExplorer = () => {
-  const context = useContext(ContractExplorerContext);
+const useAccountExplorer = () => {
+  const context = useContext(AccountExplorerContext);
   if (context === null) {
-    throw new Error("useContractExplorer must be used within a ContractExplorerProvider");
+    throw new Error("useAccountExplorer must be used within a AccountExplorerContext");
   }
   return context;
 };
 
-type ContractExplorerProps = {
+type AccountExplorerProps = {
   address: Address;
 };
 
-const Root: React.FC<PropsWithChildren<ContractExplorerProps>> = ({ address, children }) => {
+const Root: React.FC<PropsWithChildren<AccountExplorerProps>> = ({ address, children }) => {
   const client = usePublicClient();
-  const { getAppConfig } = useConfig();
 
   const query = useQuery({
-    queryKey: ["contract_explorer", address],
+    queryKey: ["account_explorer", address],
     queryFn: async () => {
-      const [appConfig, contractInfo, balances] = await Promise.all([
-        getAppConfig(),
+      const [account, contractInfo, balances] = await Promise.all([
+        client.getAccountInfo({ address }),
         client.getContractInfo({ address }),
         client.getBalances({ address }),
       ]);
 
-      const isAccount = Object.values(appConfig.accountFactory.codeHashes).includes(
-        contractInfo.codeHash,
-      );
-
-      if (isAccount) return null;
-
-      const appContract = Object.entries(appConfig.addresses).find(
-        ([_, cAddress]) => cAddress === address,
-      );
-      const name = appContract
-        ? `Dango ${camelToTitleCase(appContract[0])}`
-        : (contractInfo.label ?? "Contract");
+      if (!account) return null;
 
       return {
+        ...account,
         ...contractInfo,
-        name,
-        address,
         balances,
       };
     },
   });
 
   return (
-    <ContractExplorerContext.Provider value={{ address, ...query }}>
+    <AccountExplorerContext.Provider value={{ address, ...query }}>
       <div className="w-full md:max-w-[76rem] flex flex-col gap-6 p-4 pt-6 mb-16">{children}</div>
-    </ContractExplorerContext.Provider>
+    </AccountExplorerContext.Provider>
   );
 };
 
 const Details: React.FC = () => {
-  const { isLoading, data: contract, address } = useContractExplorer();
+  const { isLoading, data: account } = useAccountExplorer();
   const { calculateBalance } = usePrices();
   const { isMd } = useMediaQuery();
   const { settings } = useApp();
   const { formatNumberOptions } = settings;
 
-  if (!contract || isLoading) return null;
+  if (!account || isLoading) return null;
 
-  const { name, codeHash, admin, balances } = contract;
+  const { codeHash, admin, balances } = account;
   const totalCoins = Object.values(balances).length;
   const totalBalance = calculateBalance(balances, {
     format: true,
@@ -96,7 +81,7 @@ const Details: React.FC = () => {
 
   return (
     <div className="flex flex-col gap-6 lg:flex-row">
-      <ContractCard name={name} address={address} balance={totalBalance} />
+      <AccountCard account={account} balance={totalBalance} />
       <div className="flex flex-col gap-4 rounded-md px-4 py-3 bg-rice-25 shadow-card-shadow relative overflow-hidden w-full">
         <h4 className="h4-heavy">{m["explorer.contracts.details.contractDetails"]()}</h4>
         <div className="flex flex-col gap-2">
@@ -130,20 +115,20 @@ const Details: React.FC = () => {
 };
 
 const NotFound: React.FC = () => {
-  const { isLoading, data: contract, address } = useContractExplorer();
-  if (isLoading || contract) return null;
+  const { isLoading, data: account, address } = useAccountExplorer();
+  if (isLoading || account) return null;
 
   return (
     <div className="w-full md:max-w-[76rem] p-4">
       <HeaderExplorer>
         <div className="flex flex-col gap-2 items-center border border-red-bean-50">
           <h3 className="exposure-m-italic text-gray-700">
-            {m["explorer.contracts.notFound.title"]()}
+            {m["explorer.accounts.notFound.title"]()}
           </h3>
           <p className="diatype-m-medium max-w-[42.5rem] text-center text-gray-500 ">
-            {m["explorer.contracts.notFound.pre"]()}
-            <span className="break-all overflow-hidden underline"> {address}</span>{" "}
-            {m["explorer.contracts.notFound.description"]()}
+            {m["explorer.accounts.notFound.pre"]()}
+            <span className="break-all overflow-hidden underline">{address}</span>{" "}
+            {m["explorer.accounts.notFound.description"]()}
           </p>
         </div>
       </HeaderExplorer>
@@ -152,14 +137,14 @@ const NotFound: React.FC = () => {
 };
 
 const Assets: React.FC = () => {
-  const { isLoading, data: contract } = useContractExplorer();
+  const { isLoading, data: account } = useAccountExplorer();
 
-  if (isLoading || !contract) return null;
+  if (isLoading || !account) return null;
 
-  return <AssetsTable balances={contract.balances} />;
+  return <AssetsTable balances={account.balances} />;
 };
 
-export const ContractExplorer = Object.assign(Root, {
+export const AccountExplorer = Object.assign(Root, {
   Details,
   NotFound,
   Assets,
