259858398c6b5682e3b83bd45bf7775aa9dd4f33
j0nl1 <jonlitech@gmail.com>
2025-04-23 17:05:49 +0100

    fix: js comments in dex

--- sdk/dango/src/types/dex.ts
+++ sdk/dango/src/types/dex.ts
@@ -4,117 +4,117 @@ import type { Username } from "./account.js";
 export type SwapRoute = PairId[];
 
 export type DexQueryMsg =
-  /* Returns the trading volume of a username since the specified timestamp. */
+  /** Returns the trading volume of a username since the specified timestamp. */
   | {
       volumeByUser: {
-        /* The username to query trading volume for. */
+        /** The username to query trading volume for. */
         user: Username;
-        /* The start timestamp to query trading volume for. If not provided,
+        /** The start timestamp to query trading volume for. If not provided,
          * username's total trading volume will be returned. */
         since: Option<Timestamp>;
       };
     }
-  /* Returns the trading volume of a user address since the specified timestamp. */
+  /** Returns the trading volume of a user address since the specified timestamp. */
   | {
       volume: {
-        /* The user's address to query trading volume for. */
+        /** The user's address to query trading volume for. */
         user: Address;
-        /* The start timestamp to query trading volume for. If not provided,
+        /** The start timestamp to query trading volume for. If not provided,
          * user's total trading volume will be returned. */
         since: Option<Timestamp>;
       };
     }
-  /* Simulate a swap with exact output. */
+  /** Simulate a swap with exact output. */
   | {
       simulateSwapExactAmountOut: {
-        /* The route of the swap. */
+        /** The route of the swap. */
         route: SwapRoute;
-        /* The output amount of the swap. */
+        /** The output amount of the swap. */
         output: Coin;
       };
     }
-  /* Simulate a swap with exact input. */
+  /** Simulate a swap with exact input. */
   | {
       simulateSwapExactAmountIn: {
-        /* The route of the swap. */
+        /** The route of the swap. */
         route: SwapRoute;
-        /* The input amount of the swap. */
+        /** The input amount of the swap. */
         input: Coin;
       };
     }
-  /* Query the parameters of a single trading pair. */
+  /** Query the parameters of a single trading pair. */
   | {
       pair: {
-        /* The base denomination of the trading pair. */
+        /** The base denomination of the trading pair. */
         baseDenom: Denom;
-        /* The quote denomination of the trading pair. */
+        /** The quote denomination of the trading pair. */
         quoteDenom: Denom;
       };
     }
-  /* Enumerate all trading pairs and their parameters. */
+  /** Enumerate all trading pairs and their parameters. */
   | {
       pairs: {
-        /* The ID of the pair to start after. */
+        /** The ID of the pair to start after. */
         startAfter: Option<PairId>;
-        /* The maximum number of pairs to return. */
+        /** The maximum number of pairs to return. */
         limit: Option<number>;
       };
     }
-  /* Query the passive liquidity pool reserve of a single trading pair. */
+  /** Query the passive liquidity pool reserve of a single trading pair. */
   | {
       reserve: {
-        /* The base denomination of the trading pair. */
+        /** The base denomination of the trading pair. */
         baseDenom: Denom;
-        /* The quote denomination of the trading pair. */
+        /** The quote denomination of the trading pair. */
         quoteDenom: Denom;
       };
     }
-  /* Enumerate all passive liquidity pool reserves. */
+  /** Enumerate all passive liquidity pool reserves. */
   | {
       reserves: {
-        /* The ID of the reserve to start after. */
+        /** The ID of the reserve to start after. */
         startAfter: Option<PairId>;
-        /* The maximum number of reserves to return. */
+        /** The maximum number of reserves to return. */
         limit: Option<number>;
       };
     }
-  /* Query a single active order by ID. */
+  /** Query a single active order by ID. */
   | {
       order: {
-        /* The ID of the order to query. */
+        /** The ID of the order to query. */
         orderId: OrderId;
       };
     }
-  /* Enumerate active orders across all pairs and from users. */
+  /** Enumerate active orders across all pairs and from users. */
   | {
       orders: {
-        /* The ID of the order to start after. */
+        /** The ID of the order to start after. */
         startAfter: Option<OrderId>;
-        /* The maximum number of orders to return. */
+        /** The maximum number of orders to return. */
         limit: Option<number>;
       };
     }
-  /* Enumerate active orders in a single pair from all users. */
+  /** Enumerate active orders in a single pair from all users. */
   | {
       ordersByPair: {
-        /* The base denomination of the trading pair. */
+        /** The base denomination of the trading pair. */
         baseDenom: Denom;
-        /* The quote denomination of the trading pair. */
+        /** The quote denomination of the trading pair. */
         quoteDenom: Denom;
-        /* The ID of the order to start after. */
+        /** The ID of the order to start after. */
         startAfter: Option<OrderId>;
-        /* The maximum number of orders to return. */
+        /** The maximum number of orders to return. */
         limit: Option<number>;
       };
     }
-  /* Enumerate active orders from a single user across all pairs. */
+  /** Enumerate active orders from a single user across all pairs. */
   | {
       ordersByUser: {
-        /* The user address to query. */
+        /** The user address to query. */
         user: Address;
-        /* The ID of the order to start after. */
+        /** The ID of the order to start after. */
         startAfter?: Option<OrderId>;
-        /* The maximum number of orders to return. */
+        /** The maximum number of orders to return. */
         limit?: Option<number>;
       };
     };
@@ -222,11 +222,11 @@ export const CurveInvariant = {
 export type CurveInvariants = (typeof CurveInvariant)[keyof typeof CurveInvariant];
 
 export type PairParams = {
-  /* Liquidity token denom of the passive liquidity pool */
+  /**  Liquidity token denom of the passive liquidity pool */
   lpDenom: Denom;
-  /* Curve invariant for the passive liquidity pool. */
+  /**  Curve invariant for the passive liquidity pool. */
   curveInvariant: CurveInvariants;
-  /* Fee rate for instant swaps in the passive liquidity pool. */
+  /**  Fee rate for instant swaps in the passive liquidity pool. */
   swapFeeRate: string;
 };
 
