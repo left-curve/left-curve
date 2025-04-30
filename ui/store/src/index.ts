3fb6d3981a22348169d8307eb44458633355d5be
j0nl1 <jonlitech@gmail.com>
2025-04-30 18:22:42 +0100

    feat: use app config hook

--- ui/store/src/index.ts
+++ ui/store/src/index.ts
@@ -107,6 +107,12 @@ export {
   useSignin,
 } from "./hooks/useSignin.js";
 
+export {
+  type UseAppConfigParameters,
+  type UseAppConfigReturnType,
+  useAppConfig,
+} from "./hooks/useAppConfig.js";
+
 export { useSessionKey } from "./hooks/useSessionKey.js";
 
 export { useSigningClient } from "./hooks/useSigningClient.js";
