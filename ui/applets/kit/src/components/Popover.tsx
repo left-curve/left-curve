44ed342485b27d769556553dae0419890b714b63
kyar1s <iris.ledesma.27@gmail.com>
2025-05-06 18:57:10 +0100

    feat: add popover with headlessui

--- ui/applets/kit/src/components/Popover.tsx
+++ ui/applets/kit/src/components/Popover.tsx
@@ -1,61 +1,43 @@
+import { Popover as HPopover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import { IconChevronDown } from "./icons/IconChevronDown";
 import { twMerge } from "#utils/twMerge.js";
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
-import { useClickAway } from "react-use";
 
 interface Props {
   trigger: React.ReactNode | string;
   menu: React.ReactNode;
-  classNames?: {
-    base: string;
+  className?: {
+    base?: string;
     trigger?: string;
     menu?: string;
   };
   showArrow?: boolean;
 }
 
-export const Popover: React.FC<Props> = ({ menu, trigger, classNames, showArrow = true }) => {
-  const [open, setOpen] = useState(false);
-  const popoverRef = useRef<HTMLDivElement>(null);
-  const triggerRef = useRef<HTMLButtonElement>(null);
-  const panelRef = useRef<HTMLDivElement>(null);
-
-  useClickAway(popoverRef, () => setOpen(false));
-
-  useEffect(() => {
-    if (open && triggerRef.current && panelRef.current) {
-      const panel = panelRef.current;
-
-      const top = triggerRef.current.offsetTop + triggerRef.current.offsetHeight + 4;
-
-      panel.style.top = `${top}px`;
-    }
-  }, [open]);
-
+export const Popover: React.FC<Props> = ({ menu, trigger, className, showArrow = true }) => {
   return (
-    <div ref={popoverRef} className="relative group w-fit">
-      <>
-        <motion.button
-          ref={triggerRef}
-          onClick={() => setOpen((prev) => !prev)}
-          className={twMerge("flex items-center gap-2 outline-none", classNames?.trigger)}
-        >
-          {trigger}
-          {showArrow && (
-            <IconChevronDown className={twMerge("transition-all w-5 h-5", open && "rotate-180")} />
-          )}
-        </motion.button>
-
-        {open && (
-          <motion.div
-            ref={panelRef}
-            className={twMerge(
-              "left-0 xl:left-1/2 xl:-translate-x-1/2 flex flex-col absolute z-50 bg-rice-25 rounded-lg h-fit p-4 shadow-card-shadow",
-              classNames?.menu,
-            )}
+    <HPopover className={twMerge("relative group w-fit", className?.base)}>
+      {({ open }) => (
+        <>
+          <PopoverButton
+            className={twMerge("flex items-center gap-2 outline-none", className?.trigger)}
           >
-            <motion.div layout="size">
+            {trigger}
+            {showArrow && (
+              <IconChevronDown
+                className={twMerge("transition-all w-5 h-5", open && "rotate-180")}
+              />
+            )}
+          </PopoverButton>
+
+          <PopoverPanel
+            anchor="bottom"
+            className={twMerge("flex flex-col absolute z-50 p-2", className?.menu)}
+          >
+            <motion.div
+              layout="size"
+              className="bg-rice-25 rounded-lg h-fit p-4 shadow-card-shadow"
+            >
               <AnimatePresence>
                 {open && (
                   <motion.div
@@ -71,9 +53,9 @@ export const Popover: React.FC<Props> = ({ menu, trigger, classNames, showArrow 
                 )}
               </AnimatePresence>
             </motion.div>
-          </motion.div>
-        )}
-      </>
-    </div>
+          </PopoverPanel>
+        </>
+      )}
+    </HPopover>
   );
 };
