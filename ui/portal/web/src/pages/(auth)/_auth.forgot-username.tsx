93924fffa3d900334967e133cc86480f3327029c
kyar1s <iris.ledesma.27@gmail.com>
2025-05-09 16:46:25 +0100

    feat: forgot username flow

---
+++ ui/portal/web/src/pages/(auth)/_auth.forgot-username.tsx
@@ -0,0 +1,17 @@
+import { WizardProvider } from "@left-curve/applets-kit";
+import { createFileRoute } from "@tanstack/react-router";
+import { ForgotUsername } from "~/components/auth/ForgotUsername";
+
+export const Route = createFileRoute("/(auth)/_auth/forgot-username")({
+  component: ForgotUsernameComponent,
+});
+
+function ForgotUsernameComponent() {
+  return (
+    <WizardProvider wrapper={<ForgotUsername />} persistKey="dango.signup">
+      <ForgotUsername.Credential />
+      <ForgotUsername.AvailableUsernames />
+      <ForgotUsername.Signin />
+    </WizardProvider>
+  );
+}
