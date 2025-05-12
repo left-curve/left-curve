5a557461e50fcb7a28da58e7cb104bb1a48e788e
kyar1s <iris.ledesma.27@gmail.com>
2025-05-12 19:20:59 +0100

    feat: format useCountdown

--- ui/applets/kit/src/hooks/useCountdown.ts
+++ ui/applets/kit/src/hooks/useCountdown.ts
@@ -7,11 +7,17 @@ export function useCountdown(targetDate?: number | string | Date) {
     if (difference <= 0) {
       return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((difference / (1000 * 60)) % 60);
+    const seconds = Math.floor((difference / 1000) % 60);
+
     return {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / (1000 * 60)) % 60),
-      seconds: Math.floor((difference / 1000) % 60),
+      days: days < 10 ? `0${days}` : days,
+      hours: hours < 10 ? `0${hours}` : hours,
+      minutes: minutes < 10 ? `0${minutes}` : minutes,
+      seconds: seconds < 10 ? `0${seconds}` : seconds,
     };
   };
 
