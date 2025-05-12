89246e69dbd94e1073a610b7e5d72e3d7f1bc1b5
larry <26318510+larry0x@users.noreply.github.com>
2025-05-12 21:20:31 +0100

    update account factory tests

--- dango/testing/src/hyperlane.rs
+++ dango/testing/src/hyperlane.rs
@@ -1,14 +1,16 @@
 use {
     dango_genesis::Contracts,
     dango_types::{gateway::Domain, warp::TokenMessage},
-    grug::{Addr, Coins, Hash256, ResultExt, Signer, TestSuite, Uint128},
-    grug_app::{AppError, Db, Indexer, ProposalPreparer, Vm},
+    grug::{Addr, Addressable, Coins, Hash256, ResultExt, Signer, TestSuite, Uint128},
+    grug_app::{AppError, Db, Indexer, NaiveProposalPreparer, NullIndexer, ProposalPreparer, Vm},
+    grug_db_memory::MemDb,
+    grug_vm_rust::RustVm,
     hyperlane_testing::{MockValidatorSets, constants::MOCK_HYPERLANE_LOCAL_DOMAIN},
     hyperlane_types::{Addr32, mailbox},
     std::ops::{Deref, DerefMut},
 };
 
-pub struct HyperlaneTestSuite<DB, VM, PP, ID>
+pub struct HyperlaneTestSuite<DB = MemDb, VM = RustVm, PP = NaiveProposalPreparer, ID = NullIndexer>
 where
     DB: Db,
     VM: Vm + Clone + 'static,
@@ -71,22 +73,26 @@ where
         }
     }
 
-    pub fn receive_warp_transfer(
+    pub fn receive_warp_transfer<R, A>(
         &mut self,
         relayer: &mut dyn Signer,
         origin_domain: Domain,
         origin_warp: Addr32,
-        recipient: Addr,
-        amount: Uint128,
-    ) -> Hash256 {
+        recipient: &R,
+        amount: A,
+    ) -> Hash256
+    where
+        R: Addressable,
+        A: Into<Uint128>,
+    {
         // Mock validator set signs the message.
         let (message_id, raw_message, raw_metadata) = self.validator_sets.get(origin_domain).sign(
             origin_warp,
             MOCK_HYPERLANE_LOCAL_DOMAIN,
             self.warp,
             TokenMessage {
-                recipient: recipient.into(),
-                amount,
+                recipient: recipient.address().into(),
+                amount: amount.into(),
                 metadata: Default::default(),
             }
             .encode(),
