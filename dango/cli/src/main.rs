fe77db3d978056d339c98a5eed33f2dd779370e6
Zexsor33 <93092171+Zexsor33@users.noreply.github.com>
2025-05-14 21:32:16 +0200

    Add cli command to test pyth-stream (#705)
    * add cli command to test pyth-stream
    
    * minor changes
    
    ---------
    
    Co-authored-by: larry <26318510+larry0x@users.noreply.github.com>

--- dango/cli/src/main.rs
+++ dango/cli/src/main.rs
@@ -5,8 +5,12 @@ mod keys;
 mod prompt;
 mod query;
 mod start;
+#[cfg(feature = "testing")]
+mod test;
 mod tx;
 
+#[cfg(feature = "testing")]
+use crate::test::TestCmd;
 use {
     crate::{
         db::DbCmd, home_directory::HomeDirectory, keys::KeysCmd, query::QueryCmd, start::StartCmd,
@@ -48,6 +52,10 @@ enum Command {
     /// Start the node
     Start(StartCmd),
 
+    /// Run test
+    #[cfg(feature = "testing")]
+    Test(TestCmd),
+
     /// Send transactions
     #[command(next_display_order = None)]
     Tx(TxCmd),
@@ -88,6 +96,8 @@ async fn main() -> anyhow::Result<()> {
         Command::Keys(cmd) => cmd.run(app_dir.keys_dir()),
         Command::Query(cmd) => cmd.run(app_dir).await,
         Command::Start(cmd) => cmd.run(app_dir).await,
+        #[cfg(feature = "testing")]
+        Command::Test(cmd) => cmd.run().await,
         Command::Tx(cmd) => cmd.run(app_dir).await,
     }
 }
