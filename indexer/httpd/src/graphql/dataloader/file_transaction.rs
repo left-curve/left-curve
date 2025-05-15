784e94b9ac6b02a1e5f13ddddf5638e42d6be234
larry <26318510+larry0x@users.noreply.github.com>
2025-05-15 22:54:02 +0100

    use grug cache

--- indexer/httpd/src/graphql/dataloader/file_transaction.rs
+++ indexer/httpd/src/graphql/dataloader/file_transaction.rs
@@ -2,40 +2,12 @@ use {
     crate::graphql::types::transaction::Transaction,
     anyhow::{anyhow, ensure},
     async_graphql::dataloader::Loader,
-    grug_types::{Tx, TxOutcome},
+    grug_types::{Cache, Tx, TxOutcome},
     indexer_sql::{block_to_index::BlockToIndex, error::IndexerError, indexer_path::IndexerPath},
-    std::{
-        collections::{HashMap, hash_map::Entry},
-        sync::Arc,
-    },
+    std::{collections::HashMap, sync::Arc},
 };
 
-pub struct BlockCache<'a> {
-    pub blocks: HashMap<u64, BlockToIndex>,
-    pub indexer: &'a IndexerPath,
-}
-
-impl BlockCache<'_> {
-    pub fn new(indexer: &IndexerPath) -> BlockCache {
-        BlockCache {
-            indexer,
-            blocks: HashMap::new(),
-        }
-    }
-
-    pub fn get_or_load_from_disk(
-        &mut self,
-        block_height: u64,
-    ) -> Result<&BlockToIndex, IndexerError> {
-        match self.blocks.entry(block_height) {
-            Entry::Vacant(entry) => {
-                let block = BlockToIndex::load_from_disk(self.indexer.block_path(block_height))?;
-                Ok(entry.insert(block))
-            },
-            Entry::Occupied(entry) => Ok(entry.into_mut()),
-        }
-    }
-}
+type BlockCache<'a> = Cache<'a, u64, BlockToIndex, IndexerError>;
 
 pub struct FileTransactionDataLoader {
     pub indexer: IndexerPath,
@@ -49,12 +21,14 @@ impl Loader<Transaction> for FileTransactionDataLoader {
         &self,
         keys: &[Transaction],
     ) -> Result<HashMap<Transaction, Self::Value>, Self::Error> {
-        let mut cache = BlockCache::new(&self.indexer);
+        let mut cache = BlockCache::new(|block_height, _| {
+            BlockToIndex::load_from_disk(self.indexer.block_path(*block_height))
+        });
 
         keys.iter()
             .map(|graphql_tx| {
                 // Load the block.
-                let indexed_block = cache.get_or_load_from_disk(graphql_tx.block_height)?;
+                let indexed_block = cache.get_or_fetch(&graphql_tx.block_height, None)?;
 
                 // Find the transaction in the block.
                 let (tx, hash) = indexed_block
