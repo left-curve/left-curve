f1d077c212681ed5a7c42eb2c679ba7fe4e3a267
Rhaki <86773669+Rhaki@users.noreply.github.com>
2025-05-15 17:18:37 +0200

    add FileTransactionLoader to schema

--- indexer/httpd/src/graphql.rs
+++ indexer/httpd/src/graphql.rs
@@ -42,6 +42,15 @@ pub fn build_schema(app_ctx: Context) -> AppSchema {
         tokio::spawn,
     );
 
+    let file_transaction_loader: DataLoader<
+        dataloader::file_transaction::FileTranscationDataLoader,
+    > = DataLoader::new(
+        dataloader::file_transaction::FileTranscationDataLoader {
+            indexer: app_ctx.indexer_path.clone(),
+        },
+        tokio::spawn,
+    );
+
     Schema::build(
         query::Query::default(),
         mutation::Mutation::default(),
@@ -54,5 +63,6 @@ pub fn build_schema(app_ctx: Context) -> AppSchema {
     .data(block_events_loader)
     .data(transaction_messages_loader)
     .data(transaction_events_loader)
+    .data(file_transaction_loader)
     .finish()
 }
