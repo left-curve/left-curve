e20c006f52c0e3e3b9f2beff115f883ac12a58fd
j0nl1 <jonlitech@gmail.com>
2025-05-16 15:16:31 +0100

    feat: filter default favorite applets

--- ui/portal/web/constants.config.ts
+++ ui/portal/web/constants.config.ts
@@ -7,8 +7,9 @@ export const DEFAULT_SESSION_EXPIRATION = 24 * 60 * 60 * 1000; // 24 hours
 export const APPLETS = Array.from(
   { length: Object.keys(m).filter((k) => k.includes("applet")).length },
   (_, i) => {
-    if (m[`applets.${i as 0}.title`]) {
+    if (m[`applets.${i as 0}.id`]) {
       return {
+        id: m[`applets.${i as 0}.id`](),
         title: m[`applets.${i as 0}.title`](),
         description: m[`applets.${i as 0}.description`](),
         img: m[`applets.${i as 0}.img`](),
@@ -19,7 +20,9 @@ export const APPLETS = Array.from(
   },
 ).filter(Boolean) as AppletMetadata[];
 
-export const DEFAULT_FAV_APPLETS = APPLETS.reduce((acc, applet) => {
+export const DEFAULT_FAV_APPLETS = APPLETS.filter(({ id }) =>
+  ["transfer", "settings", "notifications", "simple-swap"].includes(id),
+).reduce((acc, applet) => {
   acc[applet.id] = applet;
   return acc;
 }, Object.create({}));
