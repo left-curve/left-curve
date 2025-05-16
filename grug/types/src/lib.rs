ba2df1d09195a9b0ef43200a703fa51778e144b1
larry <26318510+larry0x@users.noreply.github.com>
2025-05-16 10:41:38 +0100

    move the `GIT_COMMIT` constant to grug-types

--- grug/types/src/lib.rs
+++ grug/types/src/lib.rs
@@ -69,3 +69,9 @@ pub use {client::*, testing::*};
 pub mod __private {
     pub use {::borsh, ::hex_literal, ::serde, ::serde_json, ::serde_with};
 }
+
+// --------------------------------- constants ---------------------------------
+
+/// The git commit hash of the current build. This is replaced by Ansible during
+/// deployment.
+pub const GIT_COMMIT: &str = "{{GIT_COMMIT}}";
