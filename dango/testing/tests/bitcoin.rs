735b5ce2d36d393a1232c1c0dc5efc989bc6c098
Zexsor33 <c.mirco89@gmail.com>
2025-05-20 17:37:57 +0200

    add initialize and observe_inbound  test

--- dango/testing/tests/bitcoin.rs
+++ dango/testing/tests/bitcoin.rs
@@ -1,7 +1,7 @@
 use {
     corepc_client::bitcoin::Network,
     dango_testing::setup_test_naive,
-    dango_types::bitcoin::{BitcoinAddress, Config, ExecuteMsg, InstantiateMsg},
+    dango_types::bitcoin::{Config, ExecuteMsg, InstantiateMsg},
     grug::{
         Coins, ContractBuilder, Hash256, Message, NonEmpty, Order, ResultExt, Uint128, btree_set,
     },
@@ -10,6 +10,113 @@ use {
 
 #[test]
 fn instantiate() {
+    let (mut suite, accounts, ..) = setup_test_naive(Default::default());
+
+    let mut owner = accounts.owner;
+    let owner_address = owner.address.inner().clone();
+
+    let bitcoin_code = ContractBuilder::new(Box::new(dango_bitcoin::instantiate))
+        .with_execute(Box::new(dango_bitcoin::execute))
+        .with_query(Box::new(dango_bitcoin::query))
+        .build();
+
+    let bitcoin_hash = suite
+        .upload(&mut owner, bitcoin_code)
+        .should_succeed()
+        .code_hash;
+
+    // Try to instantiate the contract with wrong address.
+    {
+        let config = Config {
+            network: Network::Bitcoin,
+            vault: "48656c6c6f2044616e676f21".to_string(),
+            guardians: NonEmpty::new_unchecked(btree_set!(
+                accounts.user1.address.inner().clone(),
+                accounts.user2.address.inner().clone(),
+                accounts.user3.address.inner().clone(),
+            )),
+            threshold: 2,
+            sats_per_vbyte: Uint128::new(10),
+            outbound_fee: Uint128::new(10),
+            outbound_strategy: Order::Ascending,
+        };
+
+        suite
+            .instantiate(
+                &mut owner,
+                bitcoin_hash,
+                &InstantiateMsg { config },
+                "salt",
+                Some("bitcoin_bridge"),
+                Some(owner_address),
+                Coins::new(),
+            )
+            .should_fail();
+    }
+
+    // Try to instantiate the contract with wrong combination:
+    // - Network::Testnet
+    // - vault address a valid bitcoin mainnet address.
+    {
+        let config = Config {
+            network: Network::Testnet,
+            vault: "1PuJjnF476W3zXfVYmJfGnouzFDAXakkL4".to_string(),
+            guardians: NonEmpty::new_unchecked(btree_set!(
+                accounts.user1.address.inner().clone(),
+                accounts.user2.address.inner().clone(),
+                accounts.user3.address.inner().clone(),
+            )),
+            threshold: 2,
+            sats_per_vbyte: Uint128::new(10),
+            outbound_fee: Uint128::new(10),
+            outbound_strategy: Order::Ascending,
+        };
+
+        suite
+            .instantiate(
+                &mut owner,
+                bitcoin_hash,
+                &InstantiateMsg { config },
+                "salt",
+                Some("bitcoin_bridge"),
+                Some(owner_address),
+                Coins::new(),
+            )
+            .should_fail_with_error("validation error");
+    }
+
+    // Try to instantiate the contract with right combination.
+    {
+        let config = Config {
+            network: Network::Bitcoin,
+            vault: "1PuJjnF476W3zXfVYmJfGnouzFDAXakkL4".to_string(),
+            guardians: NonEmpty::new_unchecked(btree_set!(
+                accounts.user1.address.inner().clone(),
+                accounts.user2.address.inner().clone(),
+                accounts.user3.address.inner().clone(),
+            )),
+            threshold: 2,
+            sats_per_vbyte: Uint128::new(10),
+            outbound_fee: Uint128::new(10),
+            outbound_strategy: Order::Ascending,
+        };
+
+        suite
+            .instantiate(
+                &mut owner,
+                bitcoin_hash,
+                &InstantiateMsg { config },
+                "salt",
+                Some("bitcoin_bridge"),
+                Some(owner_address),
+                Coins::new(),
+            )
+            .should_succeed();
+    }
+}
+
+#[test]
+fn observe_inbound() {
     let (mut suite, mut accounts, ..) = setup_test_naive(Default::default());
 
     let mut owner = accounts.owner;
@@ -21,8 +128,8 @@ fn instantiate() {
         .build();
 
     let config = Config {
-        network: Network::Testnet,
-        vault: BitcoinAddress::default(),
+        network: Network::Bitcoin,
+        vault: "1PuJjnF476W3zXfVYmJfGnouzFDAXakkL4".to_string(),
         guardians: NonEmpty::new_unchecked(btree_set!(
             accounts.user1.address.inner().clone(),
             accounts.user2.address.inner().clone(),
@@ -48,7 +155,7 @@ fn instantiate() {
 
     let contract = res.address;
 
-    // Make a deposit to the contract.
+    // Report a deposit.
     let msg = ExecuteMsg::ObserveInbound {
         transaction_hash: Hash256::from_str(
             "C42F8B7FEFBDDE209F16A3084D9A5B44913030322F3AF27459A980674A7B9356",
