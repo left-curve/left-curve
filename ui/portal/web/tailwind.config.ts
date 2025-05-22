8aba4d2bc0e9d0dec7a3213ba2c5db0bf7310e55
j0nl1 <jonlitech@gmail.com>
2025-05-22 22:52:57 +0100

    fix: remove blue color for scrollbar

--- ui/portal/web/tailwind.config.ts
+++ ui/portal/web/tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 import { tailwindConfig } from "../../config/tailwind/config";
 
 const config: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../applets/kit/src/**/*.{js,ts,jsx,tsx}",
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../applets/kit/src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
