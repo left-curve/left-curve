4aeb2ea6bafc6bb62509897572d6287a9353155a
Fabien Penso <git@pen.so>
2025-05-23 18:15:54 +0200

    Refactor

--- dango/testing/src/account_creation.rs
+++ dango/testing/src/account_creation.rs
@@ -1,26 +1,21 @@
 use {
-    super::{Factory, HyperlaneTestSuite},
+    super::HyperlaneTestSuite,
     crate::{TestAccount, TestAccounts},
     dango_genesis::{Codes, Contracts},
     dango_proposal_preparer::ProposalPreparer,
     dango_types::{
         account::single::Params,
-        account_factory::{
-            self, AccountParams, AccountType, QueryCodeHashRequest, QueryNextAccountIndexRequest,
-            RegisterUserData, Salt, Username,
-        },
+        account_factory::{self, AccountParams, Username},
+        auth::Key,
         constants::usdc,
     },
-    grug::{Addr, Coins, ContractWrapper, Defined, HashExt, JsonSerExt, Op, QuerierExt, ResultExt},
+    grug::{Coins, ContractWrapper, Hash256, HashExt, JsonSerExt, Op, ResultExt},
     grug_db_memory::MemDb,
     grug_vm_rust::RustVm,
     hyperlane_types::constants::solana,
     indexer_sql::non_blocking_indexer::NonBlockingIndexer,
     pyth_client::PythClientCache,
-    std::{
-        ops::{Deref, DerefMut},
-        str::FromStr,
-    },
+    std::{ops::DerefMut, str::FromStr},
 };
 
 pub fn create_user_account(
@@ -34,45 +29,6 @@ pub fn create_user_account(
     test_account: &mut TestAccount,
 ) {
     test_account.register_user(suite.deref_mut(), contracts.account_factory, Coins::new());
-    // let chain_id = suite.chain_id.clone();
-
-    // suite
-    //     .execute(
-    //         &mut Factory::new(contracts.account_factory),
-    //         contracts.account_factory,
-    //         &account_factory::ExecuteMsg::RegisterUser {
-    //             seed: 0,
-    //             username: test_account.username.clone(),
-    //             key: test_account.first_key(),
-    //             key_hash: test_account.first_key_hash(),
-    //             signature: test_account
-    //                 .sign_arbitrary(RegisterUserData {
-    //                     username: test_account.username.clone(),
-    //                     chain_id,
-    //                 })
-    //                 .unwrap(),
-    //         },
-    //         Coins::new(),
-    //     )
-    //     .should_succeed();
-
-    // let index = suite
-    //     .deref()
-    //     .query_wasm_smart(contracts.account_factory, QueryNextAccountIndexRequest {})
-    //     .unwrap();
-
-    // let code_hash = suite
-    //     .deref()
-    //     .query_wasm_smart(contracts.account_factory, QueryCodeHashRequest {
-    //         account_type: AccountType::Spot,
-    //     })
-    //     .should_succeed();
-
-    // let address = Addr::derive(
-    //     contracts.account_factory,
-    //     code_hash,
-    //     Salt { index }.into_bytes().as_slice(),
-    // );
 }
 
 pub fn add_user_public_key(
@@ -83,13 +39,13 @@ pub fn add_user_public_key(
         NonBlockingIndexer<dango_indexer_sql::hooks::Hooks>,
     >,
     contracts: &Contracts,
-    mut test_account: TestAccount,
-) {
+    test_account: &mut TestAccount,
+) -> (Key, Hash256) {
     let (_, pk) = TestAccount::new_key_pair();
     let key_hash = pk.to_json_vec().unwrap().hash256();
     suite
         .execute(
-            &mut test_account,
+            test_account,
             contracts.account_factory,
             &account_factory::ExecuteMsg::UpdateKey {
                 key: Op::Insert(pk),
@@ -98,6 +54,8 @@ pub fn add_user_public_key(
             Coins::new(),
         )
         .should_succeed();
+
+    (pk, key_hash)
 }
 
 pub fn add_account_with_existing_user(
