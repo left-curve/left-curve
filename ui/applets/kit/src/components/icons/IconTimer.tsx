9d6d6f6b7d648584650cb2db82181ea9e13d4ee8
j0nl1 <jonlitech@gmail.com>
2025-05-23 16:10:47 +0100

    feat: icon network

--- ui/applets/kit/src/components/icons/IconTimer.tsx
+++ ui/applets/kit/src/components/icons/IconTimer.tsx
@@ -10,9 +10,9 @@ export const IconTimer: React.FC<React.SVGAttributes<HTMLOrSVGElement>> = ({ ...
       viewBox="0 0 24 25"
       {...props}
     >
-      <circle cx="12" cy="14" r="9" stroke="#000" strokeWidth="2.5" />
+      <circle cx="12" cy="14" r="9" stroke="currentColor" strokeWidth="2.5" />
       <path
-        stroke="#000"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeWidth="2.5"
         d="M10 2h4M12 2v3M12 14l3-3M18.5 7l.5-.5"
