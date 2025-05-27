15836b3073c82fb4397aa20d41dd4a1a28c73345
kyar1s <iris.ledesma.27@gmail.com>
2025-05-27 14:45:45 +0100

    feat: mock table

--- ui/applets/kit/src/components/Cell.tsx
+++ ui/applets/kit/src/components/Cell.tsx
@@ -7,6 +7,8 @@ import { twMerge } from "#utils/twMerge.js";
 import { AddressVisualizer } from "./AddressVisualizer";
 
 import type { Address, IndexedMessage } from "@left-curve/dango/types";
+import { format } from "date-fns";
+
 import type { FormatNumberOptions } from "@left-curve/dango/utils";
 import type { AnyCoin } from "@left-curve/store/types";
 import type React from "react";
@@ -14,6 +16,7 @@ import type { PropsWithChildren } from "react";
 import { Badge } from "./Badge";
 import { TextCopy } from "./TextCopy";
 import { IconLink } from "./icons/IconLink";
+import { Button } from "./Button";
 
 const Container: React.FC<PropsWithChildren> = ({ children }) => {
   return <>{children}</>;
@@ -54,6 +57,7 @@ type CellMarketPriceProps = {
   formatOptions: FormatNumberOptions;
   denom: string;
 };
+
 const MarketPrice: React.FC<CellMarketPriceProps> = ({ denom, className, formatOptions }) => {
   const { prices = {} } = usePrices();
   const price = prices[denom] || {};
