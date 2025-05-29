273810f39d7ef01a4234d44db070b52400eff0e2
Fabien Penso <git@pen.so>
2025-05-29 20:06:13 +0100

    Fix

--- indexer/httpd/src/graphql/mutation/tendermint.rs
+++ indexer/httpd/src/graphql/mutation/tendermint.rs
@@ -1,5 +1,6 @@
 use {
     async_graphql::*,
+    grug_math::Inner,
     grug_types::{JsonDeExt, JsonSerExt, Tx},
     sentry::configure_scope,
 };
@@ -12,25 +13,25 @@ impl TendermintMutation {
     async fn broadcast_tx_sync(
         &self,
         ctx: &async_graphql::Context<'_>,
-        #[graphql(desc = "Json string of the transaction to broadcast")] tx: String,
-    ) -> Result<String, Error> {
+        #[graphql(desc = "Transaction as JSON")] tx: serde_json::Value,
+    ) -> Result<serde_json::Value, Error> {
         let app_ctx = ctx.data::<crate::context::Context>()?;
 
-        let decoded_tx: Tx = tx.deserialize_json()?;
+        let decoded_tx: Tx = tx.to_string().deserialize_json()?;
 
         match app_ctx
             .consensus_client
             .broadcast_tx(decoded_tx.clone())
             .await
         {
-            Ok(response) => Ok(response.to_json_string()?),
+            Ok(response) => Ok(response.to_json_value()?.into_inner()),
             Err(e) => {
                 #[cfg(feature = "tracing")]
                 tracing::error!(error = ?e, tx = ?decoded_tx, "broadcast_tx_sync failed");
 
                 configure_scope(|scope| {
                     // NOTE: Sentry might truncate data if too large.
-                    scope.set_extra("transaction", tx.into());
+                    scope.set_extra("transaction", tx);
                 });
 
                 Err(e.into())
