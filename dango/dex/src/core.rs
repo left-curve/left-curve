092cfd63b521646baae10c02000319c3254eb76c
larry <26318510+larry0x@users.noreply.github.com>
2025-06-04 20:03:55 +0100

    rename `orders.rs` to `types.rs`

--- dango/dex/src/core.rs
+++ dango/dex/src/core.rs
@@ -4,10 +4,10 @@ mod market_order;
 mod merged_orders;
 mod order_filling;
 mod order_matching;
-mod orders;
 mod router;
+mod types;
 
 pub use {
     invariant::*, liquidity_pool::*, market_order::*, merged_orders::*, order_filling::*,
-    order_matching::*, orders::*, router::*,
+    order_matching::*, router::*, types::*,
 };
