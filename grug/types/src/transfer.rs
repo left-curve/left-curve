4b8767744ef2797a9d7242087b92b494f907f1b0
Larry <26318510+larry0x@users.noreply.github.com>
2025-06-05 07:24:49 +0100

    feat(grug): transfer builder (#797)

---
+++ grug/types/src/transfer.rs
@@ -0,0 +1,50 @@
+use {
+    crate::{Addr, Coin, Coins, Denom, Message, StdResult},
+    grug_math::Uint128,
+    std::collections::BTreeMap,
+};
+
+#[derive(Default)]
+pub struct TransferBuilder {
+    batch: BTreeMap<Addr, Coins>,
+}
+
+impl TransferBuilder {
+    pub fn new() -> Self {
+        Self {
+            batch: BTreeMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, address: Addr, denom: Denom, amount: Uint128) -> StdResult<()> {
+        self.batch
+            .entry(address)
+            .or_default()
+            .insert(Coin { denom, amount })
+            .map(|_| ())
+    }
+
+    pub fn insert_many(&mut self, address: Addr, coins: Coins) -> StdResult<()> {
+        self.batch
+            .entry(address)
+            .or_default()
+            .insert_many(coins)
+            .map(|_| ())
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.batch.is_empty()
+    }
+
+    pub fn is_non_empty(&self) -> bool {
+        !self.batch.is_empty()
+    }
+
+    pub fn into_batch(self) -> BTreeMap<Addr, Coins> {
+        self.batch
+    }
+
+    pub fn into_message(self) -> Message {
+        Message::Transfer(self.batch)
+    }
+}
