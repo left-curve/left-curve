e35607a23eff82ff9e93b3325250b2e8de2021b8
larry <26318510+larry0x@users.noreply.github.com>
2025-06-11 22:07:17 +0100

    simplify geometric iterators (avoid using `zip`)

--- dango/dex/src/core/geometric.rs
+++ dango/dex/src/core/geometric.rs
@@ -2,8 +2,8 @@ use {
     crate::core::xyk,
     dango_oracle::OracleQuerier,
     grug::{
-        Bounded, CoinPair, Inner, IsZero, MultiplyFraction, Number, NumberConst, StdResult,
-        Udec128, Uint128, ZeroExclusiveOneExclusive, ZeroExclusiveOneInclusive,
+        Bounded, CoinPair, IsZero, MultiplyFraction, Number, NumberConst, StdResult, Udec128,
+        Uint128, ZeroExclusiveOneExclusive, ZeroExclusiveOneInclusive,
     },
     std::iter,
 };
@@ -42,8 +42,8 @@ pub fn swap_exact_amount_out() -> StdResult<Uint128> {
 }
 
 pub fn reflect_curve(
-    mut base_reserve: Uint128,
-    mut quote_reserve: Uint128,
+    base_reserve: Uint128,
+    quote_reserve: Uint128,
     ratio: Bounded<Udec128, ZeroExclusiveOneInclusive>,
     order_spacing: Udec128,
     swap_fee_rate: Bounded<Udec128, ZeroExclusiveOneExclusive>,
@@ -59,35 +59,22 @@ pub fn reflect_curve(
         let one_sub_fee_rate = Udec128::ONE.checked_sub(*swap_fee_rate)?;
         let bid_starting_price = marginal_price.checked_mul(one_sub_fee_rate)?;
         let mut maybe_price = Some(bid_starting_price);
+        let mut remaining_quote = quote_reserve;
 
-        let bid_prices = iter::from_fn(move || {
-            let price = match maybe_price {
-                Some(price) if price.is_non_zero() => price,
-                _ => return None,
-            };
+        iter::from_fn(move || {
+            let price = maybe_price?;
+            if price.is_zero() {
+                return None;
+            }
+
+            let size_in_quote = remaining_quote.checked_mul_dec(*ratio).ok()?;
+            let size = size_in_quote.checked_div_dec_floor(price).ok()?;
 
             maybe_price = price.checked_sub(order_spacing).ok();
+            remaining_quote.checked_sub_assign(size_in_quote).ok()?;
 
-            Some(price)
-        });
-
-        let bid_sizes_in_quote = iter::from_fn(move || {
-            let size = match quote_reserve.checked_mul_dec(ratio.into_inner()) {
-                Ok(size_in_quote) => size_in_quote,
-                Err(_) => return None,
-            };
-
-            quote_reserve.checked_sub_assign(size).ok()?;
-
-            Some(size)
-        });
-
-        bid_prices
-            .zip(bid_sizes_in_quote)
-            .filter_map(|(price, size_in_quote)| {
-                let size = size_in_quote.checked_div_dec_floor(price).ok()?;
-                Some((price, size))
-            })
+            Some((price, size))
+        })
     };
 
     // Construct ask price iterator with increasing prices.
@@ -95,28 +82,17 @@ pub fn reflect_curve(
         let one_plus_fee_rate = Udec128::ONE.checked_add(*swap_fee_rate)?;
         let ask_starting_price = marginal_price.checked_mul(one_plus_fee_rate)?;
         let mut maybe_price = Some(ask_starting_price);
+        let mut remaining_base = base_reserve;
 
-        let ask_prices = iter::from_fn(move || {
-            let price = match maybe_price {
-                Some(price) if price.is_non_zero() => price,
-                _ => return None,
-            };
+        iter::from_fn(move || {
+            let price = maybe_price?;
+            let size = remaining_base.checked_mul_dec(*ratio).ok()?;
+
             maybe_price = price.checked_add(order_spacing).ok();
-            Some(price)
-        });
+            remaining_base.checked_sub_assign(size).ok()?;
 
-        // Construct ask size placing `ratio` of the remaining liquidity of
-        // base reserve into each order.
-        let ask_sizes = iter::from_fn(move || {
-            let size = match base_reserve.checked_mul_dec(ratio.into_inner()) {
-                Ok(size) => size,
-                Err(_) => return None,
-            };
-            base_reserve.checked_sub_assign(size).ok()?;
-            Some(size)
-        });
-
-        ask_prices.zip(ask_sizes)
+            Some((price, size))
+        })
     };
 
     Ok((Box::new(bids), Box::new(asks)))
