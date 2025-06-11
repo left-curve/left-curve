f397c7e38415fa15810d8f3f61b6919f0612ddca
kyar1s <iris.ledesma.27@gmail.com>
2025-06-11 13:37:47 +0100

    fix: header scroll image

--- ui/portal/web/src/components/foundation/Header.tsx
+++ ui/portal/web/src/components/foundation/Header.tsx
@@ -54,12 +54,9 @@ export const Header: React.FC<HeaderProps> = ({ isScrolled }) => {
       <div className="gap-4 relative flex flex-wrap lg:flex-nowrap items-center justify-center xl:grid xl:grid-cols-4 max-w-[76rem] mx-auto p-4">
         <Link to="/" className="w-fit">
           <img
-            src={isScrolled ? "/images/dango.svg" : "/favicon.svg"}
+            src="/favicon.svg"
             alt="dango logo"
-            className={twMerge(
-              "h-11 order-1 cursor-pointer hidden lg:flex rounded-full shadow-btn-shadow-gradient",
-              { "h-8 shadow-none rounded-none pl-3": isScrolled },
-            )}
+            className="h-11 order-1 cursor-pointer hidden lg:flex rounded-full shadow-btn-shadow-gradient"
           />
         </Link>
         <div
