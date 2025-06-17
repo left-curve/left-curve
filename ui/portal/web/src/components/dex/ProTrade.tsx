580e223771a3d7366d10add326e4acbd03fa6a0f
kyar1s <iris.ledesma.27@gmail.com>
2025-06-17 16:47:36 +0100

    feat: No open orders

--- ui/portal/web/src/components/dex/ProTrade.tsx
+++ ui/portal/web/src/components/dex/ProTrade.tsx
@@ -127,73 +127,102 @@ const ProTradeOrders: React.FC = () => {
   const { state } = useProTradeState();
   const { orders } = state;
 
-  const columns: TableColumn<OrdersByUserResponse & { id: number }> = [
-    /*  {
+  console.log("Orders:", orders.data.length);
+
+  const columns: TableColumn<OrdersByUserResponse & { id: number }> = orders.data.length
+    ? [
+        /*  {
       header: "Time",
       cell: ({ row }) => <Cell.Time date={row.original.time} />,
     }, */
-    {
-      header: "Type",
-      cell: ({ row }) => <Cell.Text text="Limit" />,
-    },
-    {
-      header: "Coin",
-      cell: ({ row }) => {
-        return <Cell.Asset noImage denom={row.original.baseDenom} />;
-      },
-    },
-    {
-      header: "Direction",
-      cell: ({ row }) => (
-        <Cell.OrderDirection
-          text={m["dex.protrade.spot.direction"]({ direction: row.original.direction })}
-          direction={row.original.direction}
-        />
-      ),
-    },
-    {
-      header: "Size",
-      cell: ({ row }) => <Cell.Text text={row.original.remaining} />,
-    },
-    {
-      header: "Original Size",
-      cell: ({ row }) => <Cell.Text text={row.original.amount} />,
-    },
-    {
-      header: "Price",
-      cell: ({ row }) => <Cell.Text text={row.original.price} />,
-    },
-    {
-      id: "cancel-order",
-      header: () => (
-        <Cell.Action
-          action={() =>
-            signingClient?.batchUpdateOrders({ cancels: "all", sender: account!.address })
-          }
-          label="Cancel All"
-          classNames={{
-            cell: "items-end diatype-xs-regular",
-            button: "!exposure-xs-italic m-0 px-1 h-fit ",
-          }}
-        />
-      ),
-      cell: ({ row }) => (
-        <Cell.Action
-          action={() =>
-            signingClient?.batchUpdateOrders({
-              sender: account!.address,
-              cancels: { some: [row.original.id] },
-            })
-          }
-          label="Cancel"
-          classNames={{ cell: "items-end", button: "!exposure-xs-italic m-0 px-1 h-fit" }}
-        />
-      ),
-    },
-  ];
+        {
+          header: "Type",
+          cell: ({ row }) => <Cell.Text text="Limit" />,
+        },
+        {
+          header: "Coin",
+          cell: ({ row }) => {
+            return <Cell.Asset noImage denom={row.original.baseDenom} />;
+          },
+        },
+        {
+          header: "Direction",
+          cell: ({ row }) => (
+            <Cell.OrderDirection
+              text={m["dex.protrade.spot.direction"]({ direction: row.original.direction })}
+              direction={row.original.direction}
+            />
+          ),
+        },
+        {
+          header: "Size",
+          cell: ({ row }) => <Cell.Text text={row.original.remaining} />,
+        },
+        {
+          header: "Original Size",
+          cell: ({ row }) => <Cell.Text text={row.original.amount} />,
+        },
+        {
+          header: "Price",
+          cell: ({ row }) => <Cell.Text text={row.original.price} />,
+        },
+        {
+          id: "cancel-order",
+          header: () => (
+            <Cell.Action
+              action={() =>
+                signingClient?.batchUpdateOrders({ cancels: "all", sender: account!.address })
+              }
+              label="Cancel All"
+              classNames={{
+                cell: "items-end diatype-xs-regular",
+                button: "!exposure-xs-italic m-0 px-1 h-fit ",
+              }}
+            />
+          ),
+          cell: ({ row }) => (
+            <Cell.Action
+              action={() =>
+                signingClient?.batchUpdateOrders({
+                  sender: account!.address,
+                  cancels: { some: [row.original.id] },
+                })
+              }
+              label="Cancel"
+              classNames={{ cell: "items-end", button: "!exposure-xs-italic m-0 px-1 h-fit" }}
+            />
+          ),
+        },
+      ]
+    : [
+        {
+          header: "Type",
+          cell: () => <Cell.Text text="No open orders yet" />,
+        },
+        {
+          header: "Coin",
+          cell: () => <Cell.Text text="" />,
+        },
+        {
+          header: "Direction",
+          cell: () => <Cell.Text text="" />,
+        },
+        {
+          header: "Size",
+          cell: () => <Cell.Text text="" />,
+        },
+        {
+          header: "Original Size",
+          cell: () => <Cell.Text text="" />,
+        },
+        {
+          header: "Price",
+          cell: () => <Cell.Text text="" />,
+        },
+      ];
 
   return (
-    <div className="flex-1 p-4 !pr-2 bg-rice-25 flex flex-col gap-2 shadow-card-shadow pb-20 lg:pb-0">
+    <div className="flex-1 p-4 !pr-2 bg-rice-25 flex flex-col gap-2 shadow-card-shadow pb-20 lg:pb-0 z-10">
       <div className="relative">
         <Tabs
           color="line-red"
@@ -207,7 +236,31 @@ const ProTradeOrders: React.FC = () => {
         <span className="w-full absolute h-[1px] bg-gray-100 bottom-[0.25rem]" />
       </div>
       {activeTab === "open order" ? (
-        <Table data={orders.data} columns={columns} style="simple" classNames={{ row: "h-fit" }} />
+        <Table
+          data={
+            orders.data.length
+              ? orders.data
+              : [
+                  {
+                    id: 0,
+                    baseDenom: "",
+                    quoteDenom: "",
+                    direction: 0,
+                    price: "",
+                    amount: "",
+                    remaining: "",
+                  },
+                ]
+          }
+          columns={columns}
+          style="simple"
+          classNames={{
+            row: "h-fit",
+            cell: twMerge("diatype-xs-regular", {
+              "group-hover:bg-transparent": !orders.data.length,
+            }),
+          }}
+        />
       ) : null}
     </div>
   );
