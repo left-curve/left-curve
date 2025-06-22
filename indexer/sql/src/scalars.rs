ee441f5ee03cc3ba6fb0a6442db6f603ad3b7d79
larry <26318510+larry0x@users.noreply.github.com>
2025-06-22 05:03:41 +0100

    formatting for consistency

--- indexer/sql/src/scalars.rs
+++ indexer/sql/src/scalars.rs
@@ -2,36 +2,36 @@ use {
     crate::serde_iso8601,
     async_graphql::{InputValueError, InputValueResult, Scalar, ScalarType, Value},
     grug_types::Timestamp,
-    sqlx::types::chrono::NaiveDateTime,
+    sqlx::types::chrono,
 };
 
 /// A custom DateTime scalar that ensures ISO8601 compliance with timezone information
 #[derive(Clone, Debug)]
-pub struct Iso8601DateTime(pub NaiveDateTime);
+pub struct Iso8601DateTime(pub chrono::NaiveDateTime);
 
 #[Scalar(name = "DateTime")]
 impl ScalarType for Iso8601DateTime {
-    fn parse(value: Value) -> InputValueResult<Self> {
-        serde_iso8601::deserialize(value)
-            .map(Self)
-            .map_err(InputValueError::custom)
-    }
-
-    /// Convert NaiveDateTime to Timestamp and use its RFC3339 formatting.
+    /// Convert `NaiveDateTime` to `Timestamp` and use its RFC3339 formatting.
     fn to_value(&self) -> Value {
         let s = Timestamp::from(self.0).to_rfc3339_string();
 
         Value::String(s)
     }
+
+    fn parse(value: Value) -> InputValueResult<Self> {
+        serde_iso8601::deserialize(value)
+            .map(Self)
+            .map_err(InputValueError::custom)
+    }
 }
 
-impl From<NaiveDateTime> for Iso8601DateTime {
-    fn from(dt: NaiveDateTime) -> Self {
+impl From<chrono::NaiveDateTime> for Iso8601DateTime {
+    fn from(dt: chrono::NaiveDateTime) -> Self {
         Self(dt)
     }
 }
 
-impl From<Iso8601DateTime> for NaiveDateTime {
+impl From<Iso8601DateTime> for chrono::NaiveDateTime {
     fn from(dt: Iso8601DateTime) -> Self {
         dt.0
     }
