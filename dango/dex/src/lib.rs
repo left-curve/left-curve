58035e9cc3be485551aac87f7047b91650704402
larry <26318510+larry0x@users.noreply.github.com>
2025-06-24 06:03:44 +0100

    make the max staleness constant public

--- dango/dex/src/lib.rs
+++ dango/dex/src/lib.rs
@@ -7,4 +7,4 @@ mod state;
 pub use {core::*, cron::*, execute::*, query::*, state::*};
 
 /// If an oracle price is older than this, it is not used for the logics in this contract.
-const MAX_ORACLE_STALENESS: grug::Duration = grug::Duration::from_seconds(5);
+pub const MAX_ORACLE_STALENESS: grug::Duration = grug::Duration::from_seconds(5);
