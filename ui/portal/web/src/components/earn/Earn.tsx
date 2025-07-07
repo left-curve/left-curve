454084d9dc5dcd6ae1522131a1d323686c3cfeb4
Kyaris <45215699+kyar1s@users.noreply.github.com>
2025-07-07 17:30:45 +0200

    feat(ui): account tabs (#893)
    * feat: create an EmptyPlaceholder Component
    
    * feat: add translations
    
    * feat: add account tabs
    
    * fix: styles
    
    * fix: add LP
    
    * fix: animation
    
    * feat: use orders by user hook
    
    * feat: orders in menu
    
    * fix: remove exit animation function
    
    * feat: show inner assets in lp
    
    * fix: remove hover in wallet assets
    
    ---------
    
    Co-authored-by: j0nl1 <jonlitech@gmail.com>

--- ui/portal/web/src/components/earn/Earn.tsx
+++ ui/portal/web/src/components/earn/Earn.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 import { uid } from "@left-curve/dango/utils";
 import { m } from "~/paraglide/messages";
 
-import { StrategyCard, createContext } from "@left-curve/applets-kit";
+import { EmptyPlaceholder, StrategyCard, createContext } from "@left-curve/applets-kit";
 import { Cell, Table } from "@left-curve/applets-kit";
 import { useAccount, useAppConfig, useBalances, useConfig } from "@left-curve/store";
 
@@ -124,9 +124,7 @@ const EarnUserPoolsTable: React.FC = () => {
         columns={columns}
         isLoading={isLoading}
         emptyComponent={
-          <div className="flex flex-col gap-1 items-center justify-center p-2 w-full bg-[url('./images/notifications/bubble-bg.svg')] bg-[50%_1rem] [background-size:100vw] bg-no-repeat rounded-xl bg-rice-50 h-[7rem]">
-            <p className="diatype-sm-regular text-gray-700">{m["earn.noLiquidity"]()}</p>
-          </div>
+          <EmptyPlaceholder component={m["earn.noLiquidity"]()} className="h-[7rem]" />
         }
       />
     </div>
