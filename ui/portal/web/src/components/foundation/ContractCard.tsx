625a3fef5fa28225767b0a015f5c1207596f968d
kyar1s <iris.ledesma.27@gmail.com>
2025-07-18 14:08:03 +0100

    fix: text color mobile card

--- ui/portal/web/src/components/foundation/ContractCard.tsx
+++ ui/portal/web/src/components/foundation/ContractCard.tsx
@@ -13,7 +13,7 @@ type ContractCardProps = {
 
 export const ContractCard: React.FC<ContractCardProps> = ({ address, balance, balanceChange }) => {
   return (
-    <div className="shadow-account-card w-full max-w-[22.5rem] md:max-w-[20.5rem] lg:min-w-[20.5rem] h-[10rem] relative overflow-hidden rounded-xl flex flex-col justify-between p-4 bg-account-card-contract">
+    <div className="shadow-account-card w-full max-w-[22.5rem] md:max-w-[20.5rem] lg:min-w-[20.5rem] h-[10rem] relative overflow-hidden rounded-xl flex flex-col justify-between p-4 bg-account-card-contract text-secondary-700">
       <img
         src="/images/emojis/detailed/factory.svg"
         alt="factory"
