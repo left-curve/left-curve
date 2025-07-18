2ca9916bb8a932eea6f955ef087f14257be10745
Rhaki <86773669+Rhaki@users.noreply.github.com>
2025-07-18 18:43:46 +0200

    revert a change

--- dango/mocks/httpd/src/bin/mock.rs
+++ dango/mocks/httpd/src/bin/mock.rs
@@ -2,13 +2,10 @@ use {
     dango_genesis::GenesisOption,
     dango_mock_httpd::{BlockCreation, Error, TestOption},
     dango_testing::Preset,
-    grug_testing::setup_tracing_subscriber,
-    tracing::Level,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    setup_tracing_subscriber(Level::INFO);
     dango_mock_httpd::run(
         8080,
         BlockCreation::OnBroadcast,
