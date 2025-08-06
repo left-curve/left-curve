fa451c093530c70ac53f6300ea0efabf1f433008
kyar1s <iris.ledesma.27@gmail.com>
2025-08-06 07:34:18 +0100

    feat: add FullPage scrolling animation

--- ui/portal/web/src/components/overview/AppletsSection.tsx
+++ ui/portal/web/src/components/overview/AppletsSection.tsx
@@ -10,20 +10,22 @@ export function AppletsSection() {
   const { setSearchBarVisibility } = useApp();
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,_minmax(64px,_1fr))] lg:grid-cols-[repeat(auto-fill,_minmax(80px,_1fr))] gap-4 md:gap-8 w-full md:min-h-fit">
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(64px,_1fr))] lg:flex lg:flex-wrap gap-4 md:gap-8 w-full md:min-h-fit lg:justify-center">
       {Object.values(favApplets).map((applet) => (
-        <div key={applet.title} className="flex flex-col items-center gap-2">
-          <Link
-            to={applet.path}
-            className="h-16 w-16 md:h-20 md:w-20 shadow-account-card bg-primary-red hover:bg-surface-secondary-red transition-all rounded-xl p-[10px]"
-          >
-            <img src={applet.img} alt={applet.title} className="w-full h-full" />
-          </Link>
-          <p className="text-sm font-bold text-center">{applet.title}</p>
+        <div className="w-[80px] h-auto fles items-center justify-center">
+          <div key={applet.title} className="flex flex-col items-center gap-2">
+            <Link
+              to={applet.path}
+              className="h-16 w-16 md:h-20 md:w-20 shadow-account-card bg-primary-red hover:bg-surface-secondary-red transition-all rounded-xl p-[10px]"
+            >
+              <img src={applet.img} alt={applet.title} className="w-full h-full" />
+            </Link>
+            <p className="text-sm font-bold text-center">{applet.title}</p>
+          </div>
         </div>
       ))}
 
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-center gap-2 w-[80px]">
         <button
           type="button"
           onClick={() => setSearchBarVisibility(true)}
--- ui/portal/web/src/pages/(app)/_app.index.tsx
+++ ui/portal/web/src/pages/(app)/_app.index.tsx
@@ -1,9 +1,17 @@
+import ReactFullpage from "@fullpage/react-fullpage";
 import { createFileRoute } from "@tanstack/react-router";
 import { m } from "~/paraglide/messages";
 
 import { useMediaQuery, useTheme } from "@left-curve/applets-kit";
 import { AppletsSection } from "~/components/overview/AppletsSection";
 import { SearchMenu } from "~/components/foundation/SearchMenu";
+
+export function getFullpageLicenseKey() {
+  if (!process.env.NEXT_PUBLIC_FULLPAGE_KEY) return "FALLBACK_KEY";
+  return new TextDecoder("utf-8", { fatal: true }).decode(
+    Uint8Array.from(atob(process.env.NEXT_PUBLIC_FULLPAGE_KEY), (c) => c.charCodeAt(0)),
+  );
+}
 
 export const Route = createFileRoute("/(app)/_app/")({
   head: () => ({
@@ -15,17 +23,77 @@ export const Route = createFileRoute("/(app)/_app/")({
 function OverviewComponent() {
   const { isLg } = useMediaQuery();
   const { theme } = useTheme();
+
   return (
-    <div className="w-full lg:max-w-3xl mx-auto flex flex-col gap-6 p-4 pt-6 pb-16 flex-1">
-      <div className="w-full flex flex-col gap-6 min-h-full lg:min-h-fit relative flex-1 items-center justify-between lg:items-center lg:justify-center lg:gap-16 lg:pb-60">
-        <img
-            src={`/images/dango${theme === "dark" ? "-dark" : ""}.svg`}
-            alt="Dango"
-            className="max-w-[10rem] lg:max-w-[13rem]"
-            />
-            {isLg && <div className="relative w-full h-11"> <SearchMenu /></div>}
-        <AppletsSection />
-      </div>
+    <div className="w-full mx-auto flex flex-col gap-6 pt-4 lg:pt-0 pb-16 flex-1">
+      <ReactFullpage
+        licenseKey={getFullpageLicenseKey()}
+        scrollingSpeed={1000}
+        credits={{ enabled: false }}
+        render={() => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section min-h-svh">
+                <div className="lg:max-w-3xl mx-auto p-4 w-full flex flex-col gap-6 relative flex-1 items-center justify-between lg:items-center lg:justify-center lg:gap-16 lg:pb-60">
+                  <img
+                    src={`/images/dango${theme === "dark" ? "-dark" : ""}.svg`}
+                    alt="Dango"
+                    className="max-w-[10rem] lg:max-w-[13rem]"
+                  />
+                  {isLg && (
+                    <div className="relative w-full h-11">
+                      {" "}
+                      <SearchMenu />
+                    </div>
+                  )}
+                  <AppletsSection />
+                </div>
+              </div>
+              {/* {isSearchBarVisible ? null : (
+                <>
+                  <section className="section w-full min-h-svh flex items-center justify-center">
+                    Section 1
+                  </section>
+                  <section className="section w-full min-h-svh flex items-center justify-center">
+                    Section 2
+                  </section>
+                </>
+              )} */}
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+      {/*       <ReactFullpage
+        licenseKey={getFullpageLicenseKey()}
+        scrollingSpeed={1000}
+        credits={{ enabled: false }}
+        render={() => {
+          return (
+            <ReactFullpage.Wrapper>
+              <header className="lg:max-w-3xl mx-auto border border-red-500 p-4 min-h-[calc(100svh-80px)] w-full flex flex-col gap-6 relative flex-1 items-center justify-between lg:items-center lg:justify-center lg:gap-16 lg:pb-60">
+                <img
+                  src={`/images/dango${theme === "dark" ? "-dark" : ""}.svg`}
+                  alt="Dango"
+                  className="max-w-[10rem] lg:max-w-[13rem]"
+                />
+                {isLg && (
+                  <div className="relative w-full h-11">
+                    {" "}
+                    <SearchMenu />
+                  </div>
+                )}
+                <AppletsSection />
+              </header>
+              <section className="border border-red-500 w-full min-h-[calc(100svh-80px)] flex items-center justify-center">
+                Section 1
+              </section>
+              <section className="border border-red-500 w-full min-h-[calc(100svh-80px)] flex items-center justify-center">
+                Section 2
+              </section>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      /> */}
     </div>
   );
 }
