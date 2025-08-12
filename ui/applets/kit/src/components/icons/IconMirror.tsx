81d2680978574d12b7a177227afd395ebdda7268
kyar1s <iris.ledesma.27@gmail.com>
2025-08-12 18:07:08 +0100

    fix: right svg format

--- ui/applets/kit/src/components/icons/IconMirror.tsx
+++ ui/applets/kit/src/components/icons/IconMirror.tsx
@@ -8,10 +8,10 @@ export const IconMirror: React.FC<React.SVGAttributes<HTMLOrSVGElement>> = ({ ..
         fill="url(#:r0:)"
       ></path>
       <path
-        clip-rule="evenodd"
+        clipRule="evenodd"
         d="M134.717 176.111V71.8216C134.717 36.8684 106.465 8.53326 71.6129 8.53326C36.7613 8.53326 8.50846 36.8684 8.50846 71.8216V176.111C8.50846 176.308 8.66719 176.467 8.86298 176.467H134.363C134.559 176.467 134.717 176.308 134.717 176.111ZM71.6129 0C32.0622 0 0 32.1556 0 71.8216V176.111C0 181.02 3.96809 185 8.86298 185H134.363C139.258 185 143.226 181.02 143.226 176.111V71.8216C143.226 32.1556 111.164 0 71.6129 0Z"
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       ></path>
       <defs>
         <linearGradient
@@ -22,8 +22,8 @@ export const IconMirror: React.FC<React.SVGAttributes<HTMLOrSVGElement>> = ({ ..
           y1="10.6666"
           y2="209.447"
         >
-          <stop offset="0.265625" stop-color="rgb(245 117 137)"></stop>
-          <stop offset="0.734375" stop-color="currentColor"></stop>
+          <stop offset="0.265625" stopColor="rgb(245 117 137)"></stop>
+          <stop offset="0.734375" stopColor="currentColor"></stop>
         </linearGradient>
       </defs>
     </svg>
