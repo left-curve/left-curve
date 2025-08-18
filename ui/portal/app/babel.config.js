64fcd76d8962028a732ba74ad83a41f45ea8efb7
j0nl1 <jonlitech@gmail.com>
2025-08-18 19:38:47 +0100

    feat: react native crypto

--- ui/portal/app/babel.config.js
+++ ui/portal/app/babel.config.js
@@ -2,5 +2,17 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            crypto: "react-native-quick-crypto",
+            stream: "readable-stream",
+            buffer: "@craftzdog/react-native-buffer",
+          },
+        },
+      ],
+    ],
   };
 };
--- ui/portal/app/index.ts
+++ ui/portal/app/index.ts
@@ -1,3 +1,7 @@
+import { install } from "react-native-quick-crypto";
+
+install();
+
 import "./assets/global.css";
 
 import "expo-router/entry";
