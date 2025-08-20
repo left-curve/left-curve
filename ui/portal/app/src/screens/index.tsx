e685107e0644d1deebb5a36a27c3adfd54ddd590
kyar1s <iris.ledesma.27@gmail.com>
2025-08-20 19:18:09 +0100

    feat: test theme

--- ui/portal/app/src/screens/index.tsx
+++ ui/portal/app/src/screens/index.tsx
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native";
-import { useInputs, twMerge } from "@left-curve/applets-kit";
+import { twMerge } from "@left-curve/applets-kit";
+import { View } from "react-native";
+import { Button, GlobalText, IconAddCross, IconAlert, iconColors, IconSun } from "~/components";
+import { useTheme } from "~/hooks/useTheme";
 
 export default function HomeScreen() {
-  const { inputs } = useInputs();
+  const { theme, setThemeSchema } = useTheme();
   return (
-    <View className={twMerge("flex-1 items-center justify-center bg-white", "bg-red-500")}>
-      <Text className="text-xl font-bold text-tertiary-green">Welcome to Dango!</Text>
+    <View className="flex-1 flex items-center justify-center bg-surface-primary-rice w-full flex-col gap-8">
+      <GlobalText>Theme: {theme}</GlobalText>
+
+      <Button
+        variant="utility"
+        size="md"
+        onPress={() => setThemeSchema(theme === "light" ? "dark" : "light")}
+      >
+        <IconSun size={28} className="w-8 h-8" color={iconColors[theme]["utility"]} /> Set Theme
+      </Button>
     </View>
   );
 }
