f88cbb462d863ac79353f79c34e710baadf77b7d
larry <26318510+larry0x@users.noreply.github.com>
2025-08-20 20:53:55 +0100

    rename a few other error enum variants

--- indexer/clickhouse/src/error.rs
+++ indexer/clickhouse/src/error.rs
@@ -6,36 +6,34 @@ pub enum IndexerError {
     Io(#[from] std::io::Error),
 
     #[error(transparent)]
-    StdError(#[from] grug::StdError),
+    Std(#[from] grug::StdError),
 
     #[error(transparent)]
-    ClickhouseError(#[from] clickhouse::error::Error),
+    Math(#[from] grug::MathError),
+
+    #[error(transparent)]
+    Clickhouse(#[from] clickhouse::error::Error),
 
     #[error("missing block or block outcome")]
     MissingBlockOrBlockOutcome,
-
-    #[error(transparent)]
-    GrugMathError(#[from] grug::MathError),
 
     #[error("candle timeout")]
     CandleTimeout,
 
     #[error(transparent)]
-    ChronoParseError(#[from] chrono::ParseError),
+    ChronoParse(#[from] chrono::ParseError),
 
     #[error(transparent)]
     SerdeJson(#[from] serde_json::Error),
 
     #[error(transparent)]
-    PubSubError(#[from] PubSubError),
+    PubSub(#[from] PubSubError),
 }
 
 impl From<IndexerError> for grug_app::IndexerError {
     fn from(error: IndexerError) -> Self {
         match error {
-            IndexerError::ClickhouseError(error) => {
-                grug_app::IndexerError::Database(error.to_string())
-            },
+            IndexerError::Clickhouse(error) => grug_app::IndexerError::Database(error.to_string()),
             IndexerError::Io(error) => grug_app::IndexerError::Io(error.to_string()),
             err => grug_app::IndexerError::Hook(err.to_string()),
         }
