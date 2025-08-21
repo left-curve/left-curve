d1423061fab9e6337af1d6df92d41d16befadf48
Kyaris <45215699+kyar1s@users.noreply.github.com>
2025-08-21 20:48:32 +0200

    feat(ui): mobile app theme (#1081)
    * feat: expo packages
    
    * fix: dark className
    
    * feat: rn button
    
    * feat: theme provider & hook
    
    * feat: add layout
    
    * feat: add globaltext
    
    * feat: add icons
    
    * feat: export components
    
    * feat: test theme
    
    * fix: build
    
    * fix: replace radiusSizes
    
    * fix: add unique id
    
    * fix: package version
    
    * fix: button shadows
    
    * fix: button icon size
    
    * fix: replace AsyncStorage, use mmkv instead
    
    * fix: useTheme update
    
    * fix: systemSchema typo
    
    * fix: error setting theme
    
    * fix: theme context
    
    * fix: pick type util

--- ui/portal/app/src/screens/index.tsx
+++ ui/portal/app/src/screens/index.tsx
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native";
-import { useInputs, twMerge } from "@left-curve/applets-kit";
+import { View } from "react-native";
+import { Button, GlobalText, iconColors, IconSun } from "~/components";
+import { useTheme } from "~/hooks/useTheme";
 
 export default function HomeScreen() {
-  const { inputs } = useInputs();
+  const { theme, setThemeSchema } = useTheme();
   return (
-    <View className={twMerge("flex-1 items-center justify-center bg-white", "bg-red-500")}>
-      <Text className="text-xl font-bold text-tertiary-green">Welcome to Dango!</Text>
+    <View className="flex-1 flex items-center justify-center bg-surface-primary-rice w-full flex-col gap-8">
+      <GlobalText>Theme: {theme}</GlobalText>
+
+      <Button
+        variant="utility"
+        size="md"
+        onPress={() => setThemeSchema(theme === "light" ? "dark" : "light")}
+        leftIcon={<IconSun size={28} color={iconColors[theme]["utility"]} />}
+      >
+        Set Theme
+      </Button>
     </View>
   );
 }
