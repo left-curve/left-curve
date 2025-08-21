3d6a7204357d9537ef2b22f583733ff439884d8e
kyar1s <iris.ledesma.27@gmail.com>
2025-08-21 15:22:30 +0100

    fix: error setting theme

--- ui/portal/app/src/components/Layout.tsx
+++ ui/portal/app/src/components/Layout.tsx
@@ -3,11 +3,9 @@ import { View } from "react-native";
 import { useTheme } from "~/hooks/useTheme";
 
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
-  const { themeSchema } = useTheme();
+  const { theme } = useTheme();
   return (
-    <View
-      className={`flex-1 bg-surface-primary-rice text-primary-900 diatype-m-medium ${themeSchema}`}
-    >
+    <View className={`flex-1 bg-surface-primary-rice text-primary-900 diatype-m-medium ${theme}`}>
       {children}
     </View>
   );
