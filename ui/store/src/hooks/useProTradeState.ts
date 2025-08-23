630c96000853b001c55e04ad693e3ae574122d61
j0nl1 <jonlitech@gmail.com>
2025-08-23 14:19:13 +0100

    fix: invalidate query

--- ui/store/src/hooks/useProTradeState.ts
+++ ui/store/src/hooks/useProTradeState.ts
@@ -243,9 +243,9 @@ export function useProTradeState(parameters: UseProTradeStateParameters) {
         orders.refetch();
         updateBalance();
         controllers.reset();
-        queryClient.invalidateQueries({ queryKey: ["ordersByUser", account?.username] });
+        queryClient.invalidateQueries({ queryKey: ["ordersByUser", account?.address] });
         queryClient.invalidateQueries({ queryKey: ["quests", account?.username] });
-        queryClient.invalidateQueries({ queryKey: ["trades", account?.username] });
+        queryClient.invalidateQueries({ queryKey: ["trades", account?.address] });
       },
     },
   });
