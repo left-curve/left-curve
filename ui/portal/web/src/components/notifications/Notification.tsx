de284cb1e6c8e1c3b006c45b0946d49d3519dc50
j0nl1 <jonlitech@gmail.com>
2025-08-28 19:14:22 +0100

    feat: order filled

--- ui/portal/web/src/components/notifications/Notification.tsx
+++ ui/portal/web/src/components/notifications/Notification.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "@tanstack/react-router";
 import { useConfig } from "@left-curve/store";
 
-import { formatNumber, formatUnits } from "@left-curve/dango/utils";
+import { Decimal, formatNumber, formatUnits } from "@left-curve/dango/utils";
 import {
   differenceInDays,
   differenceInHours,
@@ -22,11 +22,14 @@ import {
   IconReceived,
   IconNewAccount,
   Badge,
+  IconLimitOrder,
+  IconMarketOrder,
 } from "@left-curve/applets-kit";
 
 import type { PropsWithChildren } from "react";
 import type React from "react";
 import { type Notification as NotificationType, useNotifications } from "~/hooks/useNotifications";
+import { Direction, OrderType } from "@left-curve/dango/types";
 
 const formatNotificationTimestamp = (timestamp: Date): string => {
   const now = new Date();
@@ -198,8 +201,8 @@ const NotificationAccount: React.FC<NotificationAccountProps> = ({ notification 
           <IconNewAccount className="text-brand-green h-4 w-4" />
         </div>
         <div className="flex flex-col max-w-[calc(100%)] overflow-hidden">
-          <div className="flex justify-center items-center gap-2 diatype-m-medium text-secondary-700">
-            <p>{m["notifications.notification.account.title"]({ accountType })}</p>
+          <div className="flex justify-center items-center gap-2 diatype-m-medium text-secondary-700 capitalize">
+            <p>{m["notifications.notification.account.title"]()}</p>
             <Badge className="capitalize" text={accountType} />
           </div>
           <AddressVisualizer address={address} withIcon onClick={onNavigate} />
@@ -216,7 +219,224 @@ const NotificationAccount: React.FC<NotificationAccountProps> = ({ notification 
   );
 };
 
+type NotificationOrderCreatedProps = {
+  notification: NotificationType<"orderCreated">;
+};
+
+const NotificationOrderCreated: React.FC<NotificationOrderCreatedProps> = ({ notification }) => {
+  const navigate = useNavigate();
+  const { settings, setNotificationMenuVisibility } = useApp();
+  const { deleteNotification } = useNotifications();
+  const { getCoinInfo } = useConfig();
+  const { id, blockHeight, createdAt, txHash } = notification;
+  const { quote_denom, base_denom, price, kind, deposit: depositInfo } = notification.data;
+  const { formatNumberOptions } = settings;
+
+  const base = getCoinInfo(base_denom);
+  const quote = getCoinInfo(quote_denom);
+  const deposit = getCoinInfo(depositInfo.denom);
+
+  const at = formatNumber(
+    Decimal(price)
+      .times(Decimal(10).pow(base.decimals - quote.decimals))
+      .toFixed(),
+    { ...formatNumberOptions, minSignificantDigits: 8, maxSignificantDigits: 8 },
+  ).slice(0, 7);
+
+  const width = formatNumber(
+    Decimal(depositInfo.amount).div(Decimal(10).pow(deposit.decimals)).toFixed(),
+    { ...formatNumberOptions, minSignificantDigits: 8, maxSignificantDigits: 8 },
+  ).slice(0, 7);
+
+  const isLimit = kind === OrderType.Limit;
+
+  const Icon = isLimit ? IconLimitOrder : IconMarketOrder;
+
+  const onNavigate = (url: string) => {
+    setNotificationMenuVisibility(false);
+    navigate({ to: url });
+  };
+
+  return (
+    <div className="flex items-end justify-between gap-2 p-2 rounded-lg hover:bg-surface-quaternary-rice max-w-full group">
+      <div
+        className="flex items-start gap-2 max-w-full overflow-hidden cursor-pointer"
+        onClick={(event) => {
+          const element = event.target as HTMLElement;
+          if (element.closest(".address-visualizer") || element.closest(".remove-notification")) {
+            return;
+          }
+          onNavigate(txHash ? `/tx/${txHash}` : `/block/${blockHeight}`);
+        }}
+      >
+        <div
+          className={twMerge(
+            "flex items-center justify-center min-w-7 min-h-7 w-7 h-7 rounded-sm",
+            isLimit ? "bg-blue-200" : "bg-tertiary-green",
+          )}
+        >
+          <Icon className={twMerge(isLimit ? "text-secondary-blue" : "text-brand-green")} />
+        </div>
+
+        <div className="flex flex-col max-w-[calc(100%)] overflow-hidden">
+          <div className="flex items-center gap-2 diatype-m-medium text-secondary-700 capitalize">
+            <span>{m["notifications.notification.orderCreated.title"]({ orderType: kind })}</span>
+            <Badge text="Spot" />
+          </div>
+
+          <div className={twMerge("flex-wrap flex items-center gap-1")}>
+            <span>{m["common.for"]()}</span>
+            <PairAssets
+              assets={[base, quote]}
+              className="w-5 h-5 min-w-5 min-h-5"
+              mL={(i) => `${-i / 2}rem`}
+            />
+            <span className="diatype-m-bold">
+              {base.symbol}-{quote.symbol}
+            </span>
+            <span>{m["common.at"]()}</span>
+            <span className="diatype-m-bold">
+              {at} {quote.symbol}
+            </span>
+            <span>{m["common.width"]()}</span>
+            <span className="diatype-m-bold">
+              {width} {deposit.symbol}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col diatype-sm-medium text-tertiary-500 min-w-fit items-center relative">
+        <IconClose
+          className="absolute w-6 h-6 cursor-pointer group-hover:block hidden top-[-26px] remove-notification"
+          onClick={() => deleteNotification(id)}
+        />
+        <p>{formatNotificationTimestamp(new Date(createdAt))}</p>
+      </div>
+    </div>
+  );
+};
+
+type NotificationOrderFilledProps = {
+  notification: NotificationType<"orderFilled">;
+};
+
+const NotificationOrderFilled: React.FC<NotificationOrderFilledProps> = ({ notification }) => {
+  const navigate = useNavigate();
+  const { settings, setNotificationMenuVisibility } = useApp();
+  const { deleteNotification } = useNotifications();
+  const { getCoinInfo } = useConfig();
+  const { id, blockHeight, createdAt, txHash } = notification;
+  const {
+    kind,
+    base_denom,
+    quote_denom,
+    clearing_price,
+    cleared,
+    direction,
+    filled_base,
+    filled_quote,
+  } = notification.data;
+
+  const { formatNumberOptions } = settings;
+
+  const opInfo =
+    direction === Direction.Buy
+      ? {
+          amount: filled_quote,
+          denom: quote_denom,
+        }
+      : {
+          amount: filled_base,
+          denom: base_denom,
+        };
+
+  const base = getCoinInfo(base_denom);
+  const quote = getCoinInfo(quote_denom);
+  const deposit = getCoinInfo(opInfo.denom);
+
+  const at = formatNumber(
+    Decimal(clearing_price)
+      .times(Decimal(10).pow(base.decimals - quote.decimals))
+      .toFixed(),
+    { ...formatNumberOptions, minSignificantDigits: 8, maxSignificantDigits: 8 },
+  ).slice(0, 7);
+
+  const width = formatNumber(
+    Decimal(opInfo.amount).div(Decimal(10).pow(deposit.decimals)).toFixed(),
+    { ...formatNumberOptions, minSignificantDigits: 8, maxSignificantDigits: 8 },
+  ).slice(0, 7);
+
+  const isLimit = kind === OrderType.Limit;
+
+  const Icon = isLimit ? IconLimitOrder : IconMarketOrder;
+
+  const onNavigate = (url: string) => {
+    setNotificationMenuVisibility(false);
+    navigate({ to: url });
+  };
+
+  return (
+    <div className="flex items-end justify-between gap-2 p-2 rounded-lg hover:bg-surface-quaternary-rice max-w-full group">
+      <div
+        className="flex items-start gap-2 max-w-full overflow-hidden cursor-pointer"
+        onClick={(event) => {
+          const element = event.target as HTMLElement;
+          if (element.closest(".address-visualizer") || element.closest(".remove-notification")) {
+            return;
+          }
+          onNavigate(txHash ? `/tx/${txHash}` : `/block/${blockHeight}`);
+        }}
+      >
+        <div
+          className={twMerge(
+            "flex items-center justify-center min-w-7 min-h-7 w-7 h-7 rounded-sm",
+            isLimit ? "bg-blue-200" : "bg-tertiary-green",
+          )}
+        >
+          <Icon className={twMerge(isLimit ? "text-secondary-blue" : "text-brand-green")} />
+        </div>
+
+        <div className="flex flex-col max-w-[calc(100%)] overflow-hidden">
+          <div className="flex items-center gap-2 diatype-m-medium text-secondary-700 capitalize">
+            <span>{m["notifications.notification.orderCreated.title"]({ orderType: kind })}</span>
+            <Badge text="Spot" />
+          </div>
+
+          <div className={twMerge("flex-wrap flex items-center gap-1")}>
+            <span>{m["common.for"]()}</span>
+            <PairAssets
+              assets={[base, quote]}
+              className="w-5 h-5 min-w-5 min-h-5"
+              mL={(i) => `${-i / 2}rem`}
+            />
+            <span className="diatype-m-bold">
+              {base.symbol}-{quote.symbol}
+            </span>
+            <span>{m["common.at"]()}</span>
+            <span className="diatype-m-bold">
+              {at} {quote.symbol}
+            </span>
+            <span>{m["common.width"]()}</span>
+            <span className="diatype-m-bold">
+              {width} {deposit.symbol}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col diatype-sm-medium text-tertiary-500 min-w-fit items-center relative">
+        <IconClose
+          className="absolute w-6 h-6 cursor-pointer group-hover:block hidden top-[-26px] remove-notification"
+          onClick={() => deleteNotification(id)}
+        />
+        <p>{formatNotificationTimestamp(new Date(createdAt))}</p>
+      </div>
+    </div>
+  );
+};
+
 export const Notification = Object.assign(Container, {
   Transfer: NotificationTransfer,
+  OrderCreated: NotificationOrderCreated,
+  OrderFilled: NotificationOrderFilled,
   Account: NotificationAccount,
 });
