9c3c239a4dd16d06b4190ebbf8e18dc8a833d7d4
kyar1s <iris.ledesma.27@gmail.com>
2025-09-02 19:45:47 +0100

    feat: add Truncate text

---
+++ ui/portal/app/src/components/foundation/TruncateText.tsx
@@ -0,0 +1,37 @@
+import type React from "react";
+import { twMerge } from "@left-curve/foundation";
+import type { TextProps } from "react-native-svg";
+import { GlobalText } from "./GlobalText";
+
+type Props = {
+  text?: string;
+  className?: string;
+  start?: number;
+  end?: number;
+};
+
+export const TruncateText: React.FC<Props & TextProps> = ({
+  text = "",
+  className,
+  start = 8,
+  end = 8,
+  ...props
+}) => {
+  if (text.length <= start + end) {
+    return (
+      <GlobalText className={twMerge(className)} {...props}>
+        {text}
+      </GlobalText>
+    );
+  }
+
+  return (
+    <GlobalText className={twMerge("flex-row", className)} {...props}>
+      <GlobalText>{text.slice(0, start)}</GlobalText>
+      <GlobalText>…</GlobalText>
+      <GlobalText>{text.slice(text.length - end)}</GlobalText>
+    </GlobalText>
+  );
+};
+
+export default TruncateText;
