7b136e7739cc28c2434e6c8cca8a8bed7183d358
j0nl1 <jonlitech@gmail.com>
2025-09-05 18:33:14 +0100

    refactor: order canceled

--- ui/portal/web/src/components/notifications/OrderCanceled.tsx
+++ ui/portal/web/src/components/notifications/OrderCanceled.tsx
@@ -1,13 +1,14 @@
 import { useConfig } from "@left-curve/store";
 
-import { Decimal } from "@left-curve/dango/utils";
-import { Direction } from "@left-curve/dango/types";
+import { Decimal, formatNumber } from "@left-curve/dango/utils";
+import { Direction, OrderType } from "@left-curve/dango/types";
 import { m } from "@left-curve/foundation/paraglide/messages.js";
 
 import { OrderNotification } from "./OrderNotification";
 
 import type { Notification } from "~/hooks/useNotifications";
 import type React from "react";
+import { PairAssets, twMerge, useApp } from "@left-curve/applets-kit";
 
 type NotificationOrderCanceledProps = {
   notification: Notification<"orderCanceled">;
@@ -18,32 +19,57 @@ export const NotificationOrderCanceled: React.FC<NotificationOrderCanceledProps>
 }) => {
   const { getCoinInfo } = useConfig();
   const { blockHeight, txHash } = notification;
-  const { baseDenom, quoteDenom, price, kind, amount, direction } = notification.data;
+  const { quote_denom, base_denom, price, kind, direction } = notification.data;
+  const { settings } = useApp();
+  const { formatNumberOptions } = settings;
 
-  const opInfo =
-    direction === Direction.Buy
-      ? {
-          amount: Decimal(amount).times(Decimal(price)).toFixed(),
-          denom: quoteDenom,
-        }
-      : {
-          amount: amount,
-          denom: baseDenom,
-        };
+  const isLimit = kind === OrderType.Limit;
 
-  const base = getCoinInfo(baseDenom);
-  const quote = getCoinInfo(quoteDenom);
-  const deposit = getCoinInfo(opInfo.denom);
+  const base = getCoinInfo(base_denom);
+  const quote = getCoinInfo(quote_denom);
+
+  const at = isLimit
+    ? formatNumber(
+        Decimal(price)
+          .times(Decimal(10).pow(base.decimals - quote.decimals))
+          .toFixed(),
+        { ...formatNumberOptions, minSignificantDigits: 8, maxSignificantDigits: 8 },
+      ).slice(0, 7)
+    : null;
 
   return (
-    <OrderNotification
-      title={m["notifications.notification.orderCanceled.title"]({ orderType: kind })}
-      details={{ price, amount: opInfo.amount, coin: deposit }}
-      base={base}
-      quote={quote}
-      kind={kind}
-      blockHeight={blockHeight}
-      txHash={txHash}
-    />
+    <OrderNotification kind={kind} txHash={txHash} blockHeight={blockHeight}>
+      <p className="flex items-center gap-2 diatype-m-medium text-secondary-700">
+        {m["notifications.notification.orderCanceled.title"]()}
+      </p>
+
+      <div className={twMerge("flex-wrap flex items-center gap-1")}>
+        <span>{m["dex.protrade.orderType"]({ orderType: kind })}</span>
+        <span
+          className={twMerge(
+            "uppercase diatype-m-bold",
+            direction === Direction.Buy ? "text-status-success" : "text-status-fail",
+          )}
+        >
+          {m["dex.protrade.spot.direction"]({ direction })}
+        </span>
+        <PairAssets
+          assets={[base, quote]}
+          className="w-5 h-5 min-w-5 min-h-5"
+          mL={(i) => `${-i / 2}rem`}
+        />
+        <span className="diatype-m-bold">
+          {base.symbol}-{quote.symbol}
+        </span>
+        {at ? (
+          <>
+            <span>{m["notifications.notification.orderCreated.atPrice"]()}</span>
+            <span className="diatype-m-bold">
+              {at} {quote.symbol}
+            </span>
+          </>
+        ) : null}
+      </div>
+    </OrderNotification>
   );
 };
