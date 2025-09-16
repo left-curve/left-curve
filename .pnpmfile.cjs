1ac7abcdf426c88b3c6e9c1be1f71e570c5e895a
j0nl1 <jonlitech@gmail.com>
2025-09-16 16:42:59 +0100

    chore: re-order pnpm file flow

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -13,7 +13,6 @@ async function fetchPackageMetadata(packageName, version) {
 module.exports = {
   hooks: {
     async readPackage(pkg, { log }) {
-      const metadata = await fetchPackageMetadata(pkg.name, pkg.version);
       if (
         pkg.name.includes("@left-curve") ||
         pkg.name.includes("webrtc-signaling") ||
@@ -21,20 +20,24 @@ module.exports = {
         pkg.name.includes("leftcurve-monorepo")
       )
         return pkg;
-      if (!metadata?.time?.[pkg.version])
-        throw new Error(`No publish time found for ${pkg.name}@${pkg.version}`);
-      if (metadata?.time?.[pkg.version]) {
-        const publishTime = new Date(metadata.time[pkg.version]);
-        const daysOld = (Date.now() - publishTime) / (1000 * 60 * 60 * 24);
 
-        if (daysOld < 14) {
-          throw new Error(
-            `Installation blocked: Package ${pkg.name}@${
-              pkg.version
-            } is only ${daysOld.toFixed(1)} days old.`,
-          );
-        }
+      const metadata = await fetchPackageMetadata(pkg.name, pkg.version);
+
+      if (!metadata?.time?.[pkg.version]) {
+        throw new Error(`No publish time found for ${pkg.name}@${pkg.version}`);
       }
+
+      const publishTime = new Date(metadata.time[pkg.version]);
+      const daysOld = (Date.now() - publishTime) / (1000 * 60 * 60 * 24);
+
+      if (daysOld < 14) {
+        throw new Error(
+          `Installation blocked: Package ${pkg.name}@${
+            pkg.version
+          } is only ${daysOld.toFixed(1)} days old.`,
+        );
+      }
+
       log(`Package ${pkg.name}@${pkg.version} passed the age check.`);
       return pkg;
     },
