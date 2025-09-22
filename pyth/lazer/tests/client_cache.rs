879466469e58172633fde2c402ca14fb4c569be6
Zexsor33 <c.mirco89@gmail.com>
2025-09-22 22:43:18 +0200

    include lazer cache

--- pyth/lazer/tests/client_cache.rs
+++ pyth/lazer/tests/client_cache.rs
@@ -33,11 +33,9 @@ async fn test_lazer_stream() {
 fn create_cache() {
     let update_ids = LAZER_ID_ALL;
 
-    let mut client = PythClientLazerCache::new(
-        NonEmpty::new_unchecked(LAZER_ENDPOINTS_TEST),
-        "8groZyxhfobyiQod0c3tZxlDXhXQ3Xh8T9C-demo",
-    )
-    .unwrap();
+    let mut client =
+        PythClientLazerCache::new(NonEmpty::new_unchecked(LAZER_ENDPOINTS_TEST), "lazer_token")
+            .unwrap();
     client.load_or_retrieve_data(NonEmpty::new_unchecked(update_ids.clone()));
 
     for id in update_ids {
