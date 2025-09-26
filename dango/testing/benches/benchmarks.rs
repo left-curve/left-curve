3983ad5cea8e62116061a0c05093fef033901fcc
Rhaki <giacomo.encicliati@gmail.com>
2025-09-26 17:31:55 +0200

    init

--- dango/testing/benches/benchmarks.rs
+++ dango/testing/benches/benchmarks.rs
@@ -2,17 +2,23 @@ use {
     criterion::{
         AxisScale, BatchSize, Criterion, PlotConfiguration, criterion_group, criterion_main,
     },
+    dango_dex::ORDERS,
     dango_genesis::{Codes, Contracts},
     dango_testing::{TestAccounts, TestSuite, setup_benchmark_hybrid, setup_benchmark_wasm},
     dango_types::{
         account::single,
         account_factory::{self, AccountParams, Salt},
-        constants::usdc,
+        constants::{btc, dango, usdc},
+        dex::{Direction, Order, OrderId, Price, TimeInForce},
     },
-    grug::{Addr, Binary, Coins, HashExt, JsonSerExt, Message, NonEmpty, ResultExt, Tx, coins},
-    grug_app::{AppError, Db, ProposalPreparer, Vm},
+    grug::{
+        Addr, Binary, Buffer, Coins, Denom, HashExt, JsonSerExt, Message, NonEmpty, ResultExt,
+        Shared, Storage, Tx, Udec128_6, Uint128, coins,
+    },
+    grug_app::{AppError, CONTRACT_NAMESPACE, Db, ProposalPreparer, StorageProvider, Vm},
+    grug_db_disk_lite::DiskDbLite,
     rand::{Rng, distributions::Alphanumeric},
-    std::time::Duration,
+    std::{str::FromStr, time::Duration},
     temp_rocksdb::TempDataDir,
 };
 
@@ -176,6 +182,136 @@ fn sends(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, sends);
+fn setup_storage() -> (TempDataDir, StorageProvider) {
+    let dir = TempDataDir::new(&format!("__dango_bench_storage_{}", random_string(8)));
+    let db = DiskDbLite::open(&dir).unwrap();
+    let buffer = Buffer::new(db.state_storage(None).unwrap(), None);
+    let shared = Shared::new(buffer);
+    let mut storage = StorageProvider::new(Box::new(shared.clone()), &[
+        CONTRACT_NAMESPACE,
+        &Addr::mock(1),
+    ]);
+
+    let mut order_id: u64 = 0;
+
+    let data = [
+        (&dango::DENOM, Direction::Bid, "1.00"),
+        (&dango::DENOM, Direction::Bid, "1.01"),
+        (&dango::DENOM, Direction::Bid, "1.02"),
+        (&dango::DENOM, Direction::Bid, "1.03"),
+        (&dango::DENOM, Direction::Ask, "1.00"),
+        (&dango::DENOM, Direction::Ask, "1.01"),
+        (&dango::DENOM, Direction::Ask, "1.02"),
+        (&dango::DENOM, Direction::Ask, "1.03"),
+        (&btc::DENOM, Direction::Bid, "10234.1234"),
+        (&btc::DENOM, Direction::Bid, "10235.1235"),
+        (&btc::DENOM, Direction::Bid, "10236.1236"),
+        (&btc::DENOM, Direction::Ask, "10234.1234"),
+        (&btc::DENOM, Direction::Ask, "10235.1235"),
+        (&btc::DENOM, Direction::Ask, "10236.1236"),
+    ];
+
+    for (base, direction, price) in data {
+        let price = Price::from_str(price).unwrap();
+        order_id += 1;
+
+        let id = OrderId::new(if direction == Direction::Bid {
+            !order_id
+        } else {
+            order_id
+        });
+
+        ORDERS
+            .save(
+                &mut storage,
+                (((*base).clone(), usdc::DENOM.clone()), direction, price, id),
+                &Order {
+                    user: Addr::mock(2),
+                    id,
+                    direction,
+                    time_in_force: TimeInForce::GoodTilCanceled,
+                    price,
+                    amount: Uint128::new(100),
+                    remaining: Udec128_6::new(100),
+                    created_at_block_height: Some(123),
+                },
+            )
+            .unwrap();
+    }
+
+    drop(storage);
+
+    let (_, batch) = shared.disassemble().disassemble();
+
+    assert_eq!(batch.len(), data.len() * 4);
+    db.flush_and_commit(batch).unwrap();
+
+    let buffer = Buffer::new(db.state_storage(None).unwrap(), None);
+    let shared = Shared::new(buffer);
+    let storage = StorageProvider::new(Box::new(shared), &[CONTRACT_NAMESPACE, &Addr::mock(1)]);
+
+    (dir, storage)
+}
+
+fn routine_storage<S: Storage>(
+    (_dir, storage): (TempDataDir, S),
+    order: grug::Order,
+    base_denom: Denom,
+) {
+    let direction = if order == grug::Order::Ascending {
+        Direction::Ask
+    } else {
+        Direction::Bid
+    };
+
+    let mut bid_iter = ORDERS
+        .prefix((base_denom.clone(), usdc::DENOM.clone()))
+        .append(direction)
+        .values(&storage, None, None, order);
+
+    assert!(bid_iter.next().is_some());
+}
+
+fn storage(c: &mut Criterion) {
+    let mut group = c.benchmark_group("storage");
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Linear));
+    // group.sample_size(100)
+    group.measurement_time(std::time::Duration::from_secs(2)); // default 5s
+    group.warm_up_time(std::time::Duration::from_secs(1));
+
+    group.bench_function("bid-dango", |b| {
+        b.iter_batched(
+            setup_storage,
+            |a| routine_storage(a, grug::Order::Descending, dango::DENOM.clone()),
+            BatchSize::SmallInput,
+        );
+    });
+
+    group.bench_function("ask-dango", |b| {
+        b.iter_batched(
+            setup_storage,
+            |a| routine_storage(a, grug::Order::Ascending, dango::DENOM.clone()),
+            BatchSize::SmallInput,
+        );
+    });
+
+    group.bench_function("bid-btc", |b| {
+        b.iter_batched(
+            setup_storage,
+            |a| routine_storage(a, grug::Order::Descending, btc::DENOM.clone()),
+            BatchSize::SmallInput,
+        );
+    });
+
+    group.bench_function("ask-btc", |b| {
+        b.iter_batched(
+            setup_storage,
+            |a| routine_storage(a, grug::Order::Ascending, btc::DENOM.clone()),
+            BatchSize::SmallInput,
+        );
+    });
+}
+
+criterion_group!(benches, sends, storage);
 
 criterion_main!(benches);
