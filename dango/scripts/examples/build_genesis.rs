cb72e2fd1330c862d0ed89593f7f3d6f7ddad30d
Giacomo Encicliati <86773669+Rhaki@users.noreply.github.com>
2025-10-02 13:23:41 +0200

    refractor(infra): Genesis use Gemoetric instead Xyk (#1192)

--- dango/scripts/examples/build_genesis.rs
+++ dango/scripts/examples/build_genesis.rs
@@ -6,13 +6,24 @@ use {
     anyhow::anyhow,
     chrono::{DateTime, Utc},
     clap::Parser,
-    dango_genesis::{GenesisCodes, GenesisOption, build_genesis},
+    dango_genesis::{DexOption, GenesisCodes, GenesisOption, build_genesis},
     dango_testing::Preset,
-    grug::{Inner, Json, JsonDeExt, JsonSerExt},
+    dango_types::{
+        constants::{
+            FIFTY, ONE, ONE_HUNDRED, ONE_HUNDREDTH, ONE_TENTH, TEN, btc, dango, eth, sol, usdc,
+        },
+        dex::{Geometric, PairParams, PairUpdate, PassiveLiquidity},
+    },
+    grug::{
+        Bounded, Dec, Denom, Inner, Json, JsonDeExt, JsonSerExt, NonZero, NumberConst, Udec128,
+        Uint128, btree_set,
+    },
     grug_vm_rust::RustVm,
     std::{
+        collections::BTreeSet,
         fs,
         path::{Path, PathBuf},
+        str::FromStr,
     },
 };
 
@@ -34,8 +45,95 @@ struct Cli {
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    let (genesis_state, contracts, addresses) =
-        build_genesis(RustVm::genesis_codes(), GenesisOption::preset_test())?;
+    let (genesis_state, contracts, addresses) = build_genesis(
+        RustVm::genesis_codes(),
+        GenesisOption {
+            dex: DexOption {
+                pairs: vec![
+                    PairUpdate {
+                        base_denom: dango::DENOM.clone(),
+                        quote_denom: usdc::DENOM.clone(),
+                        params: PairParams {
+                            lp_denom: Denom::from_str("dex/pool/dango/usdc").unwrap(),
+                            pool_type: PassiveLiquidity::Geometric(Geometric {
+                                limit: 1,
+                                spacing: Udec128::new_bps(1),
+                                ratio: Bounded::new_unchecked(Dec::ONE),
+                            }),
+                            bucket_sizes: BTreeSet::new(), /* TODO: determine appropriate price buckets based on expected dango token price */
+                            swap_fee_rate: Bounded::new_unchecked(Udec128::new_bps(1)),
+                            min_order_size: Uint128::ZERO, /* TODO: for mainnet, a minimum of $10 is sensible */
+                        },
+                    },
+                    PairUpdate {
+                        base_denom: btc::DENOM.clone(),
+                        quote_denom: usdc::DENOM.clone(),
+                        params: PairParams {
+                            lp_denom: Denom::from_str("dex/pool/btc/usdc").unwrap(),
+                            pool_type: PassiveLiquidity::Geometric(Geometric {
+                                limit: 1,
+                                spacing: Udec128::new_bps(1),
+                                ratio: Bounded::new_unchecked(Dec::ONE),
+                            }),
+                            bucket_sizes: btree_set! {
+                                NonZero::new_unchecked(ONE_HUNDREDTH),
+                                NonZero::new_unchecked(ONE_TENTH),
+                                NonZero::new_unchecked(ONE),
+                                NonZero::new_unchecked(TEN),
+                                NonZero::new_unchecked(FIFTY),
+                                NonZero::new_unchecked(ONE_HUNDRED),
+                            },
+                            swap_fee_rate: Bounded::new_unchecked(Udec128::new_bps(1)),
+                            min_order_size: Uint128::ZERO,
+                        },
+                    },
+                    PairUpdate {
+                        base_denom: eth::DENOM.clone(),
+                        quote_denom: usdc::DENOM.clone(),
+                        params: PairParams {
+                            lp_denom: Denom::from_str("dex/pool/eth/usdc").unwrap(),
+                            pool_type: PassiveLiquidity::Geometric(Geometric {
+                                limit: 1,
+                                spacing: Udec128::new_bps(1),
+                                ratio: Bounded::new_unchecked(Dec::ONE),
+                            }),
+                            bucket_sizes: btree_set! {
+                                NonZero::new_unchecked(ONE_HUNDREDTH),
+                                NonZero::new_unchecked(ONE_TENTH),
+                                NonZero::new_unchecked(ONE),
+                                NonZero::new_unchecked(TEN),
+                                NonZero::new_unchecked(FIFTY),
+                                NonZero::new_unchecked(ONE_HUNDRED),
+                            },
+                            swap_fee_rate: Bounded::new_unchecked(Udec128::new_bps(1)),
+                            min_order_size: Uint128::ZERO,
+                        },
+                    },
+                    PairUpdate {
+                        base_denom: sol::DENOM.clone(),
+                        quote_denom: usdc::DENOM.clone(),
+                        params: PairParams {
+                            lp_denom: Denom::from_str("dex/pool/sol/usdc").unwrap(),
+                            pool_type: PassiveLiquidity::Geometric(Geometric {
+                                limit: 1,
+                                spacing: Udec128::new_bps(1),
+                                ratio: Bounded::new_unchecked(Dec::ONE),
+                            }),
+                            bucket_sizes: btree_set! {
+                                NonZero::new_unchecked(ONE_HUNDREDTH),
+                                NonZero::new_unchecked(ONE_TENTH),
+                                NonZero::new_unchecked(ONE),
+                                NonZero::new_unchecked(TEN),
+                            },
+                            swap_fee_rate: Bounded::new_unchecked(Udec128::new_bps(1)),
+                            min_order_size: Uint128::ZERO,
+                        },
+                    },
+                ],
+            },
+            ..Preset::preset_test()
+        },
+    )?;
 
     println!("genesis_state = {}", genesis_state.to_json_string_pretty()?);
     println!("\ncontracts = {}", contracts.to_json_string_pretty()?);
@@ -51,7 +149,13 @@ fn main() -> anyhow::Result<()> {
             genesis_state.clone(),
             chain_id.clone(),
             genesis_time.clone(),
-        )?;
+        )
+        .map_err(|e| {
+            anyhow!(
+                "failed to update genesis file {}\nreason: {e}",
+                path.display()
+            )
+        })?;
     }
 
     Ok(())
