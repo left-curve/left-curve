3a119466ac2535b55f87940ed8d5aad6eefaa2ad
Larry <26318510+larry0x@users.noreply.github.com>
2025-10-09 20:36:38 +0100

    dex: add more test cases for `get_bucket` related to ETH (#1210)
    * add more test cases related to ETH
    
    * more
    
    * Update dango/dex/src/liquidity_depth.rs
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    
    * Update dango/dex/src/liquidity_depth.rs
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- dango/dex/src/liquidity_depth.rs
+++ dango/dex/src/liquidity_depth.rs
@@ -116,8 +116,9 @@ pub fn decrease_liquidity_depths(
 mod tests {
     use {
         super::*,
-        dango_types::constants::mock::{
-            FIFTY, ONE, ONE_HUNDRED, ONE_HUNDREDTH, ONE_TENTH, ONE_THOUSANDTH, TEN,
+        dango_types::constants::{
+            eth_usdc,
+            mock::{FIFTY, ONE, ONE_HUNDRED, ONE_HUNDREDTH, ONE_TENTH, ONE_THOUSANDTH, TEN},
         },
         grug::Uint128,
         std::str::FromStr,
@@ -233,6 +234,80 @@ mod tests {
         Price::raw(Uint128::new(4368614282292957095945129368187275851))
         => Price::from_str("4368614282292.96").unwrap(); // ceil
         "no overflow - ask"
+    )]
+    // A few tests of real world scenario with Ether, which is tricky due to its
+    // 18 decimal places. We took the actual price of ETH from GoinGecko.
+    #[test_case(
+        *eth_usdc::ONE_HUNDREDTH,
+        Direction::Bid,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.00000000431614").unwrap() // $4316.14
+    )]
+    #[test_case(
+        *eth_usdc::ONE_HUNDREDTH,
+        Direction::Ask,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.00000000431614").unwrap() // $4316.14
+    )]
+    #[test_case(
+        *eth_usdc::ONE_TENTH,
+        Direction::Bid,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.0000000043161").unwrap() // $4316.1
+    )]
+    #[test_case(
+        *eth_usdc::ONE_TENTH,
+        Direction::Ask,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.0000000043162").unwrap() // $4316.2
+    )]
+    #[test_case(
+        *eth_usdc::ONE,
+        Direction::Bid,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004316").unwrap() // $4316
+    )]
+    #[test_case(
+        *eth_usdc::ONE,
+        Direction::Ask,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004317").unwrap() // $4317
+    )]
+    #[test_case(
+        *eth_usdc::TEN,
+        Direction::Bid,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004310").unwrap() // $4310
+    )]
+    #[test_case(
+        *eth_usdc::TEN,
+        Direction::Ask,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004320").unwrap() // $4320
+    )]
+    #[test_case(
+        *eth_usdc::FIFTY,
+        Direction::Bid,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004300").unwrap() // $4300
+    )]
+    #[test_case(
+        *eth_usdc::FIFTY,
+        Direction::Ask,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004350").unwrap() // $4350
+    )]
+    #[test_case(
+        *eth_usdc::ONE_HUNDRED,
+        Direction::Bid,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004300").unwrap() // $4300
+    )]
+    #[test_case(
+        *eth_usdc::ONE_HUNDRED,
+        Direction::Ask,
+        Price::from_str("0.00000000431614").unwrap() // $4316.14
+        => Price::from_str("0.000000004400").unwrap() // $4400
     )]
     fn getting_bucket(bucket_size: Price, direction: Direction, price: Price) -> Price {
         get_bucket(bucket_size, direction, price).unwrap()
