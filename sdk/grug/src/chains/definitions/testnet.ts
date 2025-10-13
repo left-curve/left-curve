abff8526f9a448d913662b54ce3fb6e808f70abe
j0nl1 <jonlitech@gmail.com>
2025-10-13 16:57:10 +0100

    feat: lb and proxy config

--- sdk/grug/src/chains/definitions/testnet.ts
+++ sdk/grug/src/chains/definitions/testnet.ts
@@ -11,6 +11,6 @@ export const testnet = /*#__PURE__*/ defineChain({
     contractPage: "/contract/${address}",
   },
   urls: {
-    indexer: "https://graphql.dango.exchange/graphql",
+    indexer: "http://api.testnet.dango.zone",
   },
 });
