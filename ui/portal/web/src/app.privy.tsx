294089ad4ef1c00c71d730d8d423ffab7bb91512
j0nl1 <jonlitech@gmail.com>
2025-10-14 14:37:16 +0100

    fix: don't wait for privy

--- ui/portal/web/src/app.privy.tsx
+++ ui/portal/web/src/app.privy.tsx
@@ -1,30 +1,11 @@
-import { Spinner } from "@left-curve/applets-kit";
-import { deserializeJson } from "@left-curve/dango/encoding";
 import { usePrivy, useWallets } from "@privy-io/react-auth";
-import { type PropsWithChildren, useEffect, useState } from "react";
+import { type PropsWithChildren, useEffect } from "react";
 
 import type React from "react";
 
 export const AppPrivy: React.FC<PropsWithChildren> = ({ children }) => {
-  const [isReady, setIsReady] = useState(false);
-  const { wallets, ready } = useWallets();
+  const { wallets } = useWallets();
   const { logout, authenticated } = usePrivy();
-
-  const theme = (() => {
-    const theme = localStorage.getItem("dango.app.theme");
-    const preferedSchema = window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light";
-    if (!theme) return preferedSchema;
-    const { value: themeSchema } = deserializeJson<{ value: string }>(theme);
-    if (themeSchema === "system") return preferedSchema;
-    return themeSchema;
-  })();
-
-  useEffect(() => {
-    const root = window?.document.documentElement;
-    root.classList.add(theme);
-  }, []);
 
   useEffect(() => {
     if (!authenticated) return;
@@ -35,16 +16,5 @@ export const AppPrivy: React.FC<PropsWithChildren> = ({ children }) => {
     (window as any).privy = Object.assign(wallet, { disconnect: logout });
   }, [wallets]);
 
-  useEffect(() => {
-    if (!ready) return;
-    setTimeout(() => setIsReady(true), 200);
-  }, [ready]);
-
-  return isReady ? (
-    children
-  ) : (
-    <div className="w-screen h-screen">
-      <Spinner fullContainer color="pink" size="lg" />
-    </div>
-  );
+  return children;
 };
