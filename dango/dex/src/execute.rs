d1ed8c4513af17e61bcd5bbcefb5bfe705084e5d
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2025-10-17 14:26:16 +0200

    docs: fix incorrect outdated code comment

--- dango/dex/src/execute.rs
+++ dango/dex/src/execute.rs
@@ -278,11 +278,6 @@ fn provide_liquidity(
             // If this is the first liquidity provision, mint a minimum liquidity.
             // to the contract itself and permanently lock it here. See the comment
             // on `MINIMUM_LIQUIDITY` for more details.
-            //
-            // Our implementation of this is slightly different from Uniswap's, which
-            // mints 1000 tokens less to the user, while we mint 1000 extra to
-            // the contract. Slightly different math but similarly prevents the
-            // attack.
             Some(Message::execute(
                 bank,
                 &bank::ExecuteMsg::Mint {
