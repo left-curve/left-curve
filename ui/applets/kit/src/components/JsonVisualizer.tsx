0a898f7ae64860515a29d91cd4d1453dd74f3d95
j0nl1 <jonlitech@gmail.com>
2025-10-22 14:29:25 +0100

    feat: json visualizer collapse

--- ui/applets/kit/src/components/JsonVisualizer.tsx
+++ ui/applets/kit/src/components/JsonVisualizer.tsx
@@ -1,4 +1,8 @@
-import { lazy } from "react";
+import { lazy, useState } from "react";
+
+import { twMerge } from "@left-curve/foundation";
+
+import { IconChevronDownFill } from "./icons/IconChevronDownFill";
 
 import type React from "react";
 import type { JsonString, JsonValue, OneRequired } from "@left-curve/dango/types";
@@ -10,34 +14,49 @@ type JsonVisualizerProps = {
 const JsonView = lazy(() => import("@microlink/react-json-view"));
 
 export const JsonVisualizer: React.FC<JsonVisualizerProps> = ({ json, string, collapsed }) => {
+  const [isCollapsed, setCollapsed] = useState(collapsed);
   const source = string ? JSON.parse(string) : json;
 
   return (
-    <JsonView
-      name={false}
-      src={source}
-      displayObjectSize={false}
-      displayDataTypes={false}
-      collapsed={collapsed}
-      indentWidth={1}
-      theme={{
-        base00: "#453d39",
-        base01: "#fffcf6",
-        base02: "#453d39",
-        base03: "#fffcf6",
-        base04: "#fffcf6",
-        base05: "#fffcf6",
-        base06: "#fffcf6",
-        base07: "#fff3e1", // Keys
-        base08: "#fffcf6",
-        base09: "#fffcf6", // Values
-        base0A: "#fffcf6",
-        base0B: "#fffcf6",
-        base0C: "#fffcf6",
-        base0D: "#d4882c", // Arrow Open
-        base0E: "#d4882c", // Arrow Close
-        base0F: "#fffcf6",
-      }}
-    />
+    <div className="relative">
+      <JsonView
+        name={false}
+        src={source}
+        shouldCollapse={() => false}
+        displayObjectSize={false}
+        displayDataTypes={false}
+        collapsed={isCollapsed}
+        indentWidth={1}
+        theme={{
+          base00: "#453d39",
+          base01: "#fffcf6",
+          base02: "#453d39",
+          base03: "#fffcf6",
+          base04: "#fffcf6",
+          base05: "#fffcf6",
+          base06: "#fffcf6",
+          base07: "#fff3e1", // Keys
+          base08: "#fffcf6",
+          base09: "#fffcf6", // Values
+          base0A: "#fffcf6",
+          base0B: "#fffcf6",
+          base0C: "#fffcf6",
+          base0D: "#d4882c", // Arrow Open
+          base0E: "#d4882c", // Arrow Close
+          base0F: "#fffcf6",
+        }}
+      />
+      <div style={{ position: "absolute", top: 0, right: 0 }}>
+        <button
+          type="button"
+          className="text-primitives-white-light-100 outline-none focus:outline-none"
+          onClick={() => setCollapsed(typeof isCollapsed === "number" ? false : collapsed)}
+        >
+          <IconChevronDownFill
+            className={twMerge("w-4 h-4 transition-all", isCollapsed ? "rotate-180" : "rotate-0")}
+          />
+        </button>
+      </div>
+    </div>
   );
 };
