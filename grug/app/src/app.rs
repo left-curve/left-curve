32df075a0f7dc7456deb12b0dd65dc5f28e28459
larry <26318510+larry0x@users.noreply.github.com>
2025-10-26 14:18:08 +0000

    format imports

--- grug/app/src/app.rs
+++ grug/app/src/app.rs
@@ -1,21 +1,20 @@
-use crate::{UPGRADES, query_upgrade, query_upgrades};
 #[cfg(all(feature = "abci", feature = "tracing"))]
 use data_encoding::BASE64;
 #[cfg(any(feature = "abci", feature = "tracing"))]
 use grug_types::JsonSerExt;
 #[cfg(feature = "abci")]
 use grug_types::{HashExt, JsonDeExt};
-
 use {
     crate::{
         APP_CONFIG, AppError, AppResult, CHAIN_ID, CODES, CONFIG, Db, EventResult, GasTracker,
         Indexer, LAST_FINALIZED_BLOCK, NEXT_CRONJOBS, NaiveProposalPreparer, NaiveQuerier,
-        NullIndexer, ProposalPreparer, QuerierProviderImpl, TraceOption, UpgradeHandler, Vm,
-        catch_and_push_event, catch_and_update_event, do_authenticate, do_backrun, do_configure,
-        do_cron_execute, do_execute, do_finalize_fee, do_instantiate, do_migrate, do_transfer,
-        do_upload, do_withhold_fee, query_app_config, query_balance, query_balances, query_code,
-        query_codes, query_config, query_contract, query_contracts, query_status, query_supplies,
-        query_supply, query_wasm_raw, query_wasm_scan, query_wasm_smart,
+        NullIndexer, ProposalPreparer, QuerierProviderImpl, TraceOption, UPGRADES, UpgradeHandler,
+        Vm, catch_and_push_event, catch_and_update_event, do_authenticate, do_backrun,
+        do_configure, do_cron_execute, do_execute, do_finalize_fee, do_instantiate, do_migrate,
+        do_transfer, do_upload, do_withhold_fee, query_app_config, query_balance, query_balances,
+        query_code, query_codes, query_config, query_contract, query_contracts, query_status,
+        query_supplies, query_supply, query_upgrade, query_upgrades, query_wasm_raw,
+        query_wasm_scan, query_wasm_smart,
     },
     grug_storage::PrefixBound,
     grug_types::{
