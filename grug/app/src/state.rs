6b00e9586918a06bcaa6b72d21fcfa2c103b300f
larry <26318510+larry0x@users.noreply.github.com>
2025-10-28 23:45:55 +0000

    reorganize `state.rs` file

--- grug/app/src/state.rs
+++ grug/app/src/state.rs
@@ -9,20 +9,26 @@ use {
 /// A string that identifies the chain
 pub const CHAIN_ID: Item<String> = Item::new("chain_id");
 
+/// The most recently finalized block
+pub const LAST_FINALIZED_BLOCK: Item<BlockInfo> = Item::new("last_finalized_block");
+
 /// Chain-level configuration
 pub const CONFIG: Item<Config> = Item::new("config");
 
 /// Application-specific configuration.
 pub const APP_CONFIG: Item<Json> = Item::new("app_config");
 
-/// The most recently finalized block
-pub const LAST_FINALIZED_BLOCK: Item<BlockInfo> = Item::new("last_finalized_block");
-
 /// Scheduled cronjobs.
 ///
 /// This needs to be a `Set` instead of `Map<Timestamp, Addr>` because there can
 /// be multiple jobs with the same scheduled time.
 pub const NEXT_CRONJOBS: Set<(Timestamp, Addr)> = Set::new("jobs");
+
+/// A chain upgrade that is scheduled to happen in a future block.
+pub const NEXT_UPGRADE: Item<NextUpgrade> = Item::new("next_upgrade");
+
+/// Chain upgrades that have been carried out in the past.
+pub const PREV_UPGRADES: Map<u64, PastUpgrade> = Map::new("prev_upgrade");
 
 /// Wasm contract byte codes: code_hash => byte_code
 pub const CODES: IndexedMap<Hash256, Code, CodeIndexes> = IndexedMap::new("codes", CodeIndexes {
@@ -35,12 +41,6 @@ pub const CONTRACTS: Map<Addr, ContractInfo> = Map::new("contract");
 /// Each contract has its own storage space, which we term the "substore".
 /// A key in a contract's substore is prefixed by the word "wasm" + contract address.
 pub const CONTRACT_NAMESPACE: &[u8] = b"wasm";
-
-/// A chain upgrade that is scheduled to happen in a future block.
-pub const NEXT_UPGRADE: Item<NextUpgrade> = Item::new("next_upgrade");
-
-/// Chain upgrades that have been carried out in the past.
-pub const PREV_UPGRADES: Map<u64, PastUpgrade> = Map::new("prev_upgrade");
 
 pub struct CodeIndexes<'a> {
     pub status: MultiIndex<'a, Hash256, CodeStatus, Code>,
