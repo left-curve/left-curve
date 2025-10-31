6b78f8f2ae0d607483e1975e4a6408b709b4b9e0
Pacman <pacman@apollo.farm>
2025-10-31 11:20:11 +0100

    fix(dango): use MAX_ORACLE_STALENESS in provide liquidity query

--- dango/dex/src/query.rs
+++ dango/dex/src/query.rs
@@ -516,7 +516,8 @@ fn query_simulate_provide_liquidity(
     quote_denom: Denom,
     deposit: CoinPair,
 ) -> anyhow::Result<Coin> {
-    let mut oracle_querier = OracleQuerier::new_remote(ctx.querier.query_oracle()?, ctx.querier);
+    let mut oracle_querier = OracleQuerier::new_remote(ctx.querier.query_oracle()?, ctx.querier)
+        .with_no_older_than(ctx.block.timestamp - MAX_ORACLE_STALENESS);
     let pair = PAIRS.load(ctx.storage, (&base_denom, &quote_denom))?;
     let reserve = RESERVES.load(ctx.storage, (&base_denom, &quote_denom))?;
     let lp_token_supply = ctx.querier.query_supply(pair.lp_denom.clone())?;
