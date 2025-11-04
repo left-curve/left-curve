c2ad5e04261bc385f430a235f276de278d0adbe4
j0nl1 <jonlitech@gmail.com>
2025-11-04 14:59:40 +0000

    feat: check test e2e

--- ui/portal/web/playwright.config.ts
+++ ui/portal/web/playwright.config.ts
@@ -34,9 +34,4 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "pnpm preview",
-  },
 });
