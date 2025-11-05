5c28ca6ebc15f88da4110d668ea86f3b0924502e
j0nl1 <jonlitech@gmail.com>
2025-11-05 13:23:49 +0000

    frontend url

--- ui/portal/web/playwright.config.ts
+++ ui/portal/web/playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5080",
+    baseURL: process.env.FRONTEND_URL || "http://localhost:5080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
