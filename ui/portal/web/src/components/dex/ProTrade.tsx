535b7cc9ed104ab68d42fc9e5ae5f4651dfa05cd
j0nl1 <jonlitech@gmail.com>
2025-11-11 23:59:55 +0000

    chore: remove chartiq

--- ui/portal/web/src/components/dex/ProTrade.tsx
+++ ui/portal/web/src/components/dex/ProTrade.tsx
@@ -108,22 +108,16 @@ const ProTradeOverview: React.FC = () => {
   return <OrderBookOverview state={state} controllers={controllers} />;
 };
 
-const ChartIQ = lazy(() =>
-  import("../foundation/ChartIQ").then(({ ChartIQ }) => ({ default: ChartIQ })),
-);
-
 const TradingView = lazy(() =>
-  import("./TradingView").then(({ TradingView }) => ({ default: TradingView })),
+  import("./TradingView")
+    .then(({ TradingView }) => ({ default: TradingView }))
+    .catch(() => ({ default: () => <div>Chart</div> })),
 );
 
 const ProTradeChart: React.FC = () => {
   const { state } = useProTrade();
   const { isLg } = useMediaQuery();
-  const { settings } = useApp();
-  const { chart } = settings;
   const { baseCoin, quoteCoin, orders } = state;
-
-  const ChartComponent = chart === "tradingview" ? TradingView : ChartIQ;
 
   const mobileContainer = usePortalTarget("#chart-container-mobile");
 
@@ -137,7 +131,7 @@ const ProTradeChart: React.FC = () => {
     <Suspense fallback={<Spinner color="pink" size="md" />}>
       <div className="flex w-full lg:min-h-[45vh] h-full" id="chart-container">
         <ErrorBoundary fallback={<div className="p-4">Chart Engine</div>}>
-          <ChartComponent coins={{ base: baseCoin, quote: quoteCoin }} orders={ordersByPair} />
+          <TradingView coins={{ base: baseCoin, quote: quoteCoin }} orders={ordersByPair} />
         </ErrorBoundary>
       </div>
     </Suspense>
