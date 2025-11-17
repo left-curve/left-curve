c2cc2fe588b19e94323fe35fab7be8660b306b7f
larry <26318510+larry0x@users.noreply.github.com>
2025-11-17 13:23:12 +0000

    add more tracing logs tracking migration progress

--- dango/upgrade/src/lib.rs
+++ dango/upgrade/src/lib.rs
@@ -17,16 +17,34 @@ pub fn do_upgrade<VM>(storage: Box<dyn Storage>, _vm: VM, _block: BlockInfo) -> 
         .range(&dex_storage, None, None, Order::Ascending)
         .collect::<StdResult<Vec<_>>>()?
     {
-        let volume = volume.checked_mul(SCALING_FACTOR).map_err(StdError::from)?;
-        dango_dex::VOLUMES.save(&mut dex_storage, (&address, timestamp), &volume)?;
+        let new_volume = volume.checked_mul(SCALING_FACTOR).map_err(StdError::from)?;
+
+        dango_dex::VOLUMES.save(&mut dex_storage, (&address, timestamp), &new_volume)?;
+
+        tracing::info!(
+            %address,
+            timestamp = timestamp.to_rfc3339_string(),
+            %volume,
+            %new_volume,
+            "Migrated volume"
+        );
     }
 
     for ((username, timestamp), volume) in dango_dex::VOLUMES_BY_USER
         .range(&dex_storage, None, None, Order::Ascending)
         .collect::<StdResult<Vec<_>>>()?
     {
-        let volume = volume.checked_mul(SCALING_FACTOR).map_err(StdError::from)?;
-        dango_dex::VOLUMES_BY_USER.save(&mut dex_storage, (&username, timestamp), &volume)?;
+        let new_volume = volume.checked_mul(SCALING_FACTOR).map_err(StdError::from)?;
+
+        dango_dex::VOLUMES_BY_USER.save(&mut dex_storage, (&username, timestamp), &new_volume)?;
+
+        tracing::info!(
+            %username,
+            timestamp = timestamp.to_rfc3339_string(),
+            %volume,
+            %new_volume,
+            "Migrated volume by user"
+        );
     }
 
     tracing::info!("Completed deleting DEX volume data");
