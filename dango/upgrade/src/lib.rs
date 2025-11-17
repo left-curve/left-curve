bd2acd5959ff1930e76eaaa717987e4bee983191
larry <26318510+larry0x@users.noreply.github.com>
2025-11-17 12:43:00 +0000

    scale existing volume data instead of deleting them

--- dango/upgrade/src/lib.rs
+++ dango/upgrade/src/lib.rs
@@ -1,19 +1,33 @@
 use {
-    grug::{Addr, BlockInfo, Storage, addr},
+    dango_types::constants::usdc,
+    grug::{Addr, BlockInfo, Number, Order, StdError, StdResult, Storage, Udec128_6, addr},
     grug_app::{AppResult, CONTRACT_NAMESPACE, StorageProvider},
 };
 
-/// Address of the DEX contract.
 const DEX: Addr = addr!("8dd37b7e12d36bbe1c00ce9f0c341bfe1712e73f");
+
+const SCALING_FACTOR: Udec128_6 = Udec128_6::new(10_u128.pow(usdc::DECIMAL));
 
 pub fn do_upgrade<VM>(storage: Box<dyn Storage>, _vm: VM, _block: BlockInfo) -> AppResult<()> {
     tracing::info!("Deleting DEX volume data");
 
-    // Get the storage of the DEX contract.
     let mut dex_storage = StorageProvider::new(storage, &[CONTRACT_NAMESPACE, &DEX]);
 
-    dango_dex::VOLUMES.clear(&mut dex_storage, None, None);
-    dango_dex::VOLUMES_BY_USER.clear(&mut dex_storage, None, None);
+    for ((address, timestamp), volume) in dango_dex::VOLUMES
+        .range(&dex_storage, None, None, Order::Ascending)
+        .collect::<StdResult<Vec<_>>>()?
+    {
+        let volume = volume.checked_mul(SCALING_FACTOR).map_err(StdError::from)?;
+        dango_dex::VOLUMES.save(&mut dex_storage, (&address, timestamp), &volume)?;
+    }
+
+    for ((username, timestamp), volume) in dango_dex::VOLUMES_BY_USER
+        .range(&dex_storage, None, None, Order::Ascending)
+        .collect::<StdResult<Vec<_>>>()?
+    {
+        let volume = volume.checked_mul(SCALING_FACTOR).map_err(StdError::from)?;
+        dango_dex::VOLUMES_BY_USER.save(&mut dex_storage, (&username, timestamp), &volume)?;
+    }
 
     tracing::info!("Completed deleting DEX volume data");
 
