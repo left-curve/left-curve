0145d5b98edb523cc7eca23d219127164ce55a79
Pacman <pacman@apollo.farm>
2025-11-20 21:21:19 +0100

    test(dango): add unit tests for Avellaneda-Stoikov calculations

--- dango/dex/src/core/geometric/avellaneda_stoikov.rs
+++ dango/dex/src/core/geometric/avellaneda_stoikov.rs
@@ -1,6 +1,6 @@
 use {
     super::{math::ln_dec, *},
-    grug::{Dec, Duration, Fraction},
+    grug::{Dec128_24, Dec256_24, Duration, Fraction, NextNumber, PrevNumber, Udec256_24}, std::str::FromStr,
 };
 
 /// Computes the reservation price for the Avellaneda-Stoikov model.
@@ -71,7 +71,7 @@ pub fn half_spread(
     let one_over_k = k.checked_inv()?;
     let one_plus_gamma_over_k = Price::ONE.checked_add(gamma.checked_div(k)?)?;
 
-    // Compute the natural logarithm of the one plus gamma over k.
+    // Compute the natural logarithm of one plus gamma over k.
     let natural_log_of_one_plus_gamma_over_k =
         ln_dec(one_plus_gamma_over_k.checked_into_signed()?)?.checked_into_unsigned()?;
 
@@ -84,4 +84,99 @@ pub fn half_spread(
                 .checked_mul(sigma_squared)?
                 .checked_mul(time_horizon_as_dec)?,
         )?)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    use test_case::test_case;
+
+    #[test_case(
+        Price::ONE, 
+        Price::ZERO,
+        Price::ONE,
+        Duration::from_seconds(300)
+        => Price::ZERO
+        ; "gamma = 0 means no spread"
+    )]
+    #[test_case(
+        Price::ONE, 
+        Price::ONE,
+        Price::from_str("0.00000000076787914154").unwrap(),
+        Duration::from_seconds(300)
+        => Price::from_str("0.693377544302407308652726").unwrap()
+    )]
+    #[test_case(
+        Price::ONE, 
+        Price::ONE,
+        Price::from_str("0.000000076787914154").unwrap(),
+        Duration::from_seconds(300)
+        => Price::from_str("0.716183554806145308652726").unwrap()
+        ; "higher volatility means higher spread"
+    )]
+    fn test_half_spread(
+        k: Price,
+        gamma: Price,
+        sigma_squared: Price,
+        time_horizon: Duration,
+    ) -> Price {
+        println!("gamma: {:?}", gamma.to_string());
+        half_spread(k, gamma, sigma_squared, time_horizon).unwrap()
+    }
+
+    #[test_case(
+        Price::ONE, 
+        Uint128::new(1_000_000_000u128),
+        Uint128::new(1_000_000_000u128),
+        Bounded::new(Udec128::from_str("0.5").unwrap()).unwrap(),
+        Price::ONE,
+        Price::ONE,
+        Duration::from_seconds(120)
+        => Price::ONE
+    )]
+    #[test_case(
+        Price::from_str("1.1").unwrap(),
+        Uint128::new(1_000_000_000u128),
+        Uint128::new(1_000_000_000u128),
+        Bounded::new(Udec128::from_str("0.5").unwrap()).unwrap(),
+        Price::from_str("0.00000000076787914154").unwrap(),
+        Price::from_str("0.01").unwrap(),
+        Duration::from_seconds(120)
+        => Price::from_str("1.058115683188727272727273").unwrap()
+        ; "too high reserves moves reservation price below oracle price"
+    )]
+    #[test_case(
+        Price::from_str("0.9").unwrap(), 
+        Uint128::new(1_000_000_000u128),
+        Uint128::new(1_000_000_000u128),
+        Bounded::new(Udec128::from_str("0.5").unwrap()).unwrap(),
+        Price::from_str("0.00000000076787914154").unwrap(),
+        Price::from_str("0.01").unwrap(),
+        Duration::from_seconds(120)
+        => Price::from_str("0.951191942769333333333333").unwrap()
+        ; "too low reserves moves reservation price above oracle price"
+    )]
+    fn test_reservation_price(
+        oracle_price: Price,
+        base_inventory: Uint128,
+        quote_inventory: Uint128,
+        base_inventory_target_percentage: Bounded<Udec128, ZeroExclusiveOneExclusive>,
+        sigma_squared: Price,
+        gamma: Price,
+        time_horizon: Duration,
+    ) -> Price {
+        reservation_price(
+            oracle_price,
+            base_inventory,
+            quote_inventory,
+            base_inventory_target_percentage,
+            sigma_squared,
+            gamma,
+            time_horizon,
+        )
+        .unwrap()
+    }
 }
