376f9cfe16503f1c7d8407ea9a7d8b4ce1063efd
kyar1s <iris.ledesma.27@gmail.com>
2025-11-24 15:45:34 +0000

    feat: add settings section

---
+++ ui/portal/app/src/screens/(app)/settings.tsx
@@ -0,0 +1,33 @@
+import { useRouter } from "expo-router";
+import { View } from "react-native";
+import { Button, GlobalText, IconChevronDown } from "~/components/foundation";
+import { DisplaySection } from "~/components/Settings/DisplaySection";
+
+export default function Settings() {
+  const { back } = useRouter();
+
+  return (
+    <View className="flex-1 flex bg-surface-primary-rice w-full flex-col gap-8 p-4">
+      <View className="flex flex-row gap-2 items-center lg:hidden self-start">
+        <Button
+          variant="link"
+          size="icon"
+          classNames={{ icons: "text-ink-tertiary-500 rotate-90" }}
+          onPress={back}
+          rightIcon={<IconChevronDown className="text-ink-tertiary-500" />}
+        />
+
+        <GlobalText className="h3-bold text-ink-primary-900">Settings</GlobalText>
+      </View>
+      <DisplaySection>
+        <DisplaySection.Language />
+        <DisplaySection.FormatNumber />
+        <DisplaySection.DateFormat />
+        <DisplaySection.TimeFormat />
+        <DisplaySection.TimeZone />
+        <DisplaySection.ChartEngine />
+        <DisplaySection.Theme />
+      </DisplaySection>
+    </View>
+  );
+}
