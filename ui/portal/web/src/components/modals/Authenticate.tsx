2a6c202bde2e44d25f38f7102613d755d9fce323
j0nl1 <jonlitech@gmail.com>
2025-12-03 21:25:23 +0000

    feat: account or index support

--- ui/portal/web/src/components/modals/Authenticate.tsx
+++ ui/portal/web/src/components/modals/Authenticate.tsx
@@ -16,11 +16,9 @@ export const Authenticate = forwardRef((_, __) => {
   const AuthView = views[view as keyof typeof views];
 
   return (
-    <div className="flex flex-col bg-surface-primary-rice md:border border-outline-secondary-gray pt-0 md:pt-6 rounded-xl relative p-4 md:p-6 gap-5 w-full md:max-w-[25rem]">
+    <div className="flex flex-col justify-start items-center bg-surface-primary-rice text-ink-primary-900 md:border border-outline-secondary-gray pt-0 md:pt-6 rounded-xl relative p-4 md:p-6 gap-5 w-full md:w-[40rem] md:h-[40rem]">
       <p className="text-ink-primary-900 diatype-lg-medium w-full text-center" />
-      <div className=" flex flex-col gap-4 text-ink-primary-900">
-        <AuthView goTo={(view) => setView(view)} />
-      </div>
+      <AuthView goTo={(view) => setView(view)} />
       <IconButton
         className="hidden md:block absolute right-4 top-4"
         variant="link"
