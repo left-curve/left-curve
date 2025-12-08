41e49d75f163f8e86a089bba91cdb0ee694a2ec1
Pacman <pacman@apollo.farm>
2025-12-08 14:09:20 +0100

    refactor(dango): delete SVM related script

--- dango/hyperlane-deployment/src/bin/svm/deploy_hwr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script deploys the compiled Hyperlane Warp Route (HWR) contracts to the Solana network.
-
-echo $SOLANA_MNEMONIC
-
-# Convert mnemonic to solana keypair that we can use with the Solana CLI
-uv run mnemonic-to-keypair "$SOLANA_MNEMONIC"
