9cc33ac3c28110953431797b0c81908fdf90e56c
Fabien Penso <git@pen.so>
2025-12-19 12:42:41 +0100

    Fix debian no passphrase (#1492)
    * Disable debian vault when no password
    
    * Remove warnings
    
    * Add uptime kuma pause
    
    * Pause/Resume kuma
    
    * Update uptime kuma
    
    * Fix warnings

--- deploy/debian-key.sh
+++ deploy/debian-key.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DEBIAN_KEY_FILE="group_vars/all/debian_key.vault"
+DEBIAN_KEY_FILE="vaults/debian/debian_key.vault"
 DEBIAN_PASSWORD_SCRIPT="debian-password.sh"
 
 ansible-vault view "$DEBIAN_KEY_FILE" --vault-id debian@"$DEBIAN_PASSWORD_SCRIPT"
