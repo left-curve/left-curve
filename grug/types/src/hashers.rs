8793e148d37850f9e2479b9f39af170e72f5e984
Larry <26318510+larry0x@users.noreply.github.com>
2025-12-23 18:01:12 +0000

    Update grug/types/src/hashers.rs
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- grug/types/src/hashers.rs
+++ grug/types/src/hashers.rs
@@ -26,7 +26,7 @@ pub trait HashExt {
     /// Produce a hash of the data using the SHA2-256 algorithm.
     fn sha2_256(&self) -> Hash256;
 
-    /// Produce a has of the data using the Keccak256 algorithm.
+    /// Produce a hash of the data using the Keccak256 algorithm.
     fn keccak256(&self) -> Hash256;
 }
 
