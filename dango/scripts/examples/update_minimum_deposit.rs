85492a82472e51e78202631d43794d940dec9f5d
larry <26318510+larry0x@users.noreply.github.com>
2026-01-05 23:30:51 +0000

    extract common part of the script to a library

--- dango/scripts/examples/update_minimum_deposit.rs
+++ dango/scripts/examples/update_minimum_deposit.rs
@@ -1,71 +1,28 @@
 use {
-    anyhow::anyhow,
-    dango_client::{Keystore, Secp256k1, Secret, SingleSigner},
     dango_types::{config::AppConfig, constants::usdc},
-    grug::{
-        Addr, BroadcastClientExt, GasOption, JsonSerExt, Message, QueryClientExt, SearchTxClient,
-        addr, coins,
-    },
-    grug_app::GAS_COSTS,
+    grug::{Addr, Message, QueryClientExt, addr, coins},
     indexer_client::HttpClient,
-    std::time::Duration,
 };
+
+const API_URL: &str = "https://api-mainnet.dango.zone/";
 
 const OWNER_ADDRESS: Addr = addr!("149a2e2bc3ed63aeb0410416b9123d886af1f9cd");
 
 const OWNER_SECRET_PATH: &str = "/Users/larry/.dango/keys/larry.json";
 
+struct MessageBuilder;
+
+#[async_trait::async_trait]
+impl dango_scripts::MessageBuilder for MessageBuilder {
+    async fn build_message(client: &HttpClient) -> anyhow::Result<Message> {
+        let mut app_cfg: AppConfig = client.query_app_config(None).await?;
+        app_cfg.minimum_deposit = coins! { usdc::DENOM.clone() => 10_000_000 };
+
+        Ok(Message::configure(None, Some(app_cfg))?)
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let client = HttpClient::new("https://api-mainnet.dango.zone/")?;
-
-    let status = client.query_status(None).await?;
-    println!(
-        "Connected to HTTP client. Current height: {}",
-        status.last_finalized_block.height
-    );
-
-    let owner_secret = {
-        let password = dialoguer::Password::new()
-            .with_prompt("Enter the password to decrypt the key")
-            .interact()?;
-        let sk_bytes = Keystore::from_file(OWNER_SECRET_PATH, &password)?;
-        Secp256k1::from_bytes(sk_bytes)?
-    };
-
-    let mut owner = SingleSigner::new(OWNER_ADDRESS, owner_secret)
-        .with_query_user_index(&client)
-        .await?
-        .with_query_nonce(&client)
-        .await?;
-
-    let mut app_cfg: AppConfig = client.query_app_config(None).await?;
-    app_cfg.minimum_deposit = coins! { usdc::DENOM.clone() => 10000000 };
-
-    let outcome = client
-        .send_message_with_confirmation(
-            &mut owner,
-            Message::configure(None, Some(app_cfg.to_json_value()?))?,
-            GasOption::Simulate {
-                scale: 2.,
-                flat_increase: GAS_COSTS.secp256k1_verify,
-            },
-            &status.chain_id,
-            |tx| {
-                println!("{}", tx.to_json_string_pretty()?);
-                Ok(dialoguer::Confirm::new()
-                    .with_prompt("Broadcast transaction?")
-                    .interact()?)
-            },
-        )
-        .await?
-        .ok_or_else(|| anyhow!("User rejected transaction"))?;
-    println!("Tx broadcasted: {}", outcome.tx_hash);
-
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    let outcome = client.search_tx(outcome.tx_hash).await?;
-    println!("{}", outcome.to_json_string_pretty()?);
-
-    Ok(())
+    dango_scripts::send_message::<MessageBuilder>(API_URL, OWNER_SECRET_PATH, OWNER_ADDRESS).await
 }
