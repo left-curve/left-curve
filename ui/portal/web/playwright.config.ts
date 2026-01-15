0bf8e5d71206b418b081921076299b9cec5fcf85
j0nl1 <jonlitech@gmail.com>
2026-01-15 10:06:38 +0000

    feat: increase workers in tests

--- ui/portal/web/playwright.config.ts
+++ ui/portal/web/playwright.config.ts
@@ -10,10 +10,7 @@ export default defineConfig({
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
