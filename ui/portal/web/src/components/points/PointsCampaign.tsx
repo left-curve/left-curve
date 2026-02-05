a90b7fa9abe21fbf477976b910c3c146b1c2daf3
kyar1s <iris.ledesma.27@gmail.com>
2026-02-05 03:15:53 +0000

    fix: add nft images

--- ui/portal/web/src/components/points/PointsCampaign.tsx
+++ ui/portal/web/src/components/points/PointsCampaign.tsx
@@ -102,36 +102,12 @@ const RewardsLoot: React.FC = () => {
       <div className="flex flex-col gap-3">
         <p className="h3-bold text-ink-primary-900">My NFTs</p>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-8">
-          <NFTCard
-            rarity="common"
-            quantity={4}
-            imageSrc="https://www.figma.com/api/mcp/asset/c3b5358b-c2b3-4bc0-a1d6-30117c53b423"
-          />
-          <NFTCard
-            rarity="uncommon"
-            quantity={2}
-            imageSrc="https://www.figma.com/api/mcp/asset/9680ed08-69ef-471f-83a5-813846101610"
-          />
-          <NFTCard
-            rarity="epic"
-            quantity={2}
-            imageSrc="https://www.figma.com/api/mcp/asset/fe211f83-4040-4023-ae76-da8967f68d53"
-          />
-          <NFTCard
-            rarity="golden"
-            quantity={2}
-            imageSrc="https://www.figma.com/api/mcp/asset/21afd773-9bb9-4cd4-a30c-dc9a356d0708"
-          />
-          <NFTCard
-            rarity="legendary"
-            quantity={2}
-            imageSrc="https://www.figma.com/api/mcp/asset/e21cd2e1-7549-4b53-8916-3d1713bb5699"
-          />
-          <NFTCard
-            rarity="rare"
-            quantity={2}
-            imageSrc="https://www.figma.com/api/mcp/asset/555ebcef-e6f2-490b-9c44-b72b36dad681"
-          />
+          <NFTCard rarity="common" quantity={4} imageSrc="/images/points/nft/common.png" />
+          <NFTCard rarity="uncommon" quantity={2} imageSrc="/images/points/nft/uncommon.png" />
+          <NFTCard rarity="epic" quantity={2} imageSrc="/images/points/nft/epic.png" />
+          <NFTCard rarity="golden" quantity={2} imageSrc="/images/points/nft/mythic.png" />
+          <NFTCard rarity="legendary" quantity={2} imageSrc="/images/points/nft/legendary.png" />
+          <NFTCard rarity="rare" quantity={2} imageSrc="/images/points/nft/rare.png" />
         </div>
       </div>
     </div>
