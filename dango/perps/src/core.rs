49a5f21252ce697c0c2839e19285546825c02ac4
larry <26318510+larry0x@users.noreply.github.com>
2026-02-22 01:51:21 +0000

    rename `margin.rs` to `user.rs`

--- dango/perps/src/core.rs
+++ dango/perps/src/core.rs
@@ -1,14 +1,14 @@
 mod decompose;
 mod fees;
 mod funding;
-mod margin;
 mod min_open;
 mod oi;
 mod pricing;
 mod target_price;
+mod user;
 mod vault;
 
 pub use {
-    decompose::*, fees::*, funding::*, margin::*, min_open::*, oi::*, pricing::*,
-    target_price::*, vault::*,
+    decompose::*, fees::*, funding::*, min_open::*, oi::*, pricing::*, target_price::*, user::*,
+    vault::*,
 };
