29fded94ddf4c713880e5c388caeb57b69a7f3d9
larry <26318510+larry0x@users.noreply.github.com>
2026-02-26 03:52:38 +0000

    remove a TODO item that is no longer applicable

--- dango/perps/src/execute/on_oracle_update.rs
+++ dango/perps/src/execute/on_oracle_update.rs
@@ -17,8 +17,7 @@ pub fn on_oracle_update(ctx: MutableCtx) -> anyhow::Result<Response> {
 
     // TODO:
     // 1. accrue funding for all pairs
-    // 2. check for fillable limit orders and fill them
-    // 3. check for unlocks that have completed cooldown and release the fund
+    // 2. check for unlocks that have completed cooldown and release the fund
 
     Ok(Response::new())
 }
