fd3a585505db011702696769787b905040009b13
kyar1s <iris.ledesma.27@gmail.com>
2026-03-31 20:40:03 +0100

    fix: make it work for devnet

--- ui/portal/web/src/pages/(app)/_app.trade.index.tsx
+++ ui/portal/web/src/pages/(app)/_app.trade.index.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(app)/_app/trade/")({
-  beforeLoad: async () => {
+  beforeLoad: async ({ context }) => {
+    const { config } = context;
+    const isDevnet = config.chain.id === "dev-9";
+
     throw redirect({
       to: "/trade/$pairSymbols",
-      params: { pairSymbols: "BTC-USD" },
+      params: { pairSymbols: isDevnet ? "ETH-USD" : "BTC-USD" },
       search: { type: "perps" },
     });
   },
