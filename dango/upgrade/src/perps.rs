5678aee85a2df0e0a3287d620f71e73fa4de9ace
larry <26318510+larry0x@users.noreply.github.com>
2026-05-20 16:57:59 +0100

    refactor: improve code quality in dango-upgrade

---
+++ dango/upgrade/src/perps.rs
@@ -0,0 +1,40 @@
+// The current release does not include upgrades to the perps contract.
+// This files contains only the boilerplate.
+#![allow(dead_code)]
+
+use {
+    grug::{Addr, Storage, addr},
+    grug_app::{AppResult, CHAIN_ID, CONTRACT_NAMESPACE, StorageProvider},
+};
+
+const MAINNET_CHAIN_ID: &str = "dango-1";
+const MAINNET_PERPS_ADDRESS: Addr = addr!("90bc84df68d1aa59a857e04ed529e9a26edbea4f");
+
+const TESTNET_CHAIN_ID: &str = "dango-testnet-1";
+const TESTNET_PERPS_ADDRESS: Addr = addr!("f6344c5e2792e8f9202c58a2d88fbbde4cd3142f");
+
+pub fn do_perps_upgrades(storage: Box<dyn Storage>) -> AppResult<()> {
+    // Find the address of the perps contract corresponding to the current chain.
+    let perps_address = {
+        let chain_id = CHAIN_ID.load(&storage)?;
+        match chain_id.as_str() {
+            MAINNET_CHAIN_ID => MAINNET_PERPS_ADDRESS,
+            TESTNET_CHAIN_ID => TESTNET_PERPS_ADDRESS,
+            _ => panic!("unknown chain id: {chain_id}"),
+        }
+    };
+
+    // Create the prefixed storage for the perps contract.
+    let _perps_storage = StorageProvider::new(storage, &[CONTRACT_NAMESPACE, &perps_address]);
+
+    // TODO: add actual upgrade logic here.
+
+    Ok(())
+}
+
+// ----------------------------------- tests -----------------------------------
+
+#[cfg(test)]
+mod tests {
+    // TODO: add tests here.
+}
