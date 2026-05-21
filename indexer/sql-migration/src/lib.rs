4b905b0e4fa0a97966a4d6b856fb38a398083873
j0nl1 <git@viana.email>
2026-05-21 14:57:08 +0100

    feat: transactions index

--- indexer/sql-migration/src/lib.rs
+++ indexer/sql-migration/src/lib.rs
@@ -7,6 +7,7 @@ mod m20250529_142838_transactions_sender_index;
 mod m20250717_192805_events_data_index;
 mod m20250812_110949_messages_indexes;
 mod m20250902_175929_transactions_http_request_details;
+mod m20260521_124756_transactions_sender_block_height_transaction_idx;
 
 pub struct Migrator;
 
@@ -20,6 +21,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250717_192805_events_data_index::Migration),
             Box::new(m20250812_110949_messages_indexes::Migration),
             Box::new(m20250902_175929_transactions_http_request_details::Migration),
+            Box::new(m20260521_124756_transactions_sender_block_height_transaction_idx::Migration),
         ]
     }
 
