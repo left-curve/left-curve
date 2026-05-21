5a27285ede0be2f21a77418936cbbeb5a4352f05
kyar1s <iris.ledesma.27@gmail.com>
2026-05-21 16:05:23 +0100

    fix: badge style

--- ui/applets/kit/src/components/Badge.tsx
+++ ui/applets/kit/src/components/Badge.tsx
@@ -22,7 +22,7 @@ const badgeVariants = tv(
         green: "bg-surface-tertiary-green text-fg-primary-green border-outline-primary-green",
         rice: "bg-surface-quaternary-rice text-fg-primary-rice border-outline-secondary-rice",
         "gradient-rice":
-          "bg-account-card-contract text-ink-primary-rice rounded-md border-transparent",
+          "bg-account-card-contract text-ink-primary-rice rounded-md border-transparent shadow-account-card",
         "light-red": "bg-utility-error-25 text-utility-error-600 border-utility-error-100",
         "light-green": "bg-utility-success-25 text-utility-success-500 border-utility-success-50",
         gray: "bg-utility-gray-100 text-utility-gray-500 border-utility-gray-200",
