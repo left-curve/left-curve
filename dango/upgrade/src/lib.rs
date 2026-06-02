1081893de707af76feace8d61e2d17917e59c889
Larry <26318510+larry0x@users.noreply.github.com>
2026-06-02 13:23:53 +0100

    chore: remove v0.20 upgrade logic (#2113)

--- dango/upgrade/src/lib.rs
+++ dango/upgrade/src/lib.rs
@@ -1,18 +1,16 @@
-mod app_config;
-mod gateway;
 mod oracle;
 mod perps;
-mod taxman;
 
 use {
     grug_app::AppResult,
     grug_types::{BlockInfo, Storage},
 };
 
-pub fn do_upgrade<VM>(mut storage: Box<dyn Storage>, _vm: VM, block: BlockInfo) -> AppResult<()> {
-    app_config::do_app_config_upgrade(&mut *storage)?;
-    gateway::do_gateway_upgrades(storage.clone())?;
-    oracle::do_oracle_upgrades(storage.clone())?;
-    perps::do_perps_upgrades(storage.clone(), block)?;
-    taxman::do_taxman_upgrades(storage)
+pub fn do_upgrade<VM>(_storage: Box<dyn Storage>, _vm: VM, _block: BlockInfo) -> AppResult<()> {
+    // Nothing to do in the current version.
+
+    // oracle::do_oracle_upgrades(storage.clone())?;
+    // perps::do_perps_upgrades(storage)?;
+
+    Ok(())
 }
